6006155a3e568d321277cc0cf75ea9cd9fa21c76
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-13 09:42:12 +0530

    Time: 69 ms (74.88%), Space: 42.5 MB (10.15%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -4,15 +4,12 @@
  */
 var numIdenticalPairs = function(nums) {
     let goodPairs = 0;
-    let arr = []
     for(let i = 0; i < nums.length-1; i++){
         for(let j = i+1; j < nums.length; j++){
             if(nums[i] == nums[j] && i<j ){
               goodPairs++;  
-                arr.push([nums[i], nums[j]])
             }
         }
     }
-    console.log('arr', arr)
     return goodPairs;
 };
