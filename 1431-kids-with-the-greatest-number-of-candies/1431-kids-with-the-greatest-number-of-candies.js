e7c15f7881c1878a31a8fbb917cdb26b565a0d87
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-16 21:23:35 +0530

    Time: 98 ms (31.38%), Space: 42.3 MB (72.19%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -5,15 +5,10 @@
  */
 var kidsWithCandies = function(candies, extraCandies) {
 
-    const sortedArray = [...candies];
-    sortedArray.sort((a,b) => a-b);
-    let greatest = sortedArray[sortedArray.length-1];
+    // const sortedArray = [...candies];
+    // sortedArray.sort((a,b) => a-b);
+    let greatest = Math.max(...candies)
     console.log('greatest', greatest)
-  
-    // for (let i = 0; i <= candies.length; i++) {
-    //     // candies[i] += extraCandies;
-    //     // outputArray.push(candies[i] >= greatest);
-    // }
       let outputArray = candies.map((candy)=>{
           if((candy + extraCandies) >= greatest) return true;
           else return false;
