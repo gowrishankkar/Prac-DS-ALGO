8ae689741c6331f6f67f50b2419613aaacbc1a40
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-19 11:32:31 +0530

    Time: 119 ms (27.32%), Space: 44.1 MB (9.98%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -5,14 +5,16 @@
 var isPowerOfTwo = function(n) {
     let initialValue = 0;
     // console.log(Math.pow(2, 3))
-    let result = checkFunction(n, initialValue); 
-    function checkFunction(input, power){
-         if(input == Math.pow(2, power)) return true
-         else if(input < Math.pow(2, power))  return false;
-        else return checkFunction(input, power + 1)
-    }
+//     let result = checkFunction(n, initialValue); 
+//     function checkFunction(input, power){
+//          if(input == Math.pow(2, power)) return true
+//          else if(input < Math.pow(2, power))  return false;
+//         else return checkFunction(input, power + 1)
+//     }
     
-    console.log('result', result)
-    return result
+//     console.log('result', result)
+//     return result
+     if (n < 1) return false;
+  return (n & (n - 1)) === 0; 
    
 };
