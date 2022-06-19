f679bd99fab8dc400df7d5098ec057d7cdc8b180
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-19 11:42:52 +0530

    Time: 101 ms (46.79%), Space: 42.2 MB (97.44%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -3,15 +3,26 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
-        let initialValue = 0;
-    // console.log(Math.pow(2, 3))
-    let result = checkFunction(n, initialValue); 
-    function checkFunction(input, power){
-         if(input == Math.pow(4, power)) return true
-         else if(input < Math.pow(4, power))  return false;
-        else return checkFunction(input, power + 1)
+    
+    // solution 1 - recursively
+//         let initialValue = 0;
+//     // console.log(Math.pow(2, 3))
+//     let result = checkFunction(n, initialValue); 
+//     function checkFunction(input, power){
+//          if(input == Math.pow(4, power)) return true
+//          else if(input < Math.pow(4, power))  return false;
+//         else return checkFunction(input, power + 1)
+//     }
+    
+//     console.log('result', result)
+//     return result
+    
+        
+    // Solution 2
+    let i = 1;
+    while(i < n) {
+        i *= 4;
     }
     
-    console.log('result', result)
-    return result
+    return i === n
 };
