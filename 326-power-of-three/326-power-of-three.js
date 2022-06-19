7a3e1ab9b0b3fa704a9f23c4826006df387e99ba
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-19 11:41:18 +0530

    Time: 367 ms (27.59%), Space: 51.6 MB (15.73%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -3,16 +3,25 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-            let initialValue = 0;
-    // console.log(Math.pow(2, 3))
-    let result = checkFunction(n, initialValue); 
-    function checkFunction(input, power){
-         if(input == Math.pow(3, power)) return true
-         else if(input < Math.pow(3, power))  return false;
-        else return checkFunction(input, power + 1)
+//             let initialValue = 0;
+//     // console.log(Math.pow(2, 3))
+//     let result = checkFunction(n, initialValue); 
+//     function checkFunction(input, power){
+//          if(input == Math.pow(3, power)) return true
+//          else if(input < Math.pow(3, power))  return false;
+//         else return checkFunction(input, power + 1)
+//     }
+    
+//     console.log('result', result)
+//     return result
+    
+    
+          // Solution 2
+    let i = 1;
+    while(i < n) {
+        i *= 3;
     }
     
-    console.log('result', result)
-    return result
+    return i === n
     
 };
