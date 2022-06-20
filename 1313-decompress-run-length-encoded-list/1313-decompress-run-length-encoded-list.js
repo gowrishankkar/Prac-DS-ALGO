3055805531d50e795f9f0eb45d1ad8ba9598275c
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-20 09:37:55 +0530

    Time: 111 ms (57.73%), Space: 47 MB (18.25%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var decompressRLElist = function(nums) {
+    let arr = [];
+     
+    for(let i = 0; i<nums.length-1 ; i = i+2){
+        let intArr = []
+        for(let j = 0 ; j<nums[i] ; j++){
+            arr.push(nums[i+1])
+        }
+    }
+    console.log('arr', arr)
+    return arr;    
+
+};
