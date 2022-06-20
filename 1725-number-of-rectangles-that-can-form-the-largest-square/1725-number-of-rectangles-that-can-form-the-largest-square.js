0af3cc48943a2b22a177785df324f4aae87ce39b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-20 09:19:51 +0530

    Time: 228 ms (5.47%), Space: 45.5 MB (42.08%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.js
@@ -14,13 +14,28 @@ var countGoodRectangles = function(rectangles) {
 
 //     return result
     
-    // method 2
-    const count = {};
-
-        arr.forEach(element => {
-          count[element] = (count[element] || 0) + 1;
-        });
     
-    // console.log('count', count[Math.max(...arr)])
-    return count[Math.max(...arr)]
+    
+    
+    // method 2
+//     const count = {};
+
+//         arr.forEach(element => {
+//           count[element] = (count[element] || 0) + 1;
+//         });
+    
+//     // console.log('count', count[Math.max(...arr)])
+//     return count[Math.max(...arr)]
+    
+    
+    // method 3
+    let maxLenAmt = 0;
+
+    for (let num of arr) {
+        if (num === Math.max(...arr)) maxLenAmt++;
+    }
+
+    return maxLenAmt;
+    
+    
 };
