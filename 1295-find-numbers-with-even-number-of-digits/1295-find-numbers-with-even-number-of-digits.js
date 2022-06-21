6a4d38aa87b9af2cba5391149aba63fd8008f4f2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-21 22:27:21 +0530

    Time: 109 ms (22.55%), Space: 43.9 MB (67.50%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -5,7 +5,6 @@
 var findNumbers = function(nums) {
     let count = 0;
     for(let i = 0; i <nums.length; i++){
-        console.log('(nums[i].length)%2', nums[i].toString().length)
         if((nums[i].toString().length)%2 === 0) count++
     }
     return count
