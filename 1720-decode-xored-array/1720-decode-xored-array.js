bd982dbdf73568702a2f29ce8f405813efd4a2f8
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-22 22:50:33 +0530

    Time: 205 ms (5.18%), Space: 49 MB (30.74%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} encoded
+ * @param {number} first
+ * @return {number[]}
+ */
+var decode = function(encoded, first) {
+    const decoded = [first]
+    for (let i = 0; i < encoded.length; i++) decoded.push(decoded[i]^encoded[i])
+    return decoded
+};
