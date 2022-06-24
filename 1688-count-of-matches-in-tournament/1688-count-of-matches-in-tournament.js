c52f11ccea924c4efbeaa1286ac0fa9691b6ce51
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-24 13:04:47 +0530

    Time: 93 ms (42.23%), Space: 42.3 MB (21.91%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    let result = 0;
-    let teams = n;
-    while(Math.floor(teams/2) > 0){
-        if(teams%2 === 0){
-            result+=(Math.floor(teams/2));
-            teams=Math.floor(teams/2)
-        }else {
-            result+=Math.floor((teams - 1) /2);
-            teams= 1+Math.floor((teams - 1)/2)
-        }
-    }
-    return result
+    // let result = 0;
+    // let teams = n;
+    // while(Math.floor(teams/2) > 0){
+    //     if(teams%2 === 0){
+    //         result+=(Math.floor(teams/2));
+    //         teams=Math.floor(teams/2)
+    //     }else {
+    //         result+=Math.floor((teams - 1) /2);
+    //         teams= 1+Math.floor((teams - 1)/2)
+    //     }
+    // }
+    // return result
+    return n - 1
 };
