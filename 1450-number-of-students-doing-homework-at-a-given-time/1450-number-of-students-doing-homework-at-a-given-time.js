74e2c3aa24d7fc7e23b2d3f009d04690f5a4cd0c
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-06-28 22:51:48 +0530

    Time: 104 ms (14.83%), Space: 42.2 MB (31.32%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.js
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.js
@@ -7,16 +7,23 @@
 var busyStudent = function(startTime, endTime, queryTime) {
     let length = startTime.length;
     let result = 0;
-    for(let i = 0; i < length; i++){
-        // console.log('i', i)
-            for(let j = startTime[i]; j <= endTime[i]; j++){
-                 // console.log('j', j)
-                if(j == queryTime){
-                    result++
-                }
-            }
+    // for(let i = 0; i < length; i++){
+    //     // console.log('i', i)
+    //         for(let j = startTime[i]; j <= endTime[i]; j++){
+    //              // console.log('j', j)
+    //             if(j == queryTime){
+    //                 result++
+    //             }
+    //         }
+    // }
+    // console.log('result', result)
+    
+    for (i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            result++;
+        }
     }
-    console.log('result', result)
+    
     return result;
     
 };
