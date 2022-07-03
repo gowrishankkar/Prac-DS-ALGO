0eca0b2d669c8380add3a256ecfa517441053289
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-03 13:25:40 +0530

    Time: 78 ms (85.31%), Space: 44.8 MB (83.92%) - LeetHub

---
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function(nums) {
+      let res = 0;
+    let min = Math.min(...nums);
+    for(let num of nums){
+        res = res + num - min;
+    }
+    return res;
+};
