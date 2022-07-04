56b16bead585a46ab5ede13f80e58f33cc41be45
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-04 20:04:18 +0530

    Time: 144 ms (13.13%), Space: 48.6 MB (12.39%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -3,53 +3,23 @@
  * @return {number}
  */
 var sumOddLengthSubarrays = function(arr) {
-    // let result = [];
-    // currentSum = 0;
-    // for(let i = 0; i<nums.length; i++){
-    //     currentSum += nums[i];
-    //     result.push(currentSum);
-    // }
-    // return result;
-    
-    // let prefixSumArr = [arr[0]];
-    // for (let i = 1; i < arr.length; i++) {
-    //     prefixSumArr[i] = prefixSumArr[i - 1] + arr[i]
-    // }
-    // console.log('prefixSumArr', prefixSumArr)
-//     let sum = 0
-//     for (let i = 0; i <= arr.length; i++) {
-//         if(i%2 !=0){
-//              console.log('i', i)
-//             for (let j = 0; j <= arr.length; j++){
-//                 console.log('j', j)
-//             }
+
+    let sum = 0
+    for (let i = 0; i <= arr.length -1 ; i++) {
+  
+             // console.log('i', i)
+            for (let j = arr.length -1; j >=i ; j--){
+                let tmp = (arr.slice(i, j + 1));
+                if (tmp.length % 2 != 0) {
+                    sum += tmp.reduce((a, b) => a + b, 0)
+                }
+            }
         
-//         }
+     
         
-//     }
-     if (arr.length === 1) return arr[0];
-    
-    const prefixSum = new Array(arr.length);
-    
-    prefixSum[0] = arr[0];
-    
-    let sum = 0;
-    for (let i = 1; i < arr.length; i++) {
-        prefixSum[i] = prefixSum[i - 1] + arr[i];
     }
+    console.log('sum', sum)
+    return sum
     
-    if (arr.length % 2 !== 0) {
-        sum += prefixSum[prefixSum.length - 1]
-    }
-    
-    sum += prefixSum[prefixSum.length - 1];
-    
-    for (let k = 3; k < arr.length; k += 2) {
-        sum += prefixSum[k - 1];
-        for (let i = k; i < arr.length; i++) {
-            sum += prefixSum[i];
-            sum -= prefixSum[i - k];
-        }
-    }
-    return sum;
+
 };
