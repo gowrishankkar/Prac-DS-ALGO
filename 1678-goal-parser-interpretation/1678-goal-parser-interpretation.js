57a431c3e98aafcade5803b2738633317730ecbc
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-10 00:00:28 +0530

    Time: 112 ms (10.84%), Space: 41.9 MB (64.03%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -11,10 +11,31 @@ var interpret = function(command) {
     //     if(command[i] == '(' && command[i+1] == 'a' && command[i+2] == 'l'&& command[i+3] == ')') result =  result + 'al';
     // }
     // console.log('result', result)
-    let replacer = (match) => {
-        if(match === '()') return 'o';
-        else if(match === '(al)') return 'al';
-		else return match;
-	};
-	return command.replace(/G|\(\)|\(al\)/g, replacer);
+    
+    
+	// let replacer = (match) => {
+	// if(match === '()') return 'o';
+	// else if(match === '(al)') return 'al';
+	// 	else return match;
+	// };
+	// return command.replace(/G|\(\)|\(al\)/g, replacer);
+    
+    const hash = {
+        "G": "G",
+        "()": "o",
+        "(al)": "al"
+    }
+    
+    let result = "";
+    let chars = "";
+    
+    for (let i = 0; i < command.length; i++) {
+        chars += command[i];
+        if (hash[chars]){
+            result += hash[chars];
+            chars = "";
+        }
+    }
+    
+    return result;
 };
