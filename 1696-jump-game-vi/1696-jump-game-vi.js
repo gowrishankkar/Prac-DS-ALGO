1dc583aeda90195457399327a345cf869cae2089
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-09 20:19:39 +0530

    Time: 398 ms (26.67%), Space: 53.7 MB (66.67%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxResult = function(nums, k) {
+     const n = nums.length;
+    const dp = new Array(n).fill(0);
+    dp[0] = nums[0];
+    
+    const monoDequeue = [];
+    monoDequeue.push(0);
+    
+    for (let i = 1; i < n; i++) {
+        let score = nums[i];
+
+        if (monoDequeue.length > 0 && i - k > monoDequeue[0]) monoDequeue.shift();
+        
+        score += dp[monoDequeue[0]];
+        
+        dp[i] = score;
+        
+        while (monoDequeue.length > 0 && dp[monoDequeue[monoDequeue.length - 1]] < score) {
+            monoDequeue.pop();
+        }
+
+        monoDequeue.push(i);
+    }
+   
+    return dp[n - 1];
+};
