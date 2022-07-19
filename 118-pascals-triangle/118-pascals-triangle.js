e55cab63cedac698a5e56de4f613e4234f467ea6
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-19 22:46:58 +0530

    Time: 76 ms (67.96%), Space: 42.1 MB (36.37%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    var result = [];
+    
+    if(numRows <= 0){
+        return result;
+    }
+    
+    for(var i = 0; i < numRows; i++){
+        var cur = [];
+        var pre = i > 0 ? result[i - 1] : [];
+        
+        for(var j = 0; j <= i; j++){
+            if(j === 0){
+                cur.push(1);    
+            } else if(j === i){
+                cur.push(1);
+            } else {
+                cur.push(pre[j] + pre[j-1]);
+            }
+        }
+        
+        result.push(cur);
+    }
+    
+    return result; 
+};
