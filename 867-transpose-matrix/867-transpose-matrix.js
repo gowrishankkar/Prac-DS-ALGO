e69835a430fc41dd5a4edee3e8a2bf50e97d2799
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-26 23:24:35 +0530

    Time: 89 ms (75.27%), Space: 45.1 MB (12.30%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -3,7 +3,20 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-     let rowLen = matrix.length;
+    //  let rowLen = matrix.length;
+    //     let colLen = matrix[0].length;
+    //     for (let r = 0; r < rowLen; r++) {
+    //         for (let c = r + 1; c < colLen; c++) {
+    //             // console.log(A[c][r])
+    //             let temp = matrix[r][c];
+    //             matrix[r][c] = matrix[c][r];
+    //             matrix[c][r] = temp;
+    //         }
+    //     }
+    // return matrix;
+    
+    
+         let rowLen = matrix.length;
     let result = []
         let colLen = matrix[0].length;
         for (let i = 0; i < rowLen; i++) {
