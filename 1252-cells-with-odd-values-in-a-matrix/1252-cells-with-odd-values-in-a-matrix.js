9514af7d478319e3031ca2b1e91e54eba3761e46
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-30 22:44:47 +0530

    Time: 129 ms (25.00%), Space: 44.3 MB (40.00%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.js
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.js
@@ -5,47 +5,21 @@
  * @return {number}
  */
 var oddCells = function(m, n, indices) {
-    // console.log('m, n, indices', m, n, indices)
-//     let grid = []
-//     for (let i = 0; i < m; i += 1) {
-//         let row = []
-//         for (let j = 0; j < n; j += 1) {
-//             row.push(0)
-           
-//         }
-//         grid.push(row)
-//     }
-
-    
+   
     let grid = Array(m).fill().map(() => Array(n).fill(0));
-
-    
-      for (let i = 0; i < indices.length; i += 1) {
-           let row = indices[i][0];
-        let column =  indices[i][1];
-           // console.log(row, column)
-          
-                for (let j = 0; j < n; j += 1) {
-                    grid[row][j] += 1
-                }
-                 for (let k = 0; k < m; k += 1) {
-                     console.log('k', k)
-                   grid[k][column] += 1
-                }
-
-    }
-    
-    let oddNumbers = 0;
-  for (let i = 0; i < m; i += 1) {
-     
+    for (let i = 0; i < indices.length; i += 1) {
         for (let j = 0; j < n; j += 1) {
-             if(grid[i][j] % 2 != 0) oddNumbers++
-           
+            grid[indices[i][0]][j] += 1
         }
-
+         for (let k = 0; k < m; k += 1) {
+           grid[k][indices[i][1]] += 1
+        }
     }
-    
+    let oddNumbers = 0;
+    for (let i = 0; i < m; i += 1) {
+        for (let j = 0; j < n; j += 1) {
+            if(grid[i][j] % 2 != 0) oddNumbers++
+        }
+    }
     return oddNumbers;
-     // console.log('grid', grid)
-    
 };
