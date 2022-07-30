2e98ddf8d54c2e4b273e85de4284d0fc102ff7ea
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-07-30 22:36:35 +0530

    Time: 605 ms (5.00%), Space: 50.4 MB (5.00%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(m, n, indices) {
+    // console.log('m, n, indices', m, n, indices)
+    let grid = []
+    for (let i = 0; i < m; i += 1) {
+        let row = []
+        for (let j = 0; j < n; j += 1) {
+            row.push(0)
+           
+        }
+        grid.push(row)
+    }
+
+    
+      for (let i = 0; i < indices.length; i += 1) {
+           let row = indices[i][0];
+        let column =  indices[i][1];
+           // console.log(row, column)
+          
+                for (let j = 0; j < n; j += 1) {
+                    grid[row][j] += 1
+                }
+                 for (let k = 0; k < m; k += 1) {
+                     console.log('k', k)
+                   grid[k][column] += 1
+                }
+
+    }
+    
+    let oddNumbers = 0;
+  for (let i = 0; i < m; i += 1) {
+     
+        for (let j = 0; j < n; j += 1) {
+             if(grid[i][j] % 2 != 0) oddNumbers++
+           
+        }
+
+    }
+    
+    return oddNumbers;
+     // console.log('grid', grid)
+    
+};
