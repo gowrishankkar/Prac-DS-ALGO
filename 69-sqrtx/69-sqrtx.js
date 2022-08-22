c5617909239f3218411d162e469dfb1a6cad187d
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-08-22 22:15:16 +0530

    Time: 120 ms (49.38%), Space: 42.8 MB (90.71%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -4,19 +4,29 @@
  */
 var mySqrt = function(x) {
      // return Math.sqrt(x) << 0;
-    if (x === 0) return 0;
+//     if (x === 0) return 0;
     
-    let left = 1, right = x, result = 0;
+//     let left = 1, right = x, result = 0;
     
-    while (left <= right) {
-        const mid = left + Math.floor((right - left) / 2);
+//     while (left <= right) {
+//         const mid = left + Math.floor((right - left) / 2);
         
-        if (mid * mid <= x) {
-            result = mid;
-            left = mid + 1;
-        }
-        else right = mid - 1;
-    }
+//         if (mid * mid <= x) {
+//             result = mid;
+//             left = mid + 1;
+//         }
+//         else right = mid - 1;
+//     }
     
-    return result;
+//     return result;
+    
+    
+    
+    let sqrt= 1;
+    if (x===0) return 0
+    
+    for (let i=1;i*i<=x;i++){
+        sqrt = i
+    }
+    return sqrt;
 };
