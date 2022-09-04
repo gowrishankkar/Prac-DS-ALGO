ea893aa777d9dccfc1f7fda0500b2b1c59f0d37a
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-09-04 11:06:58 +0530

    Time: 175 ms (24.72%), Space: 54.7 MB (58.10%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -4,17 +4,32 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    let maxAvg = Number.NEGATIVE_INFINITY;
-    for(let i = 0; i <= nums.length - k; i++){
-        let currentSum = 0;
-        for (let j = i, ind = 0; ind < k;  j++, ind++){
+//     let maxAvg = Number.NEGATIVE_INFINITY;
+//     for(let i = 0; i <= nums.length - k; i++){
+//         let currentSum = 0;
+//         for (let j = i, ind = 0; ind < k;  j++, ind++){
             
-            // console.log(i, j)
-            currentSum += nums[j]
-        }
-        maxAvg = Math.max(maxAvg , currentSum/k)
-        // console.log('currentSum', currentSum)
+//             // console.log(i, j)
+//             currentSum += nums[j]
+//         }
+//         maxAvg = Math.max(maxAvg , currentSum/k)
+//         // console.log('currentSum', currentSum)
+//     }
+//     // console.log('maxAvg', maxAvg)
+//     return maxAvg;
+    
+    
+    let max = -Infinity;
+    let cur = 0;
+    
+    for(let i=0; i<k; i++) {
+        cur += nums[i];
     }
-    // console.log('maxAvg', maxAvg)
-    return maxAvg;
+    max = cur;
+    for(let i=k; i<nums.length; i++) {
+        cur -= nums[i-k];
+        cur += nums[i];
+        max = Math.max(max, cur);
+    }
+    return max/k;
 };
