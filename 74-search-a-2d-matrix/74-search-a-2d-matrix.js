c6b15362486e707a192fde4eaed6834bf4d2b589
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-09-06 23:26:06 +0530

    Time: 58 ms (97.85%), Space: 42.1 MB (70.81%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,9 +4,15 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    for(let i =0; i<matrix.length; i++){
-        if(matrix[i].includes(target)) return true;
-        
+       let cols = matrix[0].length
+    for(let i = 0; i < matrix.length; i++) {
+    
+        if(target >= matrix[i][0] && target <= matrix[i][cols - 1]) {
+             for(let col = 0; col <= cols/2; col++) {
+            if(matrix[i][col] == target || matrix[i][cols - col] == target) return true
+        }
+        }
     }
-    return false;
+    
+    return false
 };
