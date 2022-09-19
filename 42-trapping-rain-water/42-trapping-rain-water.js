5a3aa162979b968f749afe443261e4fd9ea919e1
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-09-19 19:20:21 +0530

    Time: 90 ms (75.15%), Space: 43.3 MB (50.83%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -2,32 +2,61 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    const arrLength = height.length;
-        let lheight = 0, rheight = 0, water = 0;
-        let leftB = [];
-        let rightB = [];
+// var trap = function(height) {
+//     const arrLength = height.length;
+//         let lheight = 0, rheight = 0, water = 0;
+//         let leftB = [];
+//         let rightB = [];
 
-        for(let i = 0; i < arrLength; i++) {
-            if(lheight > height[i]) {
-                leftB.push(lheight);
-            } else {
-                lheight = height[i];
-                leftB.push(0);
-            }
+//         for(let i = 0; i < arrLength; i++) {
+//             if(lheight > height[i]) {
+//                 leftB.push(lheight);
+//             } else {
+//                 lheight = height[i];
+//                 leftB.push(0);
+//             }
 
-            if(rheight > height[arrLength-i-1]) {
-                rightB.unshift(rheight);
-            } else {
-                rheight = height[arrLength-i-1];
-                rightB.unshift(0);
-            }
-        }
-        for(let i = 0; i < arrLength; i++) {
-            if(leftB[i] && rightB[i]) {
-                water  += Math.min(leftB[i], rightB[i]) - height[i];
-            }
-        }
-        return water;
+//             if(rheight > height[arrLength-i-1]) {
+//                 rightB.unshift(rheight);
+//             } else {
+//                 rheight = height[arrLength-i-1];
+//                 rightB.unshift(0);
+//             }
+//         }
+//         for(let i = 0; i < arrLength; i++) {
+//             if(leftB[i] && rightB[i]) {
+//                 water  += Math.min(leftB[i], rightB[i]) - height[i];
+//             }
+//         }
+//         return water;
     
-};
+// };
+
+
+function trap(height) {
+  if (height == null || height.length === 0) return 0;
+
+  let l = 0;
+  let r = height.length - 1;
+
+  let lMax = 0;
+  let rMax = 0;
+
+  let res = 0;
+
+  while (l < r) {
+    lMax = Math.max(lMax, height[l]);
+    if (height[l] < lMax) {
+      res += lMax - height[l];
+    }
+
+    rMax = Math.max(rMax, height[r]);
+    if (height[r] < rMax) {
+      res += rMax - height[r];
+    }
+
+    height[l] < height[r] ? l++ : r--;
+  }
+
+  return res;
+}
