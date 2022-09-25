374ddcdda0fadf8abe09c336017f42f6556c100d
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-09-25 22:24:48 +0530

    Time: 120 ms (21.01%), Space: 44.4 MB (21.52%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 var findGCD = function(nums) {
-        let min = Math.min(...nums), max = Math.max(...nums), x;
+//         let min = Math.min(...nums), max = Math.max(...nums), x;
     
-    for(i = 1; i <= max; i++){
-        if(min % i == 0 && max % i == 0) x = i;
-    }
-     return x;
+//     for(i = 1; i <= max; i++){
+//         if(min % i == 0 && max % i == 0) x = i;
+//     }
+//      return x;
+    
+     let a = [...nums].sort((a, b) => a - b);
+      let b = [...nums].sort((a, b) => b - a);
+      let c = a[0];
+      let d = b[0];
+
+      for (let i = c; i >= 0; i--) {
+        if (c % i === 0 && d % i === 0) {
+          return i;
+        }
+      }
 };
