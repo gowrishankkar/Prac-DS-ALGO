86b46a8fa59636484442fa98084c5eabd17ab007
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-03 23:06:22 +0530

    Time: 95 ms (61.90%), Space: 43.4 MB (16.57%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,23 +3,50 @@
  * @return {boolean}
  */
 var isValid = function(s, stack = []) {
-        for (const bracket of s.split('')) {/* Time O(N) */
-        const isParenthesis = bracket === '(';
-        if (isParenthesis) stack.push(')');  /* Space O(N) */
+//         for (const bracket of s.split('')) {/* Time O(N) */
+//         const isParenthesis = bracket === '(';
+//         if (isParenthesis) stack.push(')');  /* Space O(N) */
 
-        const isCurlyBrace = bracket === '{';
-        if (isCurlyBrace) stack.push('}');   /* Space O(N) */
+//         const isCurlyBrace = bracket === '{';
+//         if (isCurlyBrace) stack.push('}');   /* Space O(N) */
 
-        const isSquareBracket = bracket === '[';
-        if (isSquareBracket) stack.push(']');/* Space O(N) */
+//         const isSquareBracket = bracket === '[';
+//         if (isSquareBracket) stack.push(']');/* Space O(N) */
 
-        const isOpenBracket = isParenthesis || isCurlyBrace || isSquareBracket;
-        if (isOpenBracket) continue;
+//         const isOpenBracket = isParenthesis || isCurlyBrace || isSquareBracket;
+//         if (isOpenBracket) continue;
 
-        const isEmpty = !stack.length;
-        const isWrongPair = stack.pop() !== bracket;
-        const isInvalid = isEmpty || isWrongPair;
-        if (isInvalid) return false;
+//         const isEmpty = !stack.length;
+//         const isWrongPair = stack.pop() !== bracket;
+//         const isInvalid = isEmpty || isWrongPair;
+//         if (isInvalid) return false;
+//     }
+//     return stack.length === 0;
+    
+
+    
+    const checkCar = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
     }
-    return stack.length === 0;
+    
+    for(let i = 0; i < s.length; i++){
+    if(['(', '{', '['].includes(s[i])){
+          stack.push(s[i])
+        } else {
+            let match = checkCar[s[i]]
+            
+            let c2 = stack.pop()
+            
+            if(c2 !== match)
+                return false
+            
+            
+        }
+    }
+    if(stack.length > 0)
+        return false
+    
+    return true
 };
