4b7941b1371554fa78ed26e37542cb163c8d3135
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-12 09:25:20 +0530

    Time: 138 ms (11.42%), Space: 43.3 MB (79.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,23 +3,39 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    const map = {};
-    let arr = []
-    for (let n of nums) {
-        map[n] = (map[n] || 0) + 1;
+//     const map = {};
+//     let arr = []
+//     for (let n of nums) {
+//         map[n] = (map[n] || 0) + 1;
             
         
-    }
-    console.log('map', map)
+//     }
+//     console.log('map', map)
     
-    for (let n in map) {
-     console.log('n', n)
-            if (map[n] > nums.length / 3){
-            console.log('map[n]', map[n])
-                 arr.push(n);
-        }
+//     for (let n in map) {
+//      console.log('n', n)
+//             if (map[n] > nums.length / 3){
+//             console.log('map[n]', map[n])
+//                  arr.push(n);
+//         }
         
-    }
+//     }
 
-    return arr
+//     return arr
+     let obj = {};
+    
+    for (let i = 0; i < nums.length; i++){
+      if (!obj[nums[i]]){
+          obj[nums[i]] = 0;
+      }
+      obj[nums[i]]++;
+    }
+    
+    let res = [];
+    for (let m in obj){
+      if (obj[m] > nums.length / 3){
+          res.push(parseInt(m));
+      }
+    }
+    return res;
 };
