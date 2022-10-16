3a287e81c0b710757604e2a568fa8e093fa67d63
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-16 22:50:40 +0530

    Time: 110 ms (21.69%), Space: 41.4 MB (98.53%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+//     let low = 0, mid = 0, high = nums.length - 1;
+    
+//     while(mid <= high){
+//         switch(nums[mid]){
+//             case 0:
+//                 [nums[low], nums[mid]] = [nums[mid], nums[low]];
+//                 low++;
+//                 mid++;
+//                 break;
+//             case 1:
+//                 mid++;
+//                 break;
+//             case 2:
+//                 [nums[mid], nums[high]] = [nums[high], nums[mid]];
+//                 high--;
+//                 break;
+//         }
+//     }
+//     return nums
+for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length - i - 1; j++) {
+            if (nums[j] > nums[j+1]) {
+                const tmp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = tmp;
+            }
+        }
+    }
+    
+    return nums;
+};
