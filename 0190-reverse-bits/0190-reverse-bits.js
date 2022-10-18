c3d665622b01ad45deaf51c90f0a7403b84e5863
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-18 22:26:34 +0530

    Time: 79 ms (85.89%), Space: 43.6 MB (90.57%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+     // let result = 0;
+     //    let num = 31;
+     //    for (let i = 0; i <= num; i++) {
+     //        if (n % 2 == 1) {      // if its one means ith bit is 1
+     //            result += Math.pow(2, (num - i)); // eg 1 th bit is set then we will be setting 31-1= 30th bit to 1 in reverse bit
+     //        }
+     //        n = Math.floor(n / 2);
+     //    }
+     //    return result;
+     let r = 0;
+  for (let bit = 0; bit < 32; bit++) {
+    r <<= 1;
+    r |= n & 1;
+    n >>>= 1;
+  }
+  return r >>> 0;
+};
