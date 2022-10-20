14c2da741b4a4d67ef8362dec6e983743d6eee13
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-20 23:42:49 +0530

    Time: 267 ms (5.26%), Space: 46.5 MB (5.55%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-      let chars = {"A": 1,"B": 2,"C": 3,"D": 4,"E": 5,"F": 6,"G": 7,"H": 8,"I": 9,"J": 10,"K": 11,"L": 12,"M": 13,"N": 14,"O": 15,"P": 16,"Q": 17,"R": 18,"S": 19,"T": 20,"U": 21,"V": 22,"W": 23,"X": 24,"Y": 25,"Z": 26}
+//       let chars = {"A": 1,"B": 2,"C": 3,"D": 4,"E": 5,"F": 6,"G": 7,"H": 8,"I": 9,"J": 10,"K": 11,"L": 12,"M": 13,"N": 14,"O": 15,"P": 16,"Q": 17,"R": 18,"S": 19,"T": 20,"U": 21,"V": 22,"W": 23,"X": 24,"Y": 25,"Z": 26}
 
-        if (columnTitle.length === 1) return chars[columnTitle];
+//         if (columnTitle.length === 1) return chars[columnTitle];
 
-        let tot = chars[columnTitle[0]]
-        for (let i = 1; i < columnTitle.length; i++) {
-            tot = (tot * 26) + chars[columnTitle[i]]
-        }
+//         let tot = chars[columnTitle[0]]
+//         for (let i = 1; i < columnTitle.length; i++) {
+//             tot = (tot * 26) + chars[columnTitle[i]]
+//         }
 
-        return tot
+//         return tot
+     let sum = 0;
+  for (let i = 0; i < columnTitle.length; i++) {
+    const char = columnTitle[columnTitle.length - 1 - i];
+      console.log('char', char)
+    sum += (char.charCodeAt() - 64) * 26 ** i;
+  }
+  return sum;
 };
