827a0b99732ad30e62d4f138ebfb1a464689fd7e
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-10-26 20:34:24 +0530

    Time: 72 ms (91.87%), Space: 43.4 MB (21.29%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -2,6 +2,20 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
+// var hammingWeight = function(n) {
+//     return n.toString(2).split("1").length-1;
+// };
 var hammingWeight = function(n) {
-    return n.toString(2).split("1").length-1;
+    
+    //let arr=[]; 
+    let count=0;
+    
+    let a=(n >>> 0).toString(2);          //conversion to binary string
+    
+      //transtfer integer to string and save in array for iteration   (too slow!!)
+        for(let k of a) if(k ==1) count++;                                   
+        // string can also be regarded as array-like object,  a[k] 
+        //charAt(i) obatain the ith character by giving the index of the strin         
+        //push() add a element to the end of array
+        return count;
 };
