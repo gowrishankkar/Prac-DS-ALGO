8b74ce2dde0ac40b4abd4c1cff89850cd3711367
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-11-11 23:29:53 +0530

    Time: 109 ms (20.87%), Space: 42.5 MB (30.32%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -16,19 +16,32 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = (root) => {
-    const isBaseCase = root === null;
-    if (isBaseCase) return root;
+// var invertTree = (root) => {
+//     const isBaseCase = root === null;
+//     if (isBaseCase) return root;
 
-    return dfs(root);
-}
+//     return dfs(root);
+// }
 
-const dfs = (root) => {
-    const left = invertTree(root.left);
-    const right = invertTree(root.right);
+// const dfs = (root) => {
+//     const left = invertTree(root.left);
+//     const right = invertTree(root.right);
 
-    root.left = right;
-    root.right = left;
+//     root.left = right;
+//     root.right = left;
 
+//     return root;
+// }
+
+
+var invertTree = function(root) {
+    if (!root) return null;
+    
+    invertTree(root.left);
+    invertTree(root.right);
+    
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
     return root;
-}
+};
