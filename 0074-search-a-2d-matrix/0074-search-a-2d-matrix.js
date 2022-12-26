8e5e1bbf5c6a9fb4ed3e8d6b7af88ba1a6deae0e
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2022-12-26 09:56:07 +0530

    Time: 63 ms (92.50%), Space: 42.1 MB (56.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,20 +3,39 @@
  * @param {number} target
  * @return {boolean}
  */
+// var searchMatrix = function(matrix, target) {
+//     let m = matrix;
+//     let num = target
+//      var low = 0, high = m.length * m[0].length - 1;
+//         var mid;
+//         while (low <= high) {
+//             mid = Math.floor((high - low) / 2 + low);
+//             var row = Math.floor(mid / m[0].length);
+//             var col = mid % m[0].length;
+//             if (m[row][col] > num)
+//                 high = mid - 1;
+//             else if (m[row][col] < num)
+//                 low = mid + 1;
+//             else return 1;
+//         }
+//         return 0;
+// };
+
 var searchMatrix = function(matrix, target) {
-    let m = matrix;
-    let num = target
-     var low = 0, high = m.length * m[0].length - 1;
-        var mid;
-        while (low <= high) {
-            mid = Math.floor((high - low) / 2 + low);
-            var row = Math.floor(mid / m[0].length);
-            var col = mid % m[0].length;
-            if (m[row][col] > num)
-                high = mid - 1;
-            else if (m[row][col] < num)
-                low = mid + 1;
-            else return 1;
+    let m = 0;
+    let n = matrix[0].length - 1;
+
+    while (m <= matrix.length - 1 && n >= 0) {
+        const curNode = matrix[m][n];
+        
+        if (curNode === target) return true;
+        
+        if (curNode > target) {
+            n--;
+        } else {
+            m++;
         }
-        return 0;
+    }
+
+    return false;
 };
