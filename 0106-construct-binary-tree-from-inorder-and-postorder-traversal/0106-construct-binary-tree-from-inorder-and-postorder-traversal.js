a5f9a208798ee54fee21b4d1431e45a025611a84
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-01-20 20:53:53 +0530

    Time: 149 ms (42.49%), Space: 119.6 MB (27.04%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,30 +12,30 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    // if (postorder.length == 0 || inorder.length == 0) {
-    //     return null;
-    // }
-    // let n = postorder.length - 1
-    // var root = new TreeNode(postorder[n]);
-    // var indexinorder = inorder.indexOf(postorder[n]);
-    // root.left = buildTree(postorder.slice(0, indexinorder), inorder.slice(0, indexinorder));
-    // root.right = buildTree(postorder.slice(indexinorder, postorder.length - 1), inorder.slice(indexinorder + 1, inorder.length));
-    // return root;
+    if (postorder.length == 0 || inorder.length == 0) {
+        return null;
+    }
+    let n = postorder.length - 1
+    var root = new TreeNode(postorder[n]);
+    var indexinorder = inorder.indexOf(postorder[n]);
+    root.left = buildTree(inorder.slice(0, indexinorder), postorder.slice(0, indexinorder));
+    root.right = buildTree(inorder.slice(indexinorder + 1, inorder.length),postorder.slice(indexinorder, postorder.length - 1));
+    return root;
     
     
-      if(inorder.length === 0) return null;
-        let root_val = postorder[postorder.length - 1];
-        let root = new TreeNode(root_val);
-        let i = inorder.indexOf(root_val);
+//       if(inorder.length === 0) return null;
+//         let root_val = postorder[postorder.length - 1];
+//         let root = new TreeNode(root_val);
+//         let i = inorder.indexOf(root_val);
 
-        let L_In = inorder.slice(0, i);
-        let R_In = inorder.slice(i+1, inorder.length);
-        let L_Post = postorder.slice(0 , i);
-        let R_Post = postorder.slice(i, postorder.length - 1)
+//         let L_In = inorder.slice(0, i);
+//         let R_In = inorder.slice(i+1, inorder.length);
+//         let L_Post = postorder.slice(0 , i);
+//         let R_Post = postorder.slice(i, postorder.length - 1)
 
-        root.left = buildTree(L_In, L_Post);
-        root.right = buildTree(R_In, R_Post);
-        return root;
+//         root.left = buildTree(L_In, L_Post);
+//         root.right = buildTree(R_In, R_Post);
+//         return root;
     
 };
 
