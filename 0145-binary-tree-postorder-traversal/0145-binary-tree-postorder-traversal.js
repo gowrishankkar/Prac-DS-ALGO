5e793321932068048c3bb4108011feb523ab9f76
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-01-29 11:27:27 +0530

    Time: 71 ms (45.52%), Space: 42.9 MB (9.32%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -12,14 +12,37 @@
  */
 
 
+// var postorderTraversal = function(root) {
+//     let output = [];
+//     function foo (node) {
+//         if(!node) return;
+//         foo(node.left);
+//         foo(node.right);
+//          output.push(node.val);
+//     }
+//     foo(root);
+//     return output;
+// };
+
+
 var postorderTraversal = function(root) {
-    let output = [];
-    function foo (node) {
-        if(!node) return;
-        foo(node.left);
-        foo(node.right);
-         output.push(node.val);
+   let s1 = []
+   let s2 = [];
+    let result = []
+    if(root ==  null) return []
+    s1.push(root);
+    while(s1.length){
+        let node = s1.pop();
+        s2.push(node.val);
+        if(node.left !== null) s1.push(node.left)
+        if(node.right !== null) s1.push(node.right)
+        
     }
-    foo(root);
-    return output;
+    
+    console.log(s2,'test')
+    
+    while(s2.length){
+        result.push(s2.pop())
+    }
+    return result
 };
