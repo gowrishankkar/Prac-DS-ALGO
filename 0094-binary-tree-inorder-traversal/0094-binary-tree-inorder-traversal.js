f01494430d1063a550a712dad3454fe3d56f4dc1
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-01-29 11:15:46 +0530

    Time: 109 ms (6.13%), Space: 47.5 MB (5.23%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,13 +11,37 @@
  * @return {number[]}
  */
 
- var inorderTraversal = function (root, list = []) {
+//  var inorderTraversal = function (root, list = []) {
 
-    if (!root) return [];
+//     if (!root) return [];
     
-    inorderTraversal(root.left, list);
-    list.push(root.val)
-    inorderTraversal(root.right, list);
+//     inorderTraversal(root.left, list);
+//     list.push(root.val)
+//     inorderTraversal(root.right, list);
     
-    return list
+//     return list
+// };
+
+
+ var inorderTraversal = function (root) {
+     let stack = [];
+     let result = [];
+      if (!root) return [];
+     let node = root;
+     
+     while(stack.length || node != null ){
+         while (node != null){
+             stack.push(node);
+             node = node.left
+         }
+             console.log('stack', stack)
+             let pop = stack.pop();
+             
+             result.push(pop.val);
+             node = pop.right
+         
+     }
+     
+     return result;
+
 };
