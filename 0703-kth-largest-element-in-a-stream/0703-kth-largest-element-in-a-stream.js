4cdca0afab62eaa4b6a11749829935d62c59067f
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-01-31 22:56:31 +0530

    Time: 154 ms (72.60%), Space: 51.6 MB (46.40%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     * @constructor
+     */
+    constructor(k, nums) {
+        this.k = k
+        this.minHeap = new MinPriorityQueue();
+
+        nums.forEach((num) => this.add(num))
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val, { minHeap } = this) {
+        const isUnderCapacity = minHeap.size() < this.k;
+        if (isUnderCapacity) {
+            minHeap.enqueue(val);
+        
+            return this.top();
+        }
+
+        const isLarger = this.top() < val;
+        if (isLarger) {
+            minHeap.dequeue()
+            minHeap.enqueue(val);
+        }
+        
+        return this.top();
+    }
+
+    top ({ minHeap } = this) {
+        return minHeap.front()?.element || 0
+    }
+}
