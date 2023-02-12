196b07d3d27a56fea9d3387c3619994156cad0e9
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-02-12 13:57:05 +0530

    Time: 84 ms (68.82%), Space: 47.2 MB (40.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -22,3 +22,57 @@ const backTrack = (candidates, target, i, combination, combinations) => {
         combinationSum(candidates, (target - candidates[i]), i, combination, combinations);
     combination.pop();
 }
+
+// var combinationSum = function (candidates, target){
+//     function dfs(candidate, target, idx, combination, result){
+//         if(target < 0) return result;
+//         if(target === 0){
+//            return result.push(combination.slice());
+//             }
+//         for (let i = idx; i < candidate.length; i++) {
+//             combination.push(candidate[i])
+            
+//              dfs(candidate, (target - candidate[i]) , idx, combination, result);
+//             combination.pop();
+//         }
+//         return result;
+//     }
+    
+//     let idx = 0;
+//     let result = [];
+//     let combination = [];
+ 
+//     return dfs(candidates, target, idx, combination,result)
+    
+// }
+
+
+
+// var combinationSum = function (candidates, target){
+//     function dfs(candidates, target, index, combination, combinations){
+//         const isBaseCase = target < 0;
+//         if (isBaseCase) return combinations;
+
+//         const isTarget = target === 0;
+//         if (isTarget) return combinations.push(combination.slice());
+
+//         for (let i = index; i < candidates.length; i++) {
+//             backTrack(candidates, target, i, combination, combinations);
+//         }
+
+//         return combinations;
+//     }
+    
+//     const backTrack = (candidates, target, i, combination, combinations) => {
+//         combination.push(candidates[i]);
+//             combinationSum(candidates, (target - candidates[i]), i, combination, combinations);
+//         combination.pop();
+//     }
+    
+//     let idx = 0;
+//     let combinations = [];
+//     let combination = [];
+ 
+//     return dfs(candidates, target, idx, combination,combinations)
+    
+// }
