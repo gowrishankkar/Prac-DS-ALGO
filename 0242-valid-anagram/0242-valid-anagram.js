2e07281964dfc3bdf48e39888a2da512347e7c1a
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-02-27 23:28:23 +0530

    Time: 82 ms (79.79%), Space: 46.2 MB (39.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,14 +3,55 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = (s, t) => {
+// var isAnagram = (s, t) => {
+//     const isEqual = s.length === t.length;
+//     if (!isEqual) return false;
+
+//     return reorder(s) === reorder(t); /* Time O(N * logN) | Space O(N) */
+// };
+
+// const reorder = (str) => str
+//     .split('')                         /* Time O(N)          | Space O(N) */
+//     .sort((a, b) => a.localeCompare(b))/* Time O(N * log(N)) | Space O(1 || log(N)) */
+//     .join('');  
+
+
+var isAnagram = (s, t, map = new Map()) => {
     const isEqual = s.length === t.length;
     if (!isEqual) return false;
 
-    return reorder(s) === reorder(t); /* Time O(N * logN) | Space O(N) */
+    addFrequency(s, map);      /* Time O(N) | Space O(N) */
+    console.log('map1', map)
+    subtractFrequency(t, map); /* Time O(N) | Space O(N) */
+console.log('map2', map)
+    return checkFrequency(map);/* Time O(N) */
 };
 
-const reorder = (str) => str
-    .split('')                         /* Time O(N)          | Space O(N) */
-    .sort((a, b) => a.localeCompare(b))/* Time O(N * log(N)) | Space O(1 || log(N)) */
-    .join('');  
+
+const addFrequency = (str, map) => {
+    for (const char of str) {/* Time O(N) */
+        const count = (map.get(char) || 0) + 1;
+
+        map.set(char, count);   /* Space O(N) */
+    }
+}
+
+
+const subtractFrequency = (str, map) => {
+    for (const char of str) {/* Time O(N) */
+        if (!map.has(char)) continue;
+
+        const count = map.get(char) - 1;
+
+        map.set(char, count);   /* Space O(N) */
+    }
+};
+
+const checkFrequency = (map) => {
+    for (const [ char, count ] of map) {/* Time O(N) */
+        const isEmpty = count === 0;
+        if (!isEmpty) return false;
+    }
+
+    return true;
+}
