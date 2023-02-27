cb2ec8e3088a52f7412826e807c6c9d3cdeea260
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-02-27 23:26:01 +0530

    Time: 230 ms (7.10%), Space: 50.4 MB (5.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = (s, t) => {
+    const isEqual = s.length === t.length;
+    if (!isEqual) return false;
+
+    return reorder(s) === reorder(t); /* Time O(N * logN) | Space O(N) */
+};
+
+const reorder = (str) => str
+    .split('')                         /* Time O(N)          | Space O(N) */
+    .sort((a, b) => a.localeCompare(b))/* Time O(N * log(N)) | Space O(1 || log(N)) */
+    .join('');  
