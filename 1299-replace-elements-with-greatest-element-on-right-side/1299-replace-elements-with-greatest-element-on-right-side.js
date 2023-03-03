a52ac00095907221aa1d148e909f3a43ef503b58
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-03-03 23:33:00 +0530

    Time: 168 ms (95.74%), Space: 50.9 MB (77.04%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -2,13 +2,25 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-var replaceElements = (arr, max = -1, ans = [ -1 ]) => {    
-    arr = arr.reverse();                          /* Time O(N) */
+// var replaceElements = (arr, max = -1, ans = [ -1 ]) => {    
+//     arr = arr.reverse();                          /* Time O(N) */
     
-    for (let i = 0; (i < (arr.length - 1)); i++) {/* Time O(N) */
-        max = Math.max(max, arr[i]);
-        ans[(i + 1)] = max;                           /* Space O(N) */
-    }
+//     for (let i = 0; (i < (arr.length - 1)); i++) {/* Time O(N) */
+//         max = Math.max(max, arr[i]);
+//         ans[(i + 1)] = max;                           /* Space O(N) */
+//     }
     
-    return ans.reverse();                         /* Time O(N) */
+//     return ans.reverse();                         /* Time O(N) */
+// };
+
+
+var replaceElements = (arr, max = -1) => {  
+     for (let i = (arr.length - 1); (0 <= i); i--) {/* Time O(N) */
+         const num = arr[i];
+
+         arr[i] = max;
+         max = Math.max(max, num);
+     }
+
+     return arr;
 };
