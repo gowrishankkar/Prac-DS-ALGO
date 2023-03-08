097646b4e59dbbabd6f36501fd5706d01ac394bd
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-03-08 06:38:50 +0530

    Time: 72 ms (85.66%), Space: 48.2 MB (34.50%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var maxSubarraySumCircular = function(nums) {
+//     let [globalMax, globalMin] = [nums[0], nums[0]];
+//     let [currentMax, currentMin] = [0, 0];
+//     let total = 0;
+    
+//     for(num of nums){
+//        currentMax = Math.max(num, currentMax + num);
+//         currentMin = Math.min(num, currentMin + num);
+//             total += num
+//         globalMax = Math.max(globalMax, currentMax);
+//         globalMin = Math.min(currentMin, globalMin );
+    
+//     }
+    
+//     if(globalMax >0){ return Math.max(globalMax, total - globalMin)}
+//     else {return globalMin;}
+// };
+
+var maxSubarraySumCircular = function (nums) {
+    let [globalMax, globalMin] = [nums[0], nums[0]];
+    let [currentMax, currentMin] = [0, 0];
+    let total = 0;
+
+    for (num of nums) {
+        currentMax = Math.max(num, currentMax + num);
+        currentMin = Math.min(num, currentMin + num);
+        total += num;
+        globalMax = Math.max(globalMax, currentMax);
+        globalMin = Math.min(globalMin, currentMin);
+    }
+
+    return globalMax > 0 ? Math.max(globalMax, total - globalMin) : globalMax;
+};
