63cea839433ce98105a817219cb0ac5fb1547be9
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-03-17 21:20:27 +0530

    Time: 86 ms (97.96%), Space: 49.5 MB (95.65%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -2,19 +2,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {
+// var findDisappearedNumbers = function(nums) {
     
-    const numberSet = new Set();
+//     const numberSet = new Set();
 
-    for(let i = 1; i < nums.length + 1; i++) {
-        numberSet.add(i);
-    }
+//     for(let i = 1; i < nums.length + 1; i++) {
+//         numberSet.add(i);
+//     }
 
-    nums.forEach((element) => {
-        if(numberSet.has(element)) {
-            numberSet.delete(element);
-        }
-    });
+//     nums.forEach((element) => {
+//         if(numberSet.has(element)) {
+//             numberSet.delete(element);
+//         }
+//     });
 
-    return Array.from(numberSet);
+//     return Array.from(numberSet);
+// };
+var findDisappearedNumbers = function(nums) {
+    let res = []
+    nums.forEach((val, ind, arr) => {
+        let tmp = Math.abs(arr[ind]) - 1;
+        if (arr[tmp] > 0)
+            arr[tmp] *= -1;
+    })
+    nums.forEach((val, ind) => {
+        if (val > 0)
+            res.push(ind + 1)
+    })
+    return res
 };
