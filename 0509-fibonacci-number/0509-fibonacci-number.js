642812cedfd238362b4a6f85d781283d0107b18b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-03-21 20:26:52 +0530

    Time: 52 ms (95.13%), Space: 42.1 MB (16.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,12 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, map = {}) {
-    if (n === 0) return n;
-    if (n === 1) return n;
-    if(map[n]) return map[n]
-    let ans = fib(n-1) + fib(n-2)
-    map[n] = ans;
-    return ans
+// var fib = function(n, map = {}) {
+//     if (n === 0) return n;
+//     if (n === 1) return n;
+//     if(map[n]) return map[n]
+//     let ans = fib(n-1) + fib(n-2)
+//     map[n] = ans;
+//     return ans
+    
+// };
+
+ var fib = function memoziation(n,memo={}) {
+    if(n<1) return 0
+    if (n<3)return 1    
+    if(n in memo) return memo[n]
+    memo[n]=memoziation(n-1,memo)+memoziation(n-2,memo)
+    return memo[n]
     
 };
