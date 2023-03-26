a62a84376194f2f356d85aadc9a2e4d121804743
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-03-26 22:49:50 +0530

    Time: 271 ms (55.69%), Space: 72.2 MB (73.05%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -2,28 +2,45 @@
  * @param {string} s
  * @return {number}
  */
+// var longestPalindromeSubseq = function(s) {
+    
+//     return test(s, s.split("").reverse().join(""))
+    
+//     function test (text1, text2) {
+//         if (text1 == "" || text2 == "")
+//             return 0;
+//         // The first row and the first col is an empty string (base case).
+//         // dp[row][col] represents the length of the longest possible subsequence
+//         // using substrings text1.substring(0,row) and text2.substring(0,col).
+//         let dp = new Array(text2.length + 1).fill(0).map(() => new Array(text1.length + 1));
+//         for (let row = 0; row < dp.length; row++) {
+//             for (let col = 0; col < dp[0].length; col++) {
+//                 if (row == 0 || col == 0) {
+//                     dp[row][col] = 0; // can't have a common subsequence with ""
+//                 } else if (text1[col - 1] === text2[row - 1]) {
+//                     dp[row][col] = dp[row - 1][col - 1] + 1;
+//                 } else {
+//                     dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
+//                 }
+//             }
+//         }
+//         return dp[text2.length][text1.length];
+//     }
+// };
+
+
 var longestPalindromeSubseq = function(s) {
-    
-    return test(s, s.split("").reverse().join(""))
-    
-    function test (text1, text2) {
-        if (text1 == "" || text2 == "")
-            return 0;
-        // The first row and the first col is an empty string (base case).
-        // dp[row][col] represents the length of the longest possible subsequence
-        // using substrings text1.substring(0,row) and text2.substring(0,col).
-        let dp = new Array(text2.length + 1).fill(0).map(() => new Array(text1.length + 1));
-        for (let row = 0; row < dp.length; row++) {
-            for (let col = 0; col < dp[0].length; col++) {
-                if (row == 0 || col == 0) {
-                    dp[row][col] = 0; // can't have a common subsequence with ""
-                } else if (text1[col - 1] === text2[row - 1]) {
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
-                } else {
-                    dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
-                }
+    const n = s.length;
+    const dp = Array.from({length: n}, () => new Array(n).fill(0));
+    for (let subLen = 1; subLen <= n; subLen++) {
+        for (let left = 0; left <= n - subLen; left++) {
+            const right = left + subLen - 1;
+            if (subLen == 1) dp[left][right] = 1;
+            else {
+                if (s[left] == s[right]) dp[left][right] = dp[left + 1][right - 1] + 2;
+                else dp[left][right] = Math.max(dp[left + 1][right], dp[left][right - 1]);
             }
         }
-        return dp[text2.length][text1.length];
     }
+    return dp[0][n - 1];
 };
