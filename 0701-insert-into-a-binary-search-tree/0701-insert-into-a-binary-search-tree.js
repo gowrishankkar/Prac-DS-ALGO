afc404adfac8d5f68f57ec9817ac7958cf936a3a
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-05-05 08:57:23 +0530

    Time: 133 ms (5.14%), Space: 53.7 MB (5.62%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -11,23 +11,48 @@
  * @param {number} val
  * @return {TreeNode}
  */
+// var insertIntoBST = function(root, val) {
+//     if(!root) return new TreeNode(val);
+//     let n = root;
+//     while(true){
+//         if(n.val <= val){
+//             if(n.right) n = n.right;
+//             else {
+//                 n.right = new TreeNode(val);
+//                 break;
+//             }
+//         }else {
+//              if(n.left) n = n.left;
+//             else {
+//                 n.left = new TreeNode(val);
+//                 break;
+//             }
+//         }
+//     }
+//     return root
+// };
+
+
 var insertIntoBST = function(root, val) {
-    if(!root) return new TreeNode(val);
-    let n = root;
-    while(true){
-        if(n.val <= val){
-            if(n.right) n = n.right;
-            else {
-                n.right = new TreeNode(val);
-                break;
-            }
-        }else {
-             if(n.left) n = n.left;
-            else {
-                n.left = new TreeNode(val);
-                break;
-            }
-        }
+    if (root === null) {
+        return new TreeNode(val)
     }
+    
+    const dfs = (node, value) => {
+        if (node.val > value) {
+            if (node.left === null) {
+                node.left = new TreeNode(val)
+            } else dfs(node.left, val)
+        } else {
+            if (node.right === null) {
+                node.right = new TreeNode(val)
+            } else dfs(node.right, val)
+        }
+        
+    }
+    
+    dfs(root, val)
+    
     return root
+    
 };
