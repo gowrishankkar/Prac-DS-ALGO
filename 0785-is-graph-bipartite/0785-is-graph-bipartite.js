e096bbdd05145d5160b6ba91dc1be079f8fe69a3
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-05-22 21:43:44 +0530

    Time: 188 ms (5.05%), Space: 50.9 MB (5.05%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} graph
+ * @return {boolean}
+ */
+var isBipartite = function(graph) {
+    let color = new Array(graph.length).fill(-1)
+    
+    
+    const check =(graph, color, start) =>{
+        let q = [];
+        q.push(start)
+        color[start] = 0;
+       
+        while(q.length){
+            let node = q.pop();
+            for(let i of graph[node]){
+                if(color[i] == -1){
+                    
+                color[i] = !color[node];
+                q.push(i)
+                    console.log('color', color)
+                }
+                else if(color[i] === color[node]) {
+                    console.log('color', color)
+                    return false}
+            }
+        }
+        return true
+    }
+    for(let i = 0; i <graph.length ; i++){
+        if(color[i] === -1){
+            if(check(graph, color, i) === false) return false
+        } 
+    }
+    return true
+    // console.log('color', color)
+    
+};
