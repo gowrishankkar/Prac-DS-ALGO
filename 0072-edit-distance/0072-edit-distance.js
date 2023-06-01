9d7653254f5de2847065e7bba7fd6285ea7598ba
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-06-01 23:09:42 +0530

    Time: 92 ms (52.50%), Space: 45.6 MB (94.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -3,36 +3,42 @@
  * @param {string} word2
  * @return {number}
  */
- var minDistance = (word1, word2, i = 0, j = 0, memo = initMemo(word1, word2)) => {
-    const isBaseCase1 = ((word1.length * word2.length) === 0);
-    if (isBaseCase1) return (word1.length + word2.length);
+ var minDistance = (word1, word2) => {
+    const tabu = initTabu(word2);/* Time O(M) | Space O(M) */
 
-    const isBaseCase2 = (word1.length === i);
-    if (isBaseCase2) return (word2.length - j);
+    search(word1, word2, tabu);  /* Time O(N * M) | Space O(M) */
 
-    const isBaseCase3 = (word2.length === j);
-    if (isBaseCase3) return (word1.length - i);
-
-    const hasSeen = (memo[i][j] !== -1);
-    if (hasSeen) return memo[i][j];
-
-    return dfs(word1, word2, i, j, memo);/* Time O(N * M) | Space O((N * M) + HEIGHT) */
+    return tabu[word2.length];
 }
 
-var initMemo = (word1, word2) =>  new Array(word1.length).fill()/* Time O(N) | Space O(N) */
-    .map(() => new Array(word2.length).fill(-1));                   /* Time O(N) | Space O(N) */
+var initTabu = (word2) => {
+    const tabu = new Array((word2.length + 1)).fill(0);/* Time O(M) | Space O(M) */
 
-var dfs = (word1, word2, i, j, memo) => {
-    const isEqual = (word1[i] === word2[j]);
-    if (isEqual) {
-        memo[i][j] = minDistance(word1, word2, (i + 1), (j + 1), memo);/* Time O(N * M) | Space O(HEIGHT) */
-        return memo[i][j];
+    for (let j = 1; (j <= word2.length); j++) {        /* Time O(M) */
+        tabu[j] = j;                                   /*           | Space O(M) */
     }
 
-    const insert = minDistance(word1, word2, i, (j + 1), memo);        /* Time O(N * M) | Space O(HEIGHT) */
-    const _delete = minDistance(word1, word2, (i + 1), j, memo);       /* Time O(N * M) | Space O(HEIGHT) */
-    const replace = minDistance(word1, word2, (i + 1), (j + 1), memo); /* Time O(N * M) | Space O(HEIGHT) */
+    return tabu;
+}
 
-    memo[i][j] = (Math.min(insert, _delete, replace) + 1);             /*               | Space O(N * M) */
-    return memo[i][j];
+var search = (word1, word2, tabu) => {
+    for (let i = 1; (i <= word1.length); i++) {/* Time O(N) */
+        tabu[word2.length] = update(word1, word2, i, tabu);/* Time O(M) | Space (M) */
+    }
+}
+
+const update = (word1, word2, i, tabu) => {
+    let temp = i;
+
+    for (let j = 1; (j <= word2.length); ++j) {/* Time O(M  */
+        const isEqual = (word1[(i - 1)] === word2[(j - 1)])
+        const cur = isEqual
+            ? tabu[(j - 1)]
+            : (Math.min(tabu[(j - 1)], tabu[j], temp) + 1);
+
+        tabu[(j - 1)] = temp;                      /* Space (M) */
+        temp = cur;
+    }
+
+    return temp;
 }
