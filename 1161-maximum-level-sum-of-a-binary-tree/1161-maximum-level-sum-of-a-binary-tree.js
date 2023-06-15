20e460314a8f456268d79b8a063b23697b4a76d6
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-06-15 08:35:30 +0530

    Time: 169 ms (42.44%), Space: 75.8 MB (29.76%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -10,30 +10,58 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxLevelSum = function(root) {
-    const isBaseCase = root === null;
-    if (isBaseCase) return [];
-    return bfs([ root ]) +1;
-};
+// var maxLevelSum = function(root) {
+//     const isBaseCase = root === null;
+//     if (isBaseCase) return [];
+//     return bfs([ root ]) +1;
+// };
 
-const bfs = (queue, levels = []) => {
-    let arr = []
-    while (queue.length) {
-        const level = [];
-        let sum = 0;
-        for (let i = (queue.length - 1); 0 <= i; i--) {
-            const node = queue.shift();
+// const bfs = (queue, levels = []) => {
+//     let arr = []
+//     while (queue.length) {
+//         const level = [];
+//         let sum = 0;
+//         for (let i = (queue.length - 1); 0 <= i; i--) {
+//             const node = queue.shift();
 
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
+//             if (node.left) queue.push(node.left);
+//             if (node.right) queue.push(node.right);
  
-            level.push(node.val);
-            sum += node.val
+//             level.push(node.val);
+//             sum += node.val
            
+//         }
+//         levels.push(level);
+//         arr.push(sum)
+//     }
+    
+//     return arr.indexOf(Math.max(...arr)); ;
+// }
+
+var maxLevelSum = function(root) {
+    if(root === null) return -1;
+    const queue = new Queue([root]);
+    let max = -Infinity;
+    let maxLvl = -1;
+    let level = 1;
+    
+    while(queue.size() > 0) {
+        const len = queue.size();
+        let sum = 0;
+        
+        for(let i = 0; i < len; i++) {
+            const current = queue.dequeue();
+            sum += current.val;
+            if(current.left) queue.enqueue(current.left);
+            if(current.right) queue.enqueue(current.right);
         }
-        levels.push(level);
-        arr.push(sum)
+        
+        if(sum > max) {
+            max = sum;
+            maxLvl = level;
+        }
+        level++;
     }
     
-    return arr.indexOf(Math.max(...arr)); ;
-}
+    return maxLvl;
+};
