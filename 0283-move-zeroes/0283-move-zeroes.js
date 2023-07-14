14f0f179ef9b8cd504b803636b82fa4919739ce7
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-07-15 00:00:11 +0530

    Time: 101 ms (27.02%), Space: 46.8 MB (36.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,42 +1,13 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-var moveZeroes = function(nums) {
+var moveZeroes = (nums) => {
+    let [ left, right ] = [ 0, 0 ];
 
-//     let [ left, right ] = [ 0, 0 ];
-
-//     while (right < nums.length) {
-//         const canSwap = (nums[right] !== 0)
-//         if (canSwap) {
-//             [nums[left], nums[right]] = [nums[right], nums[left]];
-//             left++;
-//         }
-
-//         right++;
-//     }
-    
-//      let start = 0, end = 0
-//      while(end < nums.length){
-//        if(nums[end]==0){
-//            end++
-//        }
-//        else{
-//            let temp = nums[start]
-//            nums[start] = nums[end]
-//            nums[end] = temp
-//            start++
-//            end++
-//        }
-//    }
-    
-    var pointer =0, temp;
-    for(var i=0;i<nums.length;i++) {
-        if(nums[i] !== 0) {
-            temp = nums[pointer];
-            nums[pointer] = nums[i];
-            nums[i] = temp;
-            pointer++;
+    while (right < nums.length) {
+        const canSwap = (nums[right] !== 0)
+        if (canSwap) {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
         }
+
+        right++;
     }
 };
