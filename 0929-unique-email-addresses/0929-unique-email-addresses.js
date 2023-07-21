a3819307de6a8c3ef2c6db4f552cce0e114d6ae2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-07-21 22:54:28 +0530

    Time: 81 ms (45.11%), Space: 46.4 MB (79.15%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function(emails) {
+  const valid=emails.map(email=>{
+       const [local, domain] = email.split('@');
+       return local.split('+').shift().split('.').join('')+'@'+domain;
+   });
+   const set=new Set(valid)
+    
+    return set.size
+      
+    
+};
