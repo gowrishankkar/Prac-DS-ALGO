57ac4b56b0c898455bff9d0915d9b2cbc792da51
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-08-14 11:38:26 +0530

    Time: 51 ms (95.80%), Space: 44.6 MB (62.42%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let res ;
-    for(let i = 0; i < nums.length; i++){
-        res = res ^ nums[i];
-    }
+    let res;
+    for(let num of nums) res ^= num;
     return res;
+    // for(let i = 0; i < nums.length; i++){
+    //     res = res ^ nums[i];
+    // }
+    // return res;
 };
