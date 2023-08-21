cac8f96e3c06b66df7fe345b442f9fdcb76d4cdb
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-08-21 23:16:38 +0530

    Time: 720 ms (47.06%), Space: 42.7 MB (66.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -3,20 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var findTargetSumWays = function(nums, target, index = 0, sum = 0) {
+ var findTargetSumWays = (nums, target, index = 0, sum = 0) => {
     const isBaseCase = (index === nums.length);
-     if (isBaseCase) {
+    if (isBaseCase) {
         const isTarget = (sum === target);
         if (isTarget) return 1;
 
         return 0;
     }
-    return dfs(nums, target, index, sum);/* Time O(2^N) | Space O(HEIGHT) */
 
-};
+    return dfs(nums, target, index, sum);/* Time O(2^N) | Space O(HEIGHT) */
+}
 
 var dfs = (nums, target, index, sum) => {
-     const left = findTargetSumWays(nums, target, (index + 1), (sum + nums[index]));
-    const right = findTargetSumWays(nums, target, (index + 1), (sum - nums[index]));
+    const left = findTargetSumWays(nums, target, (index + 1), (sum + nums[index])); /* Time O(2^N) | Space O(HEIGHT) */
+    const right = findTargetSumWays(nums, target, (index + 1), (sum - nums[index]));/* Time O(2^N) | Space O(HEIGHT) */
+
     return (left + right);
 }
