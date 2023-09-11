81ebe0a7af658d6840c96071b9c2cf9bdb7e0516
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-12 00:10:00 +0530

    Time: 79 ms (45.95%), Space: 46.3 MB (50.65%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+var shipWithinDays = function(weights, days) {
+    let l =  Math.max(...weights);
+    let h = 0;
+    weights.forEach((ele) => h += ele);
+    console.log(l, h)
+    
+    while(l <= h) {
+        let mid = l + Math.floor((h - l) / 2); 
+        let noD =  filter(weights, mid);
+        if(noD <= days) h = mid - 1;
+        else l = mid + 1;
+    }
+    return l;
+};
+
+var filter = (w, m) => {
+    let load = 0, days = 1;
+    for(let i = 0; i < w.length ; i++){
+        if(load + w[i] > m ){
+            days++;
+            load = w[i];
+        }else load += w[i]
+    }
+    return days;
+}
