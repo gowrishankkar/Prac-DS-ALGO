b27537f4d8ad79dd23431d345b42f3ed54b6a2ec
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-12 23:26:34 +0530

    Time: 55 ms (60.82%), Space: 42.3 MB (57.58%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -3,32 +3,32 @@
  * @param {number} k
  * @return {number}
  */
-// var findKthPositive = function(arr, k) {
-//     let l = 0, h = arr.length - 1;
-//     while(l <= h){
-//         let mid = l + Math.floor((h - l)/2);
-//         let missing = arr[mid] - (mid + 1);
-//         if(missing < k){ l = mid + 1;}
-//         else h = mid - 1;
-//     }
-//     return k + h + 1;
-// };
-
 var findKthPositive = function(arr, k) {
-    if (!arr || arr.length < 1) {
-        return k;
+    let l = 0, h = arr.length - 1;
+    while(l <= h){
+        let mid = l + Math.floor((h - l)/2);
+        let missing = arr[mid] - (mid + 1);
+        if(missing < k){ l = mid + 1;}
+        else h = mid - 1;
     }
-
-    let l = 0, r = arr.length - 1;
-    while (l <= r) {
-        const mid = l + Math.floor((r - l)/2);
-        const target = mid + 1;
-        if (arr[mid] - target >= k) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
-    }
-
     return l + k;
 };
+
+// var findKthPositive = function(arr, k) {
+//     if (!arr || arr.length < 1) {
+//         return k;
+//     }
+
+//     let l = 0, r = arr.length - 1;
+//     while (l <= r) {
+//         const mid = l + Math.floor((r - l)/2);
+//         const target = mid + 1;
+//         if (arr[mid] - target >= k) {
+//             r = mid - 1;
+//         } else {
+//             l = mid + 1;
+//         }
+//     }
+
+//     return l + k;
+// };
