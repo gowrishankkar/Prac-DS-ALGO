ed248d2cc085e514b68294f4c22ee7273e62c7a2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-17 18:04:47 +0530

    Time: 1158 ms (5.01%), Space: 80 MB (5.01%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence252-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div680-valid-palindrome-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence252-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div680-valid-palindrome-ii.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence252-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div680-valid-palindrome-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence252-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div680-valid-palindrome-ii.js
@@ -2,49 +2,49 @@
  * @param {string} s
  * @return {boolean}
  */
-// var validPalindrome = function (s) {
-//     let l = 0;
-//     let r = s.length - 1;
+var validPalindrome = function (s) {
+    let l = 0;
+    let r = s.length - 1;
 
-//     while (l < r) {
-//         console.log(l, r);
-//         if (s[l] !== s[r]) {
-//             const skipL = s.slice(l + 1, r + 1);
-//             console.log('skipL', skipL)
-//             const skipR = s.slice(l, r);
-//              console.log('skipR', skipR)
-//             return isPalindrome(skipL) || isPalindrome(skipR);
-//         }
-//         l++;
-//         r--;
-//     }
-//     return true;
-// };
-// const isPalindrome = (s) => {
-//     let l = 0;
-//     let r = s.length - 1;
-
-//     while (l < r) {
-//         if (s[l] !== s[r]) {
-//             return false;
-//         }
-//         l++;
-//         r--;
-//     }
-//     return true;
-// };
-
-var validPalindrome = function(s) {
-    if(checkPalindrome(s)) return true;
-    for(let i=0;i<s.length/2;i++){
-        if(s.charAt(i)!=s.charAt(s.length-1-i))
-            return checkPalindrome(s.substring(0,i)+s.substring(i+1)) || 
-                checkPalindrome(s.substring(0,s.length-1-i)+s.substring(s.length-i));
-    }
-};
-var checkPalindrome = function(s){
-    for(let i=0;i<s.length/2;i++){
-        if(s.charAt(i)!=s.charAt(s.length-1-i)) return false;
+    while (l < r) {
+        console.log(l, r);
+        if (s[l] !== s[r]) {
+            const skipL = s.slice(l + 1, r + 1);
+            console.log('skipL', skipL)
+            const skipR = s.slice(l, r);
+             console.log('skipR', skipR)
+            return isPalindrome(skipL) || isPalindrome(skipR);
+        }
+        l++;
+        r--;
     }
     return true;
-}
+};
+const isPalindrome = (s) => {
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        if (s[l] !== s[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+};
+
+// var validPalindrome = function(s) {
+//     if(checkPalindrome(s)) return true;
+//     for(let i=0;i<s.length/2;i++){
+//         if(s.charAt(i)!=s.charAt(s.length-1-i))
+//             return checkPalindrome(s.substring(0,i)+s.substring(i+1)) || 
+//                 checkPalindrome(s.substring(0,s.length-1-i)+s.substring(s.length-i));
+//     }
+// };
+// var checkPalindrome = function(s){
+//     for(let i=0;i<s.length/2;i++){
+//         if(s.charAt(i)!=s.charAt(s.length-1-i)) return false;
+//     }
+//     return true;
+// }
