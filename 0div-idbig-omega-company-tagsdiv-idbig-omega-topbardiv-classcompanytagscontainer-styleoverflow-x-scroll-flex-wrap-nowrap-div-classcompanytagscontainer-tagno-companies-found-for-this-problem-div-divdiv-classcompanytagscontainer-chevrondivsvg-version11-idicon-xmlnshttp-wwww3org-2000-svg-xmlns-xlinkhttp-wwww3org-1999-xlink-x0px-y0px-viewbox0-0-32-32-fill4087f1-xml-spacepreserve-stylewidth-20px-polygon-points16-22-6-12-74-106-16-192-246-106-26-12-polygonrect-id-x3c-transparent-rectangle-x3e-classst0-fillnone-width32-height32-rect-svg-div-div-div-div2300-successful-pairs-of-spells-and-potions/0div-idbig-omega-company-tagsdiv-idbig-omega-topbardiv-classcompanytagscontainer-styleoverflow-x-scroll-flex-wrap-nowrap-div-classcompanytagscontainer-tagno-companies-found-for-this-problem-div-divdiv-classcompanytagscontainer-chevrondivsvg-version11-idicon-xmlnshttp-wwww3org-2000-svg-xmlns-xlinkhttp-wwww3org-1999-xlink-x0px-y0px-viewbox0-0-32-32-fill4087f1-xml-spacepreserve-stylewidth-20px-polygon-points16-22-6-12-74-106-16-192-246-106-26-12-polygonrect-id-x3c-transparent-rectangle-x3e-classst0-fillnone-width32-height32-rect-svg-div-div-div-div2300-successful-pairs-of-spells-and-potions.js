35687a82588cc4dd400f9824db9d4b948aa0709b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-18 22:46:41 +0530

    Time: 262 ms (38.53%), Space: 77.7 MB (18.82%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions.js
@@ -16,61 +16,27 @@
 //     return res;
 // };
 
-// var successfulPairs = function(spells, potions, success) {
-//     let res = [];
-//     let sp = potions.sort((a,b) => a-b);
-//     const test = (x) =>{
-//         let l = sp[0], r = sp.length - 1;
-//         const t = Math.ceil(success/x);
-//         while(l <= r){
-//             let mid = Math.floor((l+r)/2);
-//             if(sp[mid] == t){
-//                 if(l == r) return l
-//                 r = mid 
-//             }
-//             else if(sp[mid] > t) {
-//                 r = mid - 1;
-//             } else  l = mid + 1
-//         }
-//         return l;
-        
-//     }
-    
-        
-//     for(let i = 0;i < spells.length; i++){
-//         res.push(potions.length - test(spells[i]))
-//     }
-    
-
-//     return res;
-// };
-
 var successfulPairs = function(spells, potions, success) {
-    const sortedPotions = potions.sort((a, b) => a-b);
-    const pl = potions.length;
-    function getSuccessIndex(s) {
-        const r = Math.ceil(success/s);
-        // binary search
-        let i = 0, j = pl-1;
-        while (i <= j) {
-            const m = Math.floor((i+j)/2);
-            if (sortedPotions[m] == r) {
-                if (i == j) {
-                    return i;
-                }
-                j = m;
-            } else if (sortedPotions[m] > r) {
-                j = m-1;
-            } else {
-                i = m+1;
+    let res = [];
+    let sp = potions.sort((a,b) => a-b);
+    const test = (x) =>{
+        let l = 0, r = sp.length - 1;
+        const t = Math.ceil(success/x);
+        while(l <= r){
+            let mid = Math.floor((l+r)/2);
+            if(sp[mid] == t){
+                if(l == r) return l
+                r = mid 
             }
+            else if(sp[mid] > t) {
+                r = mid - 1;
+            } else  l = mid + 1
         }
-        return i;
+        return l;
     }
-
-    const re = [];
-    for (const s of spells) {
-        re.push(pl - getSuccessIndex(s));
+    for(let i = 0;i < spells.length; i++){
+        res.push(potions.length - test(spells[i]))
     }
-    return re;
+    return res;
 };
+
