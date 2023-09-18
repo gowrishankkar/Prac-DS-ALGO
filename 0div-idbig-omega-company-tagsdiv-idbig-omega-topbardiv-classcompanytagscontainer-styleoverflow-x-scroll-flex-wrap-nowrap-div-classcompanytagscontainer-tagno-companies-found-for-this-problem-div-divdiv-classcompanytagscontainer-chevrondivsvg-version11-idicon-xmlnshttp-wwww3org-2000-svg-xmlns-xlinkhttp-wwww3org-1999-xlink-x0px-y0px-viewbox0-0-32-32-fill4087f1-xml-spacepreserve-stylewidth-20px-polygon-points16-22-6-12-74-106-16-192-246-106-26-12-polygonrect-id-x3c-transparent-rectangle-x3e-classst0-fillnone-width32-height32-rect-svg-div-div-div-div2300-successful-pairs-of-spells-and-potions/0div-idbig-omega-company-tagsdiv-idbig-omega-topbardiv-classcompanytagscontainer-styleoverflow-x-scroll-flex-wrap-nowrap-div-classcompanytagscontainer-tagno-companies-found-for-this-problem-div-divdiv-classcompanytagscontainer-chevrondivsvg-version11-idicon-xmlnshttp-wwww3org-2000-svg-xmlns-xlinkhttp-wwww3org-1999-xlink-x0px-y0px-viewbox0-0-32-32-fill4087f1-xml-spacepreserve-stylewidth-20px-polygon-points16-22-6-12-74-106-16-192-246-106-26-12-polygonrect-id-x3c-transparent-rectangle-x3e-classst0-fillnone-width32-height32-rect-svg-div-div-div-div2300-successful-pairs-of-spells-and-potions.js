97450a2e9aafc8c867e66a07916615eb2fbac94d
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-18 22:47:48 +0530

    Time: 258 ms (40.59%), Space: 71.2 MB (79.12%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2300-successful-pairs-of-spells-and-potions.js
@@ -16,19 +16,44 @@
 //     return res;
 // };
 
+// var successfulPairs = function(spells, potions, success) {
+//     let res = [];
+//     let sp = potions.sort((a,b) => a-b);
+//     const test = (x) =>{
+//         let l = 0, r = sp.length - 1;
+//         const t = Math.ceil(success/x);
+//         while(l <= r){
+//             let mid = Math.floor((l+r)/2);
+//             if(sp[mid] == t){
+//                 if(l == r) return l
+//                 r = mid 
+//             }
+//             else if(sp[mid] > t) {
+//                 r = mid - 1;
+//             } else  l = mid + 1
+//         }
+//         return l;
+//     }
+//     for(let i = 0;i < spells.length; i++){
+//         res.push(potions.length - test(spells[i]))
+//     }
+//     return res;
+// };
+
+
 var successfulPairs = function(spells, potions, success) {
     let res = [];
     let sp = potions.sort((a,b) => a-b);
     const test = (x) =>{
         let l = 0, r = sp.length - 1;
-        const t = Math.ceil(success/x);
+     
         while(l <= r){
             let mid = Math.floor((l+r)/2);
-            if(sp[mid] == t){
+            if(sp[mid] * x == success){
                 if(l == r) return l
                 r = mid 
             }
-            else if(sp[mid] > t) {
+            else if(sp[mid] * x > success) {
                 r = mid - 1;
             } else  l = mid + 1
         }
