9bb97988d7e2dd666efa903aef81fb0593f7a60b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-21 09:05:01 +0530

    Time: 67 ms (50.97%), Space: 44 MB (24.32%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div69-sqrtx/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div69-sqrtx.js
@@ -0,0 +1,84 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// var mySqrt = function(x) {
+//     if (x <= 1) {
+//         return x;
+//     }
+
+//     if (x <= 3) {
+//         return 1;
+//     }
+
+//     let min = 2;
+//     let max = Math.floor(x / 2);
+
+//     if (min === max) {
+//         return min;
+//     }
+
+//     let currentValue = min + Math.round((max - min) / 2);
+
+//     while (currentValue * currentValue !== x && max - min !== 1) {
+//         if (currentValue * currentValue > x) {
+//             max = currentValue;
+//             currentValue = min + Math.round((max - min) / 2);
+//             continue;
+//         }
+
+//         if (currentValue * currentValue < x) {
+//             min = currentValue;
+//             currentValue = min + Math.round((max - min) / 2);
+//             continue;
+//         }
+//     }
+
+//     if (currentValue * currentValue > x) {
+//         return min;
+//     }
+
+//     return currentValue;
+// };
+
+
+
+// var mySqrt = function(x) {
+//     if(x < 2) return x;
+//     let l = 0, r = x;
+//     let res = 0;
+//     while(l <= r){
+//         let mid = l + Math.floor((l + r) / 2);
+//         let power = mid * mid;
+//         if(power < x) {
+//             l = mid + 1;
+//             res = mid;
+//         }
+//         else if(power > x) {
+//             r = mid - 1
+            
+//         }
+//         else if(power == x) return mid;
+//     }
+//     return res;
+ 
+// };
+
+const mySqrt = (t) => {
+    if(t < 2) return t;
+    let s = 0, e = t;
+    let ans = -1;
+    while(s <= e) {
+        const m = Math.floor((s+e) / 2);
+        if(m * m === t) {
+            return m;
+        } else if(m * m > t) {
+            e = m - 1;
+        } else {
+            s = m + 1;
+            ans = m;
+        }
+    }
+
+    return ans;
+};
