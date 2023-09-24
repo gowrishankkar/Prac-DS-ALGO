f0f9e26f5033cce90593ef4218d104c119098345
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-24 12:58:45 +0530

    Time: 49 ms (75.49%), Space: 42.1 MB (82.99%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,23 +7,48 @@
 //     return [nums.indexOf(target), nums.lastIndexOf(target)]
 // };
 
+// var searchRange = function(nums, target) {
+//     let l = 0, r = nums.length - 1;
+//     let res = 0, contains = false;
+//     while(l <= r){
+//         let m = l + Math.floor((r - l) / 2);
+//         if(target == nums[m]){
+//             contains = true;
+//             res = m;
+//         } 
+//         if(target < nums[m]){
+//             r = m - 1;
+//         } else l = m + 1;
+//     }
+//     if(contains){
+//         let i = res, j = res;
+//         while(nums[i - 1] == target) i--;
+//         while(nums[j + 1] == target) j++;
+//         return [i, j]
+//     }   return [-1, -1] 
+// };
+
+
 var searchRange = function(nums, target) {
-    let l = 0, r = nums.length - 1;
-    let res = 0, contains = false;
-    while(l <= r){
-        let m = l + Math.floor((r - l) / 2);
-        if(target == nums[m]){
-            contains = true;
-            res = m;
-        } 
-        if(target < nums[m]){
-            r = m - 1;
-        } else l = m + 1;
-    }
-    if(contains){
-        let i = res, j = res;
-        while(nums[i - 1] == target) i--;
-        while(nums[j + 1] == target) j++;
-        return [i, j]
-    }   return [-1, -1] 
+    return [boundearyBinSearch(nums, target, true), boundearyBinSearch(nums, target)]
 };
+
+function boundearyBinSearch(nums, target, checkLeft = false) {
+    let l = 0;
+    let r = nums.length - 1;
+    let res = -1;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if (nums[mid] < target) {
+            l = mid + 1;
+        } else if(nums[mid] > target){
+            r = mid - 1
+        } else {
+            res = mid
+            if(checkLeft) r = mid - 1;
+            else l = mid + 1;
+        }
+    }
+
+    return res;
+}
