a1f1221e962688f42963d1cfc5b27a622537f64d
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-24 12:52:10 +0530

    Time: 55 ms (40.99%), Space: 44.3 MB (5.22%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// var searchRange = function(nums, target) {
+//     return [nums.indexOf(target), nums.lastIndexOf(target)]
+// };
+
+var searchRange = function(nums, target) {
+    let l = 0, r = nums.length - 1;
+    let res = 0, contains = false;
+    while(l <= r){
+        let m = l + Math.floor((r - l) / 2);
+        console.log('m', m)
+        if(target == nums[m]){
+            contains = true;
+            res = m;
+        } 
+        if(target < nums[m]){
+            r = m - 1;
+        } else l = m + 1;
+    }
+    
+    console.log('contains', contains, res)
+    if(contains){
+        let i = res, j = res;
+        while(nums[i - 1] == target) i--;
+        while(nums[j + 1] == target) j++;
+        return [i, j]
+    }   return [-1, -1] 
+};
