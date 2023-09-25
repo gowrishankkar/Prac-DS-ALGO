f44d59d9d1ab0218ca20be1f24102f78eb284fd4
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-26 00:52:07 +0530

    Time: 63 ms (80.08%), Space: 49.1 MB (30.82%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div116-populating-next-right-pointers-in-each-node/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div116-populating-next-right-pointers-in-each-node.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div116-populating-next-right-pointers-in-each-node/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div116-populating-next-right-pointers-in-each-node.js
@@ -12,21 +12,40 @@
  * @param {Node} root
  * @return {Node}
  */
-var connect = function(root) {
-    if (root == null) return null;
-    const queue = [root];
-    while (queue.length > 0) {
-        let size = queue.length;
-        while (size > 0){
-            let top = queue.shift();
-            if (size > 1){
-                top.next = queue[0];
-            }
+// var connect = function(root) {
+//     if (root == null) return null;
+//     const queue = [root];
+//     while (queue.length > 0) {
+//         let size = queue.length;
+//         while (size > 0){
+//             let top = queue.shift();
+//             if (size > 1){
+//                 top.next = queue[0];
+//             }
             
-            if (top.left != null) queue.push(top.left);
-            if (top.right != null) queue.push(top.right);
-            size -= 1
+//             if (top.left != null) queue.push(top.left);
+//             if (top.right != null) queue.push(top.right);
+//             size -= 1
+//         }
+//     }
+//     return root
+// };
+
+
+var connect = function(root) {
+    if (root == null) return root;
+    let queue = [root];
+    while(queue.length!=0) {
+        let next = [];
+        while(queue.length!=0) {
+            let node = queue.shift();
+            node.next = queue[0]||null;
+            if (node.left!=null) {
+                next.push(node.left);
+                next.push(node.right);
+            }
         }
+        queue = next;
     }
-    return root
+    return root;
 };
