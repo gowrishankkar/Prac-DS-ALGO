caad23a54ac0bc9e354a4c89cd4a7be802c1dfef
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-25 23:55:29 +0530

    Time: 647 ms (37.84%), Space: 131.7 MB (5.41%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters.js
@@ -4,68 +4,33 @@
  * @param {number[]} removable
  * @return {number}
  */
-// var maximumRemovals = function(s, p, removable) {
-//     let res = 0, l = 0, r = removable.length - 1;
-//     while(l <= r){
-//         let mid = l + Math.floor((r - l) / 2);
-//         if(isSubseq(s, p, removable.slice(0, mid + 1))){
-//             l = mid + 1;
-//             res = Math.max(res, mid + 1);
-//         }
-//         else r = mid - 1
-//     }
-//     console.log('res', res)
-//     return res;
-// };
-
-// var isSubseq = (str, p, arr) =>{
-//     let i = 0, j = 0;
-//     while( i < str.length && j < arr.length){
-//         if(arr.includes(i) || str[i] != arr[j]) {
-//             i++;
-//             continue;
-//         }
-//         i++;
-//         j++;
-//     }
-//     console.log('arr', arr, j === arr.length, j, i)
-//      // return true
-//     return j == arr.length;
-// }
-
-
-
 var maximumRemovals = function(s, p, removable) {
-    function isSubsequence(x){
-        let i = 0;
-        let j = 0;
-
-        while (i < x.length) {
-            if (x[i] === p[j]){
-                j++;
-                if (j === p.length) return true;
+    let res = 0, l = 0, r = removable.length - 1;
+    let removedSet = new Set();
+    while(l <= r){
+        let mid = l + Math.floor((r - l) / 2);
+          removedSet.clear();
+            for( let i=0; i <= mid; i++ ) {
+                removedSet.add(removable[i]);
             }
-            i++;
+        if(isSubseq(s, p, removedSet)){
+            l = mid + 1;
+            res = Math.max(res, mid + 1);
         }
-        return false;
+        else r = mid - 1
     }
-
-    let res = null;
-    s = Array.from(s);
-    let i = 0;
-    let j = removable.length - 1;
-    
-    while (i <= j) {
-        const mid = Math.floor((i + j)/2);
-        const temp = new Set(removable.slice(0, mid + 1));
-        let x = s.map((val, index) => temp.has(index) ? null: val);
-        
-        if (isSubsequence(x)) {
-            res = mid;
-            if (i === j) break;
-            i = mid + 1;
-        } else j = mid - 1;
-    }
-
-    return res !== null ? res + 1 : 0;
+    return res;
 };
+
+var isSubseq = (str, p, removedSet) =>{
+    let i = 0, j = 0;
+    while( i < str.length && j < p.length){
+        if(removedSet.has(i) || str[i] !== p[j]) {
+            i++;
+            continue;
+        }
+        i++;
+        j++;
+    }
+    return j == p.length;
+}
