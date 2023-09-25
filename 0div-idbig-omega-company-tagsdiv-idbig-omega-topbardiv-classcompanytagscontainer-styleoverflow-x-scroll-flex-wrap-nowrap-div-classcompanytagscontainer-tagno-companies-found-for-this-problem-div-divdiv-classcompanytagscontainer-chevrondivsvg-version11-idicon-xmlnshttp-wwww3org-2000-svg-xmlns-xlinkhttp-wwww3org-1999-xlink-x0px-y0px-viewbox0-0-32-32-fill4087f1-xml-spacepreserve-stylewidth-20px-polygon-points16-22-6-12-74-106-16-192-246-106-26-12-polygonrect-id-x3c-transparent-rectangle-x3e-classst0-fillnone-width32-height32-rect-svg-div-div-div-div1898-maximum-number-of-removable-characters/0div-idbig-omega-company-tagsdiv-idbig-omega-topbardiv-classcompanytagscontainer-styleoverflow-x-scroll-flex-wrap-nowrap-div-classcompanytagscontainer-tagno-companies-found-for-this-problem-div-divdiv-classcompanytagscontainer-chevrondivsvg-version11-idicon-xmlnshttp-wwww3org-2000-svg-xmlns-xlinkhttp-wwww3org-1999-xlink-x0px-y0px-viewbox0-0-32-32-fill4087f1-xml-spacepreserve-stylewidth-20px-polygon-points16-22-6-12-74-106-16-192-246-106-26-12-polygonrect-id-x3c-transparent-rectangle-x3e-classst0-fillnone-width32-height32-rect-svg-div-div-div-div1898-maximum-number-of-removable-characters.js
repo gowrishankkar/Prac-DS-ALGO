c54373efb975776b81631870616687e65b5cf733
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-25 23:55:59 +0530

    Time: 222 ms (81.08%), Space: 67.9 MB (78.38%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1898-maximum-number-of-removable-characters.js
@@ -4,33 +4,72 @@
  * @param {number[]} removable
  * @return {number}
  */
-var maximumRemovals = function(s, p, removable) {
-    let res = 0, l = 0, r = removable.length - 1;
-    let removedSet = new Set();
-    while(l <= r){
-        let mid = l + Math.floor((r - l) / 2);
-          removedSet.clear();
-            for( let i=0; i <= mid; i++ ) {
-                removedSet.add(removable[i]);
-            }
-        if(isSubseq(s, p, removedSet)){
-            l = mid + 1;
-            res = Math.max(res, mid + 1);
+// var maximumRemovals = function(s, p, removable) {
+//     let res = 0, l = 0, r = removable.length - 1;
+//     let removedSet = new Set();
+//     while(l <= r){
+//         let mid = l + Math.floor((r - l) / 2);
+//           removedSet.clear();
+//             for( let i=0; i <= mid; i++ ) {
+//                 removedSet.add(removable[i]);
+//             }
+//         if(isSubseq(s, p, removedSet)){
+//             l = mid + 1;
+//             res = Math.max(res, mid + 1);
+//         }
+//         else r = mid - 1
+//     }
+//     return res;
+// };
+
+// var isSubseq = (str, p, removedSet) =>{
+//     let i = 0, j = 0;
+//     while( i < str.length && j < p.length){
+//         if(removedSet.has(i) || str[i] !== p[j]) {
+//             i++;
+//             continue;
+//         }
+//         i++;
+//         j++;
+//     }
+//     return j == p.length;
+// }
+
+
+
+function maximumRemovals(s, p, removable) {
+    let left = 0;
+    let right = removable.length;
+
+    while (left < right) {
+        const removed = new Array(s.length);
+        const pivot = (left + right) >> 1;
+
+        for (let i = 0; i <= pivot; i++) {
+            const index = removable[i];
+            removed[index] = true;
         }
-        else r = mid - 1
+
+        if (isSubsequence(s, p, removed)) {
+            left = pivot + 1;
+        } else {
+            right = pivot;
+        }
     }
-    return res;
+
+    return left;
 };
 
-var isSubseq = (str, p, removedSet) =>{
-    let i = 0, j = 0;
-    while( i < str.length && j < p.length){
-        if(removedSet.has(i) || str[i] !== p[j]) {
-            i++;
-            continue;
-        }
-        i++;
-        j++;
+function isSubsequence(s, p, removed) {
+    let pS = 0;
+    let pP = 0;
+
+    while (pP < p.length) {
+        if (pS >= s.length) return false;
+
+        if (!removed[pS] && s[pS] === p[pP]) pP++;
+        pS++;
     }
-    return j == p.length;
+
+    return true;
 }
