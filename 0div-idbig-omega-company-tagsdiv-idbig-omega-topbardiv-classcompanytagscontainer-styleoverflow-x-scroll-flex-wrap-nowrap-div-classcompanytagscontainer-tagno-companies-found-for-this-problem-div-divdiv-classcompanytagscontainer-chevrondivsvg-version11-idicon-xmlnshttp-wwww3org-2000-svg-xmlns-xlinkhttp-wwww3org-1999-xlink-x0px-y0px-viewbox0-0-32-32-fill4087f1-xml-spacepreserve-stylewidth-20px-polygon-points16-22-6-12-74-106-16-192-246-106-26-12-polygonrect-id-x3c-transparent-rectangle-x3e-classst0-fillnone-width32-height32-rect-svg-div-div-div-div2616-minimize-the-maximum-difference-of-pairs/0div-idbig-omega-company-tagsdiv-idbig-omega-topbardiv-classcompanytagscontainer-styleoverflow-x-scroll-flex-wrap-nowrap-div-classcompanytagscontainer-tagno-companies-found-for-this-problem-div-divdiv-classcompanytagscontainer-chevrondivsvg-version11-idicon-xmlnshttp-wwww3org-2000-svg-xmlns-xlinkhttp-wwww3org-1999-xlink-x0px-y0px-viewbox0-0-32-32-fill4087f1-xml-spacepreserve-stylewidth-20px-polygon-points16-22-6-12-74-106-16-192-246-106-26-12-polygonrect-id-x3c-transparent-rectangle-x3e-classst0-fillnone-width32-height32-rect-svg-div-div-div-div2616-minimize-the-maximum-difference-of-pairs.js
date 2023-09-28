2da022f1c944d02963b4f60eb8cd256b1198af24
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-09-28 08:47:50 +0530

    Time: 117 ms (83.61%), Space: 54.8 MB (20.64%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2616-minimize-the-maximum-difference-of-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2616-minimize-the-maximum-difference-of-pairs.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2616-minimize-the-maximum-difference-of-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2616-minimize-the-maximum-difference-of-pairs.js
@@ -3,69 +3,69 @@
  * @param {number} p
  * @return {number}
  */
-// var minimizeMax = function(nums, p) {
-//     nums.sort((a, b) => a - b);
-//     let l = 0, r = Math.pow(10 , 9);
-//     let res = Math.pow(10 , 9);
-//     while( l <= r){
-//         let m =  l + Math.floor((r - l)/ 2);
-//         if(isValid(m, nums, p)){
-//             res = m;
-//             r = m - 1;
-//         }else l = m + 1;
-//     }
-//     return res;
+var minimizeMax = function(nums, p) {
+    nums.sort((a, b) => a - b);
+    let l = 0, r = nums[nums.length - 1];
+    let res = Math.pow(10 , 9);
+    while( l <= r){
+        let m =  l + Math.floor((r - l)/ 2);
+        if(isValid(m, nums, p)){
+            res = m;
+            r = m - 1;
+        }else l = m + 1;
+    }
+    return res;
     
-// };
+};
 
-// var isValid = (t, nums, p) => {
-//     let i = 1, count = 0;
-//     while(i < nums.length){
-//         if(nums[i] - nums[i - 1] <= t){
-//             count++;
-//             i = i + 2;
-//         } else i++;
-//         // if(count == p) return true;
-//     }
-//     return count>=p;;
+var isValid = (t, nums, p) => {
+    let i = 1, count = 0;
+    while(i < nums.length){
+        if(nums[i] - nums[i - 1] <= t){
+            count++;
+            i = i + 2;
+        } else i++;
+        // if(count == p) return true;
+    }
+    return count>=p;;
     
+}
+// function isValid(maxDiff, nums, p, ){
+//     let n = 0;
+//     for(let i=1; i<nums.length; i++){
+//         if(nums[i]- nums[i-1] <= maxDiff){
+//             n++;
+//             i++;
+//         }
+//     }
+//     return n>=p;
 // }
-// // function isValid(maxDiff, nums, p, ){
-// //     let n = 0;
-// //     for(let i=1; i<nums.length; i++){
-// //         if(nums[i]- nums[i-1] <= maxDiff){
-// //             n++;
-// //             i++;
-// //         }
-// //     }
-// //     return n>=p;
-// // }
 
 
 
 // ----------------------------
-var minimizeMax = function(nums, p) {
-    nums.sort((x,y)=>x-y);
-    let l=0,r=nums[nums.length-1]-nums[0];
+// var minimizeMax = function(nums, p) {
+//     nums.sort((x,y)=>x-y);
+//     let l=0,r=nums[nums.length-1]-nums[0];
 
-    while(l<r){
-        let m = Math.trunc((l+r)/2);
-        if(numsPairs(nums,m)>=p)
-            r=m;
-        else
-            l=m+1;
-    }
+//     while(l<r){
+//         let m = Math.trunc((l+r)/2);
+//         if(numsPairs(nums,m)>=p)
+//             r=m;
+//         else
+//             l=m+1;
+//     }
 
-    return l;
+//     return l;
 
-    function numsPairs(nums,maxDiff){
-        let pairs = 0;
-        for(let i=1;i<nums.length;i++){
-            if(nums[i]-nums[i-1]<=maxDiff){
-                pairs++;
-                i++;
-            }
-        }
-        return pairs;
-    }
-};
+//     function numsPairs(nums,maxDiff){
+//         let pairs = 0;
+//         for(let i=1;i<nums.length;i++){
+//             if(nums[i]-nums[i-1]<=maxDiff){
+//                 pairs++;
+//                 i++;
+//             }
+//         }
+//         return pairs;
+//     }
+// };
