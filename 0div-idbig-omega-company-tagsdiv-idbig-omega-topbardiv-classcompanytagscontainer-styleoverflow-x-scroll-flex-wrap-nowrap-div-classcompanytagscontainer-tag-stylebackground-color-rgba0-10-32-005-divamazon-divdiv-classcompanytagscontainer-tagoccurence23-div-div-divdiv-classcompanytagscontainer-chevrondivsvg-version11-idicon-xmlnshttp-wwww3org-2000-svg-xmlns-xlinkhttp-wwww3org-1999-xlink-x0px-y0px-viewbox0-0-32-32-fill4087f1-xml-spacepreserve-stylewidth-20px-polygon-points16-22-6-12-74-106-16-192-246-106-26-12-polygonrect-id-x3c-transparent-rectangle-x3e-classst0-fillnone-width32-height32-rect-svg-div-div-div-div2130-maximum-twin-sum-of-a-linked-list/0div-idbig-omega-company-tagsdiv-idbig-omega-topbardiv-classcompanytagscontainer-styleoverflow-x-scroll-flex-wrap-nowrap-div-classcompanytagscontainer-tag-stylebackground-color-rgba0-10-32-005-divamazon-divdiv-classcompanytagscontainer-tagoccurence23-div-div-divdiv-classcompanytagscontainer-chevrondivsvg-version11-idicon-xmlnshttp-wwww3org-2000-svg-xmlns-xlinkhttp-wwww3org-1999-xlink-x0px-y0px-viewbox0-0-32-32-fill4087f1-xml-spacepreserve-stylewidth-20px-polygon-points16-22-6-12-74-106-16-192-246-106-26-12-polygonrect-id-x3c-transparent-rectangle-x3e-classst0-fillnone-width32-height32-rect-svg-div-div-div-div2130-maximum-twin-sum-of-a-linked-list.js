bb2f9d9b0abdffc15977eb574461603b40685252
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-22 11:19:58 +0530

    Time: 143 ms (21.87%), Space: 91.7 MB (55.58%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence23-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2130-maximum-twin-sum-of-a-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence23-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2130-maximum-twin-sum-of-a-linked-list.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence23-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2130-maximum-twin-sum-of-a-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence23-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2130-maximum-twin-sum-of-a-linked-list.js
@@ -9,31 +9,51 @@
  * @param {ListNode} head
  * @return {number}
  */
+// var pairSum = function (head) {
+//   let stack = [];
+
+//   let tmp = head;
+//   let count = 0,
+//     length = 0,
+//     ans = 0;
+
+//   while (tmp) {
+//     length++;
+//     tmp = tmp.next;
+//   }
+
+//   tmp = head;
+
+//   while (count++ != length / 2) {
+//     stack.push(tmp.val);
+//     tmp = tmp.next;
+//   }
+
+//   while (count++ <= length) {
+//     var curr = stack.pop();
+//     ans = Math.max(ans, curr + tmp.val);
+//     tmp = tmp.next;
+//   }
+
+//   return ans;
+// };
+
+
+
 var pairSum = function (head) {
-  let stack = [];
-
-  let tmp = head;
-  let count = 0,
-    length = 0,
-    ans = 0;
-
-  while (tmp) {
-    length++;
-    tmp = tmp.next;
-  }
-
-  tmp = head;
-
-  while (count++ != length / 2) {
-    stack.push(tmp.val);
-    tmp = tmp.next;
-  }
-
-  while (count++ <= length) {
-    var curr = stack.pop();
-    ans = Math.max(ans, curr + tmp.val);
-    tmp = tmp.next;
-  }
+  let stack = [], ans = 0;
+    let [slow, fast] = [head, head]
+      while (fast &&  fast.next) {
+        stack.push(slow.val);
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+    // console.log('stack', stack, slow)
+      while (stack.length && slow ) {
+        var curr = stack.pop();
+        ans = Math.max(ans, curr + slow.val);
+        slow = slow.next;
+      }
 
   return ans;
 };
