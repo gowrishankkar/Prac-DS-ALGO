bd7a7c99d1e509a0ca70d2458e329b35f792434b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-28 07:19:29 +0530

    Time: 46 ms (85.15%), Space: 42.4 MB (12.43%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtusimple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div145-binary-tree-postorder-traversal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtusimple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div145-binary-tree-postorder-traversal.js
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+
+// var postorderTraversal = function(root) {
+//     let output = [];
+//     function foo (node) {
+//         if(!node) return;
+//         foo(node.left);
+//         foo(node.right);
+//          output.push(node.val);
+//     }
+//     foo(root);
+//     return output;
+// };
+
+
+// var postorderTraversal = function(root) {
+//    let s1 = []
+//    let s2 = [];
+//     let result = []
+//     if(root ==  null) return []
+//     s1.push(root);
+//     while(s1.length){
+//         let node = s1.pop();
+//         s2.push(node.val);
+//         if(node.left !== null) s1.push(node.left)
+//         if(node.right !== null) s1.push(node.right)
+        
+//     }
+    
+//     console.log(s2,'test')
+    
+//     while(s2.length){
+//         result.push(s2.pop())
+//     }
+//     return result
+// };
+
+
+
+
+// var postorderTraversal = function(root) {
+//     let s1 = []
+//     let result = []
+//     if(root ==  null) return []
+//     s1.push(root);
+//     while(s1.length){
+//         let node = s1.pop();
+//         result.unshift(node.val);
+//         if(node.left !== null) s1.push(node.left)
+//         if(node.right !== null) s1.push(node.right)
+        
+//     }
+
+//     return result
+// };
+
+
+var postorderTraversal = function(root) {
+    let s1 = []
+    let result = []
+    if(root ==  null) return []
+    s1.push(root);
+    while(s1.length){
+        let node = s1.pop();
+        result.unshift(node.val);
+        if(node.left !== null) s1.push(node.left)
+        if(node.right !== null) s1.push(node.right)
+        
+    }
+
+    return result
+};
