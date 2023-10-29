3dbf7a4d6d2841f4de932ff039322027594087bc
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-29 18:30:42 +0530

    Time: 46 ms (98.51%), Space: 44.5 MB (65.67%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree.js
@@ -46,51 +46,43 @@
 // };
 
 
-// var verticalTraversal = function(root) 
-// {
-//      let map = {}
-//         const dfs = (node, v, lev) =>{
-//             if(node == null) return;
-//             if(!map[v]) map[v] = []
-//             map[v] = [...map[v], {lev, val : node.val}];
-//             dfs(node.left, v - 1, lev + 1);
-//             dfs(node.right, v + 1, lev + 1);
-//         }
+var verticalTraversal = function(root) 
+{
+     let map = {}
+        const dfs = (node, v, lev) =>{
+            if(node == null) return;
+            if(!map[v]) map[v] = []
+            map[v] = [...map[v], {lev, val : node.val}];
+            dfs(node.left, v - 1, lev + 1);
+            dfs(node.right, v + 1, lev + 1);
+        }
         
-//         dfs(root, 0, 0);
-//         console.log('map', map)
-    
-//         const sortedKeys = Object.keys(map).sort()
-//         console.log('data', sortedKeys)
-//         let result = [];
-//         Object.keys(map).sort((a,b)=> a-b).forEach((key) =>{
-//             // let item = []
-//             map[key].sort((a,b)=>(a.lev===b.lev)?a.val-b.val:a.lev-b.lev);
-//              result.push(map[key].map(el=>el.val));
-//             // map[key].forEach(({val})=>{
-//             //     item.push(val)
-//             // })
-//             // result.push(item)
-//         })
-//     return result;
-// };
+        dfs(root, 0, 0);
+        let result = [];
+        Object.keys(map).sort((a,b)=> a-b).forEach((key) =>{
+            map[key].sort((a,b)=>(a.lev===b.lev)?a.val-b.val:a.lev-b.lev);
+            result.push(map[key].map(el=>el.val));
 
-
-var verticalTraversal = function(root) {
-  let map = {};
-  const traverse = (node, row = 0, col = 0) => {
-    if (!node) return;
-    if (!map[col]) map[col] = [];
-    if (!map[col][row]) map[col][row] = [];
-    map[col][row].push(node.val);
-    traverse(node.left, row + 1, col - 1);
-    traverse(node.right, row + 1, col + 1);
-  }
-  traverse(root);
-  
-  let sortedKeys = Object.keys(map).sort((a, b) => a - b);
-  return sortedKeys.map(key => map[key].reduce((acc, curr) => {
-    acc.push(...curr.sort((a, b) => a - b));
-    return acc;
-  }, []));
+        })
+    return result;
 };
+
+
+// var verticalTraversal = function(root) {
+//   let map = {};
+//   const traverse = (node, row = 0, col = 0) => {
+//     if (!node) return;
+//     if (!map[col]) map[col] = [];
+//     if (!map[col][row]) map[col][row] = [];
+//     map[col][row].push(node.val);
+//     traverse(node.left, row + 1, col - 1);
+//     traverse(node.right, row + 1, col + 1);
+//   }
+//   traverse(root);
+  
+//   let sortedKeys = Object.keys(map).sort((a, b) => a - b);
+//   return sortedKeys.map(key => map[key].reduce((acc, curr) => {
+//     acc.push(...curr.sort((a, b) => a - b));
+//     return acc;
+//   }, []));
+// };
