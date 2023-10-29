f36344dbfae7a98f09f9fe9089f9c5c03b426df2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-29 18:25:26 +0530

    Time: 78 ms (8.46%), Space: 47.2 MB (6.47%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence82-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div987-vertical-order-traversal-of-a-binary-tree.js
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+//  */
+// var verticalTraversal = function(root) 
+// {
+//         let colMap = {}, min=0,max=0;
+        
+//         const dfs = function(node,x=0,y=0)
+//         {
+//                 if(node === null)
+//                         return;
+                
+//                 /* Push the item onto the appropriate x listing. */
+//                 let item = colMap[x];
+//                 if(item === undefined)
+//                 {
+//                         item = [];
+//                         min = Math.min(min,x);
+//                         max = Math.max(max,x);
+//                 }
+//                 item.push({y,val:node.val});
+//                 colMap[x] = item;
+//                 dfs(node.left,x-1,y+1);
+//                 dfs(node.right,x+1,y+1);
+//         }
+
+//         dfs(root);
+
+//         let result = [];
+//         for(let i=min; i<= max; i++)
+//         {
+//                 colMap[i].sort((a,b)=>(a.y===b.y)?a.val-b.val:a.y-b.y);
+//                 result.push(colMap[i].map(el=>el.val));
+//         }
+
+//         return result;
+// };
+
+
+var verticalTraversal = function(root) 
+{
+     let map = {}
+        const dfs = (node, v, lev) =>{
+            if(node == null) return;
+            if(!map[v]) map[v] = []
+            map[v] = [...map[v], {lev, val : node.val}];
+            dfs(node.left, v - 1, lev + 1);
+            dfs(node.right, v + 1, lev + 1);
+        }
+        
+        dfs(root, 0, 0);
+        console.log('map', map)
+    
+        const sortedKeys = Object.keys(map).sort()
+        console.log('data', sortedKeys)
+        let result = [];
+        Object.keys(map).sort((a,b)=> a-b).forEach((key) =>{
+            // let item = []
+            map[key].sort((a,b)=>(a.lev===b.lev)?a.val-b.val:a.lev-b.lev);
+             result.push(map[key].map(el=>el.val));
+            // map[key].forEach(({val})=>{
+            //     item.push(val)
+            // })
+            // result.push(item)
+        })
+    return result;
+};
