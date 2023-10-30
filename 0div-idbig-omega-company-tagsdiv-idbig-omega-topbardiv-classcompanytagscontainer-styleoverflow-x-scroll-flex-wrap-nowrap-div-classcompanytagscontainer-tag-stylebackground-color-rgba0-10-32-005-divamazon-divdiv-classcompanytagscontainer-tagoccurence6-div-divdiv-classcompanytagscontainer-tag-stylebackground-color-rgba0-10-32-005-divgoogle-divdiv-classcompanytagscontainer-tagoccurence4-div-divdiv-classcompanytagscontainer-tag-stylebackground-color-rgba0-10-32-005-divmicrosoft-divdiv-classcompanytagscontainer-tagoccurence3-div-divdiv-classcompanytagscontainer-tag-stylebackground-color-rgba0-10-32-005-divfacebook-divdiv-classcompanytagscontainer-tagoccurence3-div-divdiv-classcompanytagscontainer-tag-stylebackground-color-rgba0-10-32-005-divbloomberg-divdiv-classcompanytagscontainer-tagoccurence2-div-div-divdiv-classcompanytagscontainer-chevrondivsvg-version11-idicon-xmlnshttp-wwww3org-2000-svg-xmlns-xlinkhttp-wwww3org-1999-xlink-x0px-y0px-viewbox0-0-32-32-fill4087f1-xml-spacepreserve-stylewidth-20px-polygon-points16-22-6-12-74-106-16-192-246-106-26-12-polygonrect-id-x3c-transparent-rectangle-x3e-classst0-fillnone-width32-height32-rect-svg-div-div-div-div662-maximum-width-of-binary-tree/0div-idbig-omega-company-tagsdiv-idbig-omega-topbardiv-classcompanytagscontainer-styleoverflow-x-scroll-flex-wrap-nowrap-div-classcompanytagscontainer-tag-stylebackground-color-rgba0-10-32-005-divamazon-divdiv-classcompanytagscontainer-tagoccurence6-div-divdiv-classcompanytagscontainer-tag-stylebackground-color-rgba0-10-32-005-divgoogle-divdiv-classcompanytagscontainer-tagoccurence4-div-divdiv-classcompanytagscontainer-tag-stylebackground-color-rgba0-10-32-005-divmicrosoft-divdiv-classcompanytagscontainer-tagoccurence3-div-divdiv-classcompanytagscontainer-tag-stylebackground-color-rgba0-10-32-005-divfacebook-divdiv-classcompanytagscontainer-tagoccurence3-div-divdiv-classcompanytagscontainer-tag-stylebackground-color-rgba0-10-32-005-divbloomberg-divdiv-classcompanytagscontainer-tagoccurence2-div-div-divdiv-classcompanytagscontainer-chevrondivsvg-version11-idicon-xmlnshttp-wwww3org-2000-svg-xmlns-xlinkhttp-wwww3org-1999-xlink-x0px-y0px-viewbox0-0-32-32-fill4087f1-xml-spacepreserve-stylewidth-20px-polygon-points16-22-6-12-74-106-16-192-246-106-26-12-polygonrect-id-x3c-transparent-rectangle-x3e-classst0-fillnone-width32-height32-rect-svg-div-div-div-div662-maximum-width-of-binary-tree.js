c65163ae681d351fd2752e8d778b4d9ca01f1192
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-30 22:50:58 +0530

    Time: 72 ms (21.53%), Space: 46.9 MB (59.33%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div662-maximum-width-of-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div662-maximum-width-of-binary-tree.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div662-maximum-width-of-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div662-maximum-width-of-binary-tree.js
@@ -10,28 +10,28 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var widthOfBinaryTree = function(root) {
-    if(!root) return 0;
-    let ans = 0;
-    let s = [];
-    s.push([root, 0])
-    while(s.length){
-        let first = 0, last = 0;
-        let minVal = s[0][1];
-        console.log('minVal', minVal)
-        const size = s.length;
-        for(let i = 0; i < size; i++){
-            let [node, val] = s.shift();
-            let curri = val - minVal;
-            if(i == 0) first = curri;
-            if(i == size - 1) last = curri;
-            if(node.left) s.push([node.left, curri * 2 + 1])
-            if(node.right) s.push([node.right, curri * 2 + 2])
-        }
-        ans = Math.max(ans, last - first + 1)
-    }
-    return ans;
-};
+// var widthOfBinaryTree = function(root) {
+//     if(!root) return 0;
+//     let ans = 0;
+//     let s = [];
+//     s.push([root, 0])
+//     while(s.length){
+//         let first = 0, last = 0;
+//         let minVal = s[0][1];
+//         console.log('minVal', minVal)
+//         const size = s.length;
+//         for(let i = 0; i < size; i++){
+//             let [node, val] = s.shift();
+//             let curri = val - minVal;
+//             if(i == 0) first = curri;
+//             if(i == size - 1) last = curri;
+//             if(node.left) s.push([node.left, curri * 2 + 1])
+//             if(node.right) s.push([node.right, curri * 2 + 2])
+//         }
+//         ans = Math.max(ans, last - first + 1)
+//     }
+//     return ans;
+// };
 
 
 // var widthOfBinaryTree = function(root) {
@@ -61,3 +61,32 @@ var widthOfBinaryTree = function(root) {
 //     }
 //     return max;
 // };
+
+
+var widthOfBinaryTree = function(root) {
+    if (root === null) {
+    return 0;
+  }
+
+  let ans = 0;
+  const q = []; // {node, index}
+  q.push([root, 1]);
+
+  while (q.length > 0) {
+    const offset = q[0][1] * 2;
+    ans = Math.max(ans, q[q.length - 1][1] - q[0][1] + 1);
+    const sz = q.length;
+    for (let i = 0; i < sz; i++) {
+      const node = q[0][0];
+      const index = q.shift()[1];
+      if (node.left !== null) {
+        q.push([node.left, index * 2 - offset]);
+      }
+      if (node.right !== null) {
+        q.push([node.right, index * 2 + 1 - offset]);
+      }
+    }
+  }
+
+  return ans;
+};
