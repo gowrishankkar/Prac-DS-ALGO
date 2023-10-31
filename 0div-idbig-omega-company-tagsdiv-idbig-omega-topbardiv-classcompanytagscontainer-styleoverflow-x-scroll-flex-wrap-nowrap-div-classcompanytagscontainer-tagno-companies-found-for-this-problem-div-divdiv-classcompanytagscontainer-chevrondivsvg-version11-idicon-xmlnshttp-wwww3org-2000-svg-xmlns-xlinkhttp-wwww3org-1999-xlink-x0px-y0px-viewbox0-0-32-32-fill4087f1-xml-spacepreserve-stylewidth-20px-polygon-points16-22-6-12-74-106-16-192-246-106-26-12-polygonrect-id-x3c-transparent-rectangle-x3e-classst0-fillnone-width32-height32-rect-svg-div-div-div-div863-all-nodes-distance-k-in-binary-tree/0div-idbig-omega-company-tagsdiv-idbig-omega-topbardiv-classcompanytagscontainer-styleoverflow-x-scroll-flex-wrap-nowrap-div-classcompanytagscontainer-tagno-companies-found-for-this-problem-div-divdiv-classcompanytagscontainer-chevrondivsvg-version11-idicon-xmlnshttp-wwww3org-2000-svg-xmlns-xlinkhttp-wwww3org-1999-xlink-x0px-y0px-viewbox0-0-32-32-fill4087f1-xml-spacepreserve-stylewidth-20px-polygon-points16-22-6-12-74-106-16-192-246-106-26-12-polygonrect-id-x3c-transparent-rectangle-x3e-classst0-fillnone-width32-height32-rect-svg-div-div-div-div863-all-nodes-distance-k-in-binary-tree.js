e953b17b913ae5ad30e0d889ca62590494161d22
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-31 11:41:49 +0530

    Time: 78 ms (12.26%), Space: 45.3 MB (23.58%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div863-all-nodes-distance-k-in-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div863-all-nodes-distance-k-in-binary-tree.js
@@ -0,0 +1,112 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+
+var getMap = (root) =>{
+    
+    let q= [];
+    let map = {};
+    q.push(root)
+    while(q.length){
+        let node = q.shift();
+        if(node.left) {
+            q.push(node.left);
+            map[node.left.val] = node;
+        }
+        if(node.right) {
+            q.push(node.right);
+            map[node.right.val] = node;
+        }
+    }
+    return map;
+}
+var distanceK = function(root, target, k) {
+    if(!root) return []
+    let map = getMap(root);
+    let visited = {};
+    visited[target.val] = true;
+    let q = [];
+    q.push(target);
+    let currLevel = 0
+    while(q.length){
+        let size = q.length;
+        if(currLevel == k) break;
+        for(let i = 0; i < size ; i++){
+              let node = q.shift();
+            if(node.left && !visited[node.left.val]){
+                 visited[node.left.val] = true;
+                q.push(node.left)
+            }
+            if(node.right && !visited[node.right.val]){
+                 visited[node.right.val] = true;
+                q.push(node.right)
+            }
+            
+            const parent = map[node.val];
+            // console.log('parent', Boolean(parent), parent.val)
+            if(parent && !visited.hasOwnProperty(parent.val)){
+                 visited[parent.val] = true;
+                q.push(parent)
+            }  
+        }
+      
+        currLevel++;
+    }
+      console.log('visited', visited, q)
+    return q.map((ele) => ele.val);
+
+    
+};
+
+
+// var distanceK = function(root, target, k) {
+//       const result = [];
+//   const parentMap = new Map(); // Map to store parent node of each node
+
+//   // DFS to populate parentMap
+//   const dfs = (node, parent) => {
+//     if (node === null) {
+//       return;
+//     }
+//     parentMap.set(node.val, parent);
+//     dfs(node.left, node);
+//     dfs(node.right, node);
+//   };
+
+//   dfs(root, null); // Populate parentMap
+
+//   // BFS to find nodes at distance k from target
+//   const queue = [{ node: target, distance: 0 }];
+//   const visited = new Set();
+//   while (queue.length > 0) {
+//     const { node, distance } = queue.shift();
+//     visited.add(node.val);
+
+//     if (distance === k) {
+//       result.push(node.val);
+//     } else if (distance < k) {
+//       if (node.left && !visited.has(node.left.val)) {
+//         queue.push({ node: node.left, distance: distance + 1 });
+//       }
+//       if (node.right && !visited.has(node.right.val)) {
+//         queue.push({ node: node.right, distance: distance + 1 });
+//       }
+//       const parent = parentMap.get(node.val);
+//       if (parent && !visited.has(parent.val)) {
+//         queue.push({ node: parent, distance: distance + 1 });
+//       }
+//     }
+//   }
+
+//   return result;
+// };
