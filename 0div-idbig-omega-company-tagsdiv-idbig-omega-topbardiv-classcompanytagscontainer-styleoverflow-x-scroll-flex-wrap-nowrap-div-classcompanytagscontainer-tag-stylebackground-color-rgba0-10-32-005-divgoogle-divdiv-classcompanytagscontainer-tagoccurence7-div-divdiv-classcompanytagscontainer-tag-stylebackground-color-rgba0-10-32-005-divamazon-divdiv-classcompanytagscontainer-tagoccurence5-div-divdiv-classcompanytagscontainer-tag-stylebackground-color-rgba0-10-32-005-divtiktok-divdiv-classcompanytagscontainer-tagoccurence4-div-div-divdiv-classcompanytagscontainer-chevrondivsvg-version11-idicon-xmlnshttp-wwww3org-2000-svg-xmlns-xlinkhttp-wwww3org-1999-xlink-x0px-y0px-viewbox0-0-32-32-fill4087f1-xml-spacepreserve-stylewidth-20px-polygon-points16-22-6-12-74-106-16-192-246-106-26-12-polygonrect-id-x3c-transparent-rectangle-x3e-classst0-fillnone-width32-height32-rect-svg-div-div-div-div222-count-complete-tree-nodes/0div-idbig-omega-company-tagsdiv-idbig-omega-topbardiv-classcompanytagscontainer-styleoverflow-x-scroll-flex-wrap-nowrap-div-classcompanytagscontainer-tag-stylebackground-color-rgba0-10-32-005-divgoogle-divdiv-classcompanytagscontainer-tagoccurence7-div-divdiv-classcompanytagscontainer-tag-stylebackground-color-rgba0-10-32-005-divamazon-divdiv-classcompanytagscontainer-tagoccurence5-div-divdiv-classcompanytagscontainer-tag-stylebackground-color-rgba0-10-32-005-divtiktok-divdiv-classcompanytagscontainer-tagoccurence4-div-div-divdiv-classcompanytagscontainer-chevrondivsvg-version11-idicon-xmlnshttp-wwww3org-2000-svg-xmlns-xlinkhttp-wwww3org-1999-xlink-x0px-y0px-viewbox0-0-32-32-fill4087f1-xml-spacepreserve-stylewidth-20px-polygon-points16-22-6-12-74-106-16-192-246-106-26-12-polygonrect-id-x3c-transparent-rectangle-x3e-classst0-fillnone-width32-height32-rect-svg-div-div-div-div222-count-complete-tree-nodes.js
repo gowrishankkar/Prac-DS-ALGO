b57133f37b9c1b1592c7a4cd9889e80490cdb652
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-10-31 20:10:23 +0530

    Time: 79 ms (83.98%), Space: 68.9 MB (99.34%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div222-count-complete-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div222-count-complete-tree-nodes.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div222-count-complete-tree-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div222-count-complete-tree-nodes.js
@@ -10,31 +10,31 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var countNodes = function(root) {
-    if(!root) return 0;
-    let left = getLeftHeight(root);
-    let right = getRightHeight(root);
-    if(left == right) return Math.pow(2, left) - 1;
-    return 1 + countNodes(root.left) + countNodes(root.right);
-};
+// var countNodes = function(root) {
+//     if(!root) return 0;
+//     let left = getLeftHeight(root);
+//     let right = getRightHeight(root);
+//     if(left == right) return Math.pow(2, left) - 1;
+//     return 1 + countNodes(root.left) + countNodes(root.right);
+// };
 
-var getLeftHeight = (node) =>{
-    let h = 0;
-    while(node){
-        h++;
-        node = node.left;
-    }
-    return h;
-}
+// var getLeftHeight = (node) =>{
+//     let h = 0;
+//     while(node){
+//         h++;
+//         node = node.left;
+//     }
+//     return h;
+// }
 
-var getRightHeight = (node) =>{
-    let h = 0;
-    while(node){
-        h++;
-        node = node.right;
-    }
-    return h;
-}
+// var getRightHeight = (node) =>{
+//     let h = 0;
+//     while(node){
+//         h++;
+//         node = node.right;
+//     }
+//     return h;
+// }
 
 
 
@@ -45,3 +45,18 @@ var getRightHeight = (node) =>{
 //     return 1 + countNodes(root.left) + countNodes(root.right);
     
 // };
+
+
+var countNodes = function(root) {
+    if(!root) return null;
+    let stack = [root];
+    let c = 0;
+    while(stack.length) {
+        let node = stack.pop() ;
+        node.left && stack.push(node.left);        
+        node.right && stack.push(node.right);
+		// if(node.val) will take node value 0 as falsy, so make sure to check for 0 as well, since a node can have a value of 0
+        if(node.val >= 0) c++;
+    }
+    return c;
+};
