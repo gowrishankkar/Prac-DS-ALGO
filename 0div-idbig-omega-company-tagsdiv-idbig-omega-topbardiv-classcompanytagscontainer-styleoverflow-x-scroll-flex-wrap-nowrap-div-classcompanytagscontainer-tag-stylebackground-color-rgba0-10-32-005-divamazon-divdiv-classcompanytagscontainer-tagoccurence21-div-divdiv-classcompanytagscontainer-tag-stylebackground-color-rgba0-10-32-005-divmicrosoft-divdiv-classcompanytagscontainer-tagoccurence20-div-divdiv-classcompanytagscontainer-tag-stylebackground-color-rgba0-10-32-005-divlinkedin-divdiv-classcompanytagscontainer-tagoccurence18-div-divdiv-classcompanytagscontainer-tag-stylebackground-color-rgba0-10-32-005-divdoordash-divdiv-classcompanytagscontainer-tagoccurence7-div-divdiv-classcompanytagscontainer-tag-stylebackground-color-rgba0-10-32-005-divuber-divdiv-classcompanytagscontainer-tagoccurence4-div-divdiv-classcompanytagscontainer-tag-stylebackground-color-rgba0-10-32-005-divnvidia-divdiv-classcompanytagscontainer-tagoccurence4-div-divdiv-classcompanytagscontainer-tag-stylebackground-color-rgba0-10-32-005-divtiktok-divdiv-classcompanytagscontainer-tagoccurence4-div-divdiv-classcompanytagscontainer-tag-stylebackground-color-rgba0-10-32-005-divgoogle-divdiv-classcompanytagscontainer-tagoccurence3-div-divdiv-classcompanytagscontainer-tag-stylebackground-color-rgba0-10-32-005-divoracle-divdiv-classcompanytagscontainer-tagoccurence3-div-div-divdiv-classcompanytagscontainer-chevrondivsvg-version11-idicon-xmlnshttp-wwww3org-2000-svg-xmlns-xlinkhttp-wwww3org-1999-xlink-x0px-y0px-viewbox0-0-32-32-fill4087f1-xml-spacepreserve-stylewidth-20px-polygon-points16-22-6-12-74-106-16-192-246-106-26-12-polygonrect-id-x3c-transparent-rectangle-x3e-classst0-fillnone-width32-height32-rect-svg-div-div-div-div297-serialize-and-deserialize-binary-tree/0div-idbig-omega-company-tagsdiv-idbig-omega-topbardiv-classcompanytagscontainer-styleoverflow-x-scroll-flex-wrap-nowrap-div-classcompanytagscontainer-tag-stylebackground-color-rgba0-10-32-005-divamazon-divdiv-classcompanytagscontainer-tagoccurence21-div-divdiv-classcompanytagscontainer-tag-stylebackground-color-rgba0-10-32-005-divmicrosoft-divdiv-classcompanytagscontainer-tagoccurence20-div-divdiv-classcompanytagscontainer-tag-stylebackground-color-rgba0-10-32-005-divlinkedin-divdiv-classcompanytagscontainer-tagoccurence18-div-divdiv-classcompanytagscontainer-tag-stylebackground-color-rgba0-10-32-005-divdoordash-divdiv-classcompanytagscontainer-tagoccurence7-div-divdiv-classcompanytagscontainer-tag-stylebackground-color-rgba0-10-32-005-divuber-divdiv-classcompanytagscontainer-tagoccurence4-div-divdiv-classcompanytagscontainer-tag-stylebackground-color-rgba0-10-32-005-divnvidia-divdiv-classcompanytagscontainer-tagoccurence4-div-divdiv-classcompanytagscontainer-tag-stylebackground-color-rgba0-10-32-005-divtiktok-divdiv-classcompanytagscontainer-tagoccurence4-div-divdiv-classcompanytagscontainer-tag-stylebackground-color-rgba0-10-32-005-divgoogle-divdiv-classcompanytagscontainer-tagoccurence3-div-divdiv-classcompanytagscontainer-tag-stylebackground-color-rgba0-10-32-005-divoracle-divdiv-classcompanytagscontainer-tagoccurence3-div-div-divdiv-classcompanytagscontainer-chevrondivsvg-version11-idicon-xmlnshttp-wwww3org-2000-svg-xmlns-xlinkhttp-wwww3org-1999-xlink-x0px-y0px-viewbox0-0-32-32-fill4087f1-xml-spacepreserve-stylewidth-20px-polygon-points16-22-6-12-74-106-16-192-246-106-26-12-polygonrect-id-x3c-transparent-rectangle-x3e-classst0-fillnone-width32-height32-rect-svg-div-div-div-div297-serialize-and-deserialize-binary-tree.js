b5c36b76eafc6976c189931ee0216d2c1a5a85c8
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-11-01 10:35:01 +0530

    Time: 84 ms (85.34%), Space: 53.5 MB (64.66%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divnvidia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div297-serialize-and-deserialize-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divnvidia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div297-serialize-and-deserialize-binary-tree.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divnvidia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div297-serialize-and-deserialize-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divnvidia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div297-serialize-and-deserialize-binary-tree.js
@@ -12,19 +12,57 @@
  * @param {TreeNode} root
  * @return {string}
  */
+// var serialize = function(root) {
+//     let queue = [], res = [];
+//     queue.push(root);
+//     while (queue.length > 0) {
+//         let node = queue.shift();
+//         node ? res.push(node.val) : res.push("null");
+//         if (node) queue.push(node.left);
+//         if (node) queue.push(node.right);
+//     }
+//     let i = res.length-1;
+//     console.log('res', res, res.join("$"))
+//     while (res[i] === "null") i--;
+//     return res.slice(0,i+1).join("#");
+// };
+
+// /**
+//  * Decodes your encoded data to tree.
+//  *
+//  * @param {string} data
+//  * @return {TreeNode}
+//  */
+// var deserialize = function(data) {
+//       if (data === "") {
+//         return null;
+//     }
+//     let arr = data.split("#"), i = 1;
+//     let queue = [];
+//     let root = new TreeNode(Number(arr[0]));
+//     console.log('arr', arr)
+//     queue.push(root);
+//     while (queue.length > 0) {
+//         let left = (i >= arr.length || arr[i] === "null") ? null : new TreeNode(Number(arr[i]));
+//         let right = (i+1 >= arr.length || arr[i+1] === "null") ? null : new TreeNode(Number(arr[i+1]));
+//         i += 2;
+//         let root = queue.shift();
+//         root.left = left;
+//         root.right = right;
+//         if (left !== null) queue.push(left);
+//         if (right !== null) queue.push(right);
+//     }
+//     return root;
+// };
+
+
+
 var serialize = function(root) {
-    let queue = [], res = [];
-    queue.push(root);
-    while (queue.length > 0) {
-        let node = queue.shift();
-        node ? res.push(node.val) : res.push("null");
-        if (node) queue.push(node.left);
-        if (node) queue.push(node.right);
-    }
-    let i = res.length-1;
-    console.log('res', res, res.join("$"))
-    while (res[i] === "null") i--;
-    return res.slice(0,i+1).join("#");
+  if (!root) {
+    return 'n';
+  }
+
+  return `${root.val} ${serialize(root.left)} ${serialize(root.right)}`;
 };
 
 /**
@@ -34,27 +72,24 @@ var serialize = function(root) {
  * @return {TreeNode}
  */
 var deserialize = function(data) {
-      if (data === "") {
-        return null;
-    }
-    let arr = data.split("#"), i = 1;
-    let queue = [];
-    let root = new TreeNode(Number(arr[0]));
-    console.log('arr', arr)
-    queue.push(root);
-    while (queue.length > 0) {
-        let left = (i >= arr.length || arr[i] === "null") ? null : new TreeNode(Number(arr[i]));
-        let right = (i+1 >= arr.length || arr[i+1] === "null") ? null : new TreeNode(Number(arr[i+1]));
-        i += 2;
-        let root = queue.shift();
-        root.left = left;
-        root.right = right;
-        if (left !== null) queue.push(left);
-        if (right !== null) queue.push(right);
-    }
-    return root;
-};
+  const treeNodes = data.split(' ');
+  let i = 0;
 
+  function restoreTree() {
+    if (treeNodes[i] === 'n' || i >= treeNodes.length) {
+      i++;
+      return null;
+    }
+
+    const node = new TreeNode(treeNodes[i]);
+    i++;
+    node.left = restoreTree();
+    node.right = restoreTree();
+    return node;
+  }
+
+  return restoreTree();
+};
 /**
  * Your functions will be called as such:
  * deserialize(serialize(root));
