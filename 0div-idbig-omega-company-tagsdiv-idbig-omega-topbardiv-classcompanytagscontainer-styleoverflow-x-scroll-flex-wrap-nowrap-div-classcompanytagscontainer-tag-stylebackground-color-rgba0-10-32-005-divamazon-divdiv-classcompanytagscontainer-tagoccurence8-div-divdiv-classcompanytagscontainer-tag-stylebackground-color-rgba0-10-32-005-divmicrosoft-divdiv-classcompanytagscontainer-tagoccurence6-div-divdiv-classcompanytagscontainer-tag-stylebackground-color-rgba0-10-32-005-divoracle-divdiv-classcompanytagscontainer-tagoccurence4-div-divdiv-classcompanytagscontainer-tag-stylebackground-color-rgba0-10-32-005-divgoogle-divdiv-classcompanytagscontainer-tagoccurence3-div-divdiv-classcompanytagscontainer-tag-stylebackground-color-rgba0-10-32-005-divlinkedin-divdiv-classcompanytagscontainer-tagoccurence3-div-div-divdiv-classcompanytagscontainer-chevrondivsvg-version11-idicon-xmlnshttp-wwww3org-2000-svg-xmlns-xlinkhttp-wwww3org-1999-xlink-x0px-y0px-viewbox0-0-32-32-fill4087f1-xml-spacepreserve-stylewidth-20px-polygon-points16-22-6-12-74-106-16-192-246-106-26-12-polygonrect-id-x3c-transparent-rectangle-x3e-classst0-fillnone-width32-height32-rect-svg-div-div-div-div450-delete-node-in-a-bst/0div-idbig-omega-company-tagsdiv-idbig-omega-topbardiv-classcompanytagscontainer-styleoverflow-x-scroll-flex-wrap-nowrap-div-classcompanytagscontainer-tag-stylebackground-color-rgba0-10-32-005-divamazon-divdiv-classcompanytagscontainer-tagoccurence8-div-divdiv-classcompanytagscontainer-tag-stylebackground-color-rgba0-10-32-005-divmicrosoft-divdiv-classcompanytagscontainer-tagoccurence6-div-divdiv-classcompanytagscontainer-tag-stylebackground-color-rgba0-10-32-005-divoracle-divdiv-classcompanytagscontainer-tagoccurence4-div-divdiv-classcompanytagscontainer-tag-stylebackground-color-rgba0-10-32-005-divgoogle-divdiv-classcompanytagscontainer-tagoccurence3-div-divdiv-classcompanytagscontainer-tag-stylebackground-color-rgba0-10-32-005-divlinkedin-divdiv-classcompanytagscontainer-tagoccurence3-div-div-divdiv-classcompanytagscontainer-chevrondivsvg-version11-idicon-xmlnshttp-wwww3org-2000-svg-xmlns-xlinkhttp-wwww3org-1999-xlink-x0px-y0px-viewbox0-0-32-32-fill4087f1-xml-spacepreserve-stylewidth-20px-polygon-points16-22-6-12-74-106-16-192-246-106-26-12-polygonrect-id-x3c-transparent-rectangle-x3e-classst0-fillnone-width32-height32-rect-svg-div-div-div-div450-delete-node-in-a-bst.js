97cbfbe191323e121a2f06a47afaf234e291fade
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-11-03 17:47:09 +0530

    Time: 77 ms (83.24%), Space: 51.2 MB (13.81%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div450-delete-node-in-a-bst/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div450-delete-node-in-a-bst.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div450-delete-node-in-a-bst/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div450-delete-node-in-a-bst.js
@@ -12,36 +12,67 @@
  * @return {TreeNode}
  */
 
-var getLastRight = (root) =>{
-    if(!root.right) return root;
-    return getLastRight(root.right)
-}
-var helper = (root) => {
-    if(!root.left) return root.right;
-    if(!root.right) return root.left;
-    let rightChild =  root.right;
-    let lastChild = getLastRight(root.left);
-    lastChild.right = rightChild;
-    return root.left;
-}
-var deleteNode = function(root, key) {
-    if(!root) return null;
-    if(root.val == key) return helper(root);
-    let dummy = root;
-    while(root){
-        if(root.val > key){
-            if(root.left && root.left.val == key){
-                root.left = helper(root.left)
-                break;
-            } else root = root.left;
-        } else {
-            if(root.right && root.right.val == key){
-                root.right = helper(root.right)
-                break;
-            } else root = root.right;
-        }
+// var getLastRight = (root) =>{
+//     if(!root.right) return root;
+//     return getLastRight(root.right)
+// }
+// var helper = (root) => {
+//     if(!root.left) return root.right;
+//     if(!root.right) return root.left;
+//     let rightChild =  root.right;
+//     let lastChild = getLastRight(root.left);
+//     lastChild.right = rightChild;
+//     return root.left;
+// }
+// var deleteNode = function(root, key) {
+//     if(!root) return null;
+//     if(root.val == key) return helper(root);
+//     let dummy = root;
+//     while(root){
+//         if(root.val > key){
+//             if(root.left && root.left.val == key){
+//                 root.left = helper(root.left)
+//                 break;
+//             } else root = root.left;
+//         } else {
+//             if(root.right && root.right.val == key){
+//                 root.right = helper(root.right)
+//                 break;
+//             } else root = root.right;
+//         }
 
-    }
-    return dummy;
+//     }
+//     return dummy;
     
-};
+// };
+
+
+
+var deleteNode = function (root, key) {
+    function dfs(node) {
+        if (!node) {
+            return null;
+        }
+        if(node.val === key){
+            if(!node.left){
+                return node.right;
+            }
+            if(!node.right){
+                return node.left;
+            }
+            let curr = node.right;
+            while(curr.left){
+                curr = curr.left;
+            }
+            curr.left = node.left;
+            return node.right;
+        }
+        if(key < node.val){
+            node.left = dfs(node.left);
+        } else {
+            node.right = dfs(node.right);
+        }
+        return node;
+    }
+    return dfs(root);
+}
