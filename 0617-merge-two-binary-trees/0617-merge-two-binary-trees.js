97f29aceac66c05a220c9a0e3a5836efec614214
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-11-06 00:22:45 +0530

    Time: 74 ms (93.02%), Space: 50.5 MB (10.96%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -11,12 +11,22 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
-var mergeTrees = function(root1, root2) {
-    if(!root1 && !root2) return null;
-    let v1 = root1 ? root1.val : 0;
-    let v2 = root2 ? root2.val : 0;
-    let root = new TreeNode(v1 + v2);
-    root.left = mergeTrees(root1 && root1.left ? root1.left : null, root2 && root2.left ? root2.left : null);
-    root.right = mergeTrees(root1 && root1.right ? root1.right : null, root2 && root2.right ? root2.right : null);
-    return root;
+// var mergeTrees = function(root1, root2) {
+//     if(!root1 && !root2) return null;
+//     let v1 = root1 ? root1.val : 0;
+//     let v2 = root2 ? root2.val : 0;
+//     let root = new TreeNode(v1 + v2);
+//     root.left = mergeTrees(root1 && root1.left ? root1.left : null, root2 && root2.left ? root2.left : null);
+//     root.right = mergeTrees(root1 && root1.right ? root1.right : null, root2 && root2.right ? root2.right : null);
+//     return root;
+// };
+
+
+const mergeTrees = (root1, root2) => {
+    if (!root1) return root2;
+    if (!root2) return root1;
+    root1.val += root2.val;
+    root1.left = mergeTrees(root1.left, root2.left);
+    root1.right = mergeTrees(root1.right, root2.right);
+    return root1;
 };
