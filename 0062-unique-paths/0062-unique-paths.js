356d1fe49988f13044589efb062098befdeb1dba
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-11-06 22:32:42 +0530

    Time: 67 ms (6.77%), Space: 46.3 MB (5.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -3,46 +3,19 @@
  * @param {number} n
  * @return {number}
  */
-// var uniquePaths = (row, col) => {
-//     const isBaseCase = ((row == 1) || (col == 1));
-//     if (isBaseCase) return 1;
-
-//     return dfs(row, col);/* Time O(2^N) | Space O(HEIGHT) */
-// }
-
-// var dfs = (row, col) => {
-//     const left = uniquePaths((row - 1), col); /* Time O(2^N) | Space O(HEIGHT) */
-//     const right = uniquePaths(row, (col - 1));/* Time O(2^N) | Space O(HEIGHT) */
-
-//     return (left + right);
-// }
-
-
-/**
- * DP - Bottom Up
- * Array - Tabulation
- * Time O(ROWS * COLS) | Space O(COLS)
- * https://leetcode.com/problems/unique-paths/
- * @param {number} m
- * @param {number} n
- * @return {number}
- */
-var uniquePaths = (row, col) => {
-    const tabu = initTabu(col);/* Time O(COLS)        | Space O(COLS) */ 
-
-    search(row, col, tabu);    /* Time O(ROWS * COLS) | Space O(COLS) */ 
-
-    return tabu[(col - 1)];
-};
-
-var initTabu = (col) => new Array(col).fill(1); /* Time O(COLS) | Space O(COLS) */ 
-
-var search = (row, col, tabu) => {
-    for (let _row = 1; (_row < row); _row++) {/* Time O(ROWS) */ 
-        for (let _col = 1; (_col < col); _col++) {/* Time O(COLS) */ 
-            const prev = tabu[(_col - 1)];
-
-            tabu[_col] += prev;                     /* Space O(COLS) */ 
-        }
+var uniquePaths = function(m, n) {
+    
+    function test(i, j){
+        if(i == 0 && j == 0 ) return 1;
+        if(i < 0 || j < 0 ) return 0;
+        if(dp[i][j] !== -1) return dp[i][j]; 
+        let up = test(i - 1, j, dp);
+        let left  = test(i, j - 1, dp);
+        return dp[i][j] = left + up;
     }
-}
+    // let dp = new Array(m).fill(null).map(()=>Array(81).fill('q'))
+        let dp = new Array(m).fill(-1).map(()=>Array(n).fill(-1))
+        console.log(dp)
+    return test(m - 1, n - 1, dp);
+    
+};
