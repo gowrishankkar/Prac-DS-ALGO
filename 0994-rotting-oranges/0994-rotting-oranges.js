2167d294529b0d4c8d03e9d2fb409aff6215ba3f
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-03 10:25:18 +0530

    Time: 60 ms (85.84%), Space: 44.5 MB (76.26%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -49,106 +49,106 @@
 // };
 
 
-var orangesRotting = function(grid) {
-    let n =  grid.length;
-    let m = grid[0].length;
+// var orangesRotting = function(grid) {
+//     let n =  grid.length;
+//     let m = grid[0].length;
     
-    let q = [];
-    let flag = 0
-    for(let i = 0; i< n; i++){
-        for(let j = 0; j< m; j++){
-            if(grid[i][j] === 2){
-                q.push([i, j, 0]);
-            }
-            if(grid[i][j] == 1) flag = 1;
-        }
-    }
-    if(!flag) return 0;
+//     let q = [];
+//     let flag = 0
+//     for(let i = 0; i< n; i++){
+//         for(let j = 0; j< m; j++){
+//             if(grid[i][j] === 2){
+//                 q.push([i, j, 0]);
+//             }
+//             if(grid[i][j] == 1) flag = 1;
+//         }
+//     }
+//     if(!flag) return 0;
     
-    var getNeighbors = (currRow, n, currCol, m, grid) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
-    .map(([ r, c ]) => [ (currRow + r), (currCol + c) ])
-    .filter(([ r, c ]) => (0 <= r) && (r < n) && (0 <= c) && (c < m) && grid[r][c] !== 2 && grid[r][c] == 1)
+//     var getNeighbors = (currRow, n, currCol, m, grid) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
+//     .map(([ r, c ]) => [ (currRow + r), (currCol + c) ])
+//     .filter(([ r, c ]) => (0 <= r) && (r < n) && (0 <= c) && (c < m) && grid[r][c] !== 2 && grid[r][c] == 1)
 
-    let tMin = 0;
-    while(q.length){
-        let [r, c, t] = q.shift();
-        tMin = Math.max(tMin, t)
-        for(let [nRow, nCol] of getNeighbors(r, n, c, m,  grid)){
-            q.push([nRow, nCol, t + 1])
-            grid[nRow][nCol] = 2;
-        }
+//     let tMin = 0;
+//     while(q.length){
+//         let [r, c, t] = q.shift();
+//         tMin = Math.max(tMin, t)
+//         for(let [nRow, nCol] of getNeighbors(r, n, c, m,  grid)){
+//             q.push([nRow, nCol, t + 1])
+//             grid[nRow][nCol] = 2;
+//         }
         
-    }
+//     }
     
-    for(let i = 0; i< n; i++){
-        for(let j = 0; j< m; j++){
-            if(grid[i][j] == 1){
-                return -1;
-            }
-        }
-    }
+//     for(let i = 0; i< n; i++){
+//         for(let j = 0; j< m; j++){
+//             if(grid[i][j] == 1){
+//                 return -1;
+//             }
+//         }
+//     }
     
-    return tMin;
+//     return tMin;
 
 
-};
+// };
 
    
 
 
-// var orangesRotting = function(grid) {
-//     const dir = [
-//         [-1,0],
-//         [0,-1],
-//         [1,0],
-//         [0,1]
-//     ]
-//     let rot = []
-//     let flag = 0
-//     for(let i=0;i<grid.length;i++){
-//         for(let j=0;j<grid[0].length;j++){
-//             if(grid[i][j]===1){
-//                 flag = 1
-//                 break
-//             }
-//         }
-//     }
-//     if(flag === 0) return 0
-//     for(let i=0;i<grid.length;i++){
-//         for(let j=0;j<grid[0].length;j++){
-//             if(grid[i][j]===2){
-//                 rot.push([i,j])
-//             }
-//         }
-//     }
-//     let rc = 0
-//     let count = rot.length
-//      while(rot.length){
-//          let rotten = rot.shift()
-//          count--
-//          for(let i=0;i<dir.length;i++){
-//              let nr = rotten[0] + dir[i][0]
-//              let nc = rotten[1] + dir[i][1]
-//              if(nr<0 || nr>=grid.length || nc<0 || nc>=grid[0].length) {continue;}
-//              if(grid[nr][nc] === 1){
-//                  grid[nr][nc] = 2
-//                  rot.push([nr,nc])
+var orangesRotting = function(grid) {
+    const dir = [
+        [-1,0],
+        [0,-1],
+        [1,0],
+        [0,1]
+    ]
+    let rot = []
+    let flag = 0
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]===1){
+                flag = 1
+                break
+            }
+        }
+    }
+    if(flag === 0) return 0
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]===2){
+                rot.push([i,j])
+            }
+        }
+    }
+    let rc = 0
+    let count = rot.length
+     while(rot.length){
+         let rotten = rot.shift()
+         count--
+         for(let i=0;i<dir.length;i++){
+             let nr = rotten[0] + dir[i][0]
+             let nc = rotten[1] + dir[i][1]
+             if(nr<0 || nr>=grid.length || nc<0 || nc>=grid[0].length) {continue;}
+             if(grid[nr][nc] === 1){
+                 grid[nr][nc] = 2
+                 rot.push([nr,nc])
                  
-//              }
-//          }
-//          if(count === 0 && rot.length>0){
-//                      rc++
-//                      count = rot.length
-//                  }
-//      }
-//     for(let i=0;i<grid.length;i++){
-//         for(let j=0;j<grid[0].length;j++){
-//             if(grid[i][j]===1){
-//                 return -1
-//             }
-//         }
-//     }
-//     return rc
+             }
+         }
+         if(count === 0 && rot.length>0){
+                     rc++
+                     count = rot.length
+                 }
+     }
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]===1){
+                return -1
+            }
+        }
+    }
+    return rc
      
 
-// };
+};
