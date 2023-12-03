d271182853b914c527429ae14e84c508a4c9a7c4
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-03 20:15:35 +0530

    Time: 54 ms (87.87%), Space: 44.2 MB (90.80%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.js
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.js
@@ -2,34 +2,79 @@
  * @param {number[][]} graph
  * @return {boolean}
  */
-var isBipartite = function(graph) {
-    let color = new Array(graph.length).fill(-1)
-    const check =(graph, color, start) =>{
-        let q = [];
-        q.push(start)
-        color[start] = 0;
+// var isBipartite = function(graph) {
+//     let color = new Array(graph.length).fill(-1)
+//     const check =(graph, color, start) =>{
+//         let q = [];
+//         q.push(start)
+//         color[start] = 0;
        
-        while(q.length){
-            let node = q.pop();
-            for(let i of graph[node]){
-                if(color[i] == -1){
+//         while(q.length){
+//             let node = q.pop();
+//             for(let i of graph[node]){
+//                 if(color[i] == -1){
                     
-                color[i] = !color[node];
-                q.push(i)
-                    console.log('color', color)
-                }
-                else if(color[i] === color[node]) {
-                    console.log('color', color)
-                    return false}
-            }
-        }
-        return true
-    }
+//                 color[i] = !color[node];
+//                 q.push(i)
+//                     console.log('color', color)
+//                 }
+//                 else if(color[i] === color[node]) {
+//                     console.log('color', color)
+//                     return false}
+//             }
+//         }
+//         return true
+//     }
     
-    for(let i = 0; i <graph.length ; i++){
-        if(color[i] === -1 && !check(graph, color, i)) return false
-    }
-    return true
+//     for(let i = 0; i <graph.length ; i++){
+//         if(color[i] === -1 && !check(graph, color, i)) return false
+//     }
+//     return true
 
     
+// };
+
+// BDS
+// var isBipartite = function(graph) {
+//     let colors = new Array(graph.length).fill(-1);
+//     for (let i = 0; i < graph.length; i++){
+//         if (colors[i] === -1){
+//             let queue = [i];
+//             colors[i] = 0;
+//             while (queue.length){
+//                 let node = queue.shift();
+//                 for (let neighbor of graph[node]){
+//                     if (colors[neighbor] === -1){
+//                         colors[neighbor] = 1 - colors[node];
+//                         queue.push(neighbor);
+//                     } else if (colors[neighbor] === colors[node]){
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return true;
+
+// };
+
+// DFS
+var isBipartite = function(graph) {
+    let colors = new Array(graph.length).fill(-1);
+    for(let i = 0; i <graph.length ; i++){
+        if(colors[i] === -1 && !dfs(graph, colors, i, 0)) return false
+    }
+    return true;
 };
+
+var dfs = (graph, colors, node, c) => {
+    colors[node] = c;
+    for (let neighbor of graph[node]){
+        if (colors[neighbor] === -1 && !dfs(graph, colors, neighbor, 1 - c)){
+            return false
+        } else if (colors[neighbor] === colors[node]){
+            return false;
+        }
+    }
+    return true
+}
