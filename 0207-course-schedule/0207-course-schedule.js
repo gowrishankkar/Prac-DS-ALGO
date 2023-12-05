7ec4ab0f5dd9fc03679fe11f2dc5e91e6c109cec
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-05 09:27:52 +0530

    Time: 153 ms (8.14%), Space: 52.2 MB (8.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,15 +4,18 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    let adj = new Array(numCourses).fill([]),
-    q = [],
-    topo = [],
-    inDegree = new Array(numCourses).fill(0);
-
+    let adj = new Array(numCourses).fill([]);
+    let q = [];
+    let topo = [];
+    let inDegree = new Array(numCourses).fill(0);
+    let map = {}
+    
     for(const [e, v] of prerequisites){
-       adj[e].push(v);
+        // map[v]
+        if(!map[v]) map[v] = []
+         map[v].push(e)
+       // adj[v].push(e);
        inDegree[e]++
-        
     }
 
     for(let i = 0; i< numCourses ; i++){
@@ -22,51 +25,56 @@ var canFinish = function(numCourses, prerequisites) {
     while(q.length){
         let node = q.shift();
         topo.push(node)
-        for(let it in adj[node]){
-            inDegree[it]--;
-            if(inDegree[it] == 0) q.push(it)
+        if(map[node]){
+            console.log('map[node]', map[node])
+            for(let e of map[node]){
+                inDegree[e]--;
+                if(inDegree[e] == 0) q.push(e)
+            }  
         }
+        
     }
     return topo.length === numCourses ;
     
 };
 
 
-var canFinish = function(numCourses, prerequisites) {
-    let coursesOrder = [],
-        graph = new Map(),
-        inDegree = Array(numCourses).fill(0),
-        queue = []
+// var canFinish = function(numCourses, prerequisites) {
+//     let coursesOrder = [],
+//         graph = new Map(),
+//         inDegree = Array(numCourses).fill(0),
+//         queue = []
 
-    for(let [e, v] of prerequisites)   {
-        if(!graph.has(v)) {
-            graph.set(v, [])
-        }
+//     for(let [e, v] of prerequisites)   {
+//         if(!graph.has(v)) {
+//             graph.set(v, [])
+//         }
 
-        graph.get(v).push(e)
-        inDegree[e]++
-    } 
+//         graph.get(v).push(e)
+//         inDegree[e]++
+//     } 
 
-    for(let i = 0; i < inDegree.length; i++) {
-        if (inDegree[i] === 0) {
-            queue.push(i)
-        }
-    }
+//     for(let i = 0; i < inDegree.length; i++) {
+//         if (inDegree[i] === 0) {
+//             queue.push(i)
+//         }
+//     }
 
-    while(queue.length > 0) {
-        let v = queue.shift()
+//     while(queue.length > 0) {
+//         let v = queue.shift()
 
-        if (graph.has(v)) {
-            for(let e of graph.get(v)) {
-                inDegree[e]--
-                if(inDegree[e] === 0) {
-                    queue.push(e)
-                }
-            }
-        }
+//         if (graph.has(v)) {
+//             for(let e of graph.get(v)) {
+//                 inDegree[e]--
+//                 if(inDegree[e] === 0) {
+//                     queue.push(e)
+//                 }
+//             }
+//         }
 
-        coursesOrder.push(v)
-    }
+//         coursesOrder.push(v)
+//     }
 
-    return numCourses === coursesOrder.length
-};
+//     return numCourses === coursesOrder.length
+// };
+
