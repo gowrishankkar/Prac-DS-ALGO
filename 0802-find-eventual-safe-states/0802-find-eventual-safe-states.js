d553a0b7336613dfe0f7a9be4cb2627c8dc71a6b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-05 10:01:39 +0530

    Time: 129 ms (38.76%), Space: 53.3 MB (48.31%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.js
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.js
@@ -6,6 +6,20 @@ var eventualSafeNodes = function(graph) {
     let vis = new Array(graph.length).fill(0);
     let pv = new Array(graph.length).fill(0);
     let check = new Array(graph.length).fill(0);
+    var dfs = (node, vis, pv, check, graph) => {
+        vis[node] = 1;
+        pv[node] = 1;
+        for (let neighbor of graph[node]){
+            if (vis[neighbor] === 0 && dfs(neighbor, vis, pv, check, graph)){
+                return true
+            } else if (pv[neighbor]){
+                return true;
+            }
+        }
+        check[node] = 1;
+        pv[node] = 0;
+        return false
+    }
     for(let i = 0; i <graph.length ; i++){
         if(vis[i] === 0) dfs(i, vis, pv, check, graph)
     }
@@ -18,17 +32,3 @@ var eventualSafeNodes = function(graph) {
 };
 
 
-var dfs = (node, vis, pv, check, graph) => {
-    vis[node] = 1;
-    pv[node] = 1;
-    for (let neighbor of graph[node]){
-        if (vis[neighbor] === 0 && dfs(neighbor, vis, pv, check, graph)){
-            return true
-        } else if (pv[neighbor]){
-            return true;
-        }
-    }
-    check[node] = 1;
-    pv[node] = 0;
-    return false
-}
