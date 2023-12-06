d51b5238ff7502e369ab231efe0663a6935edb4b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-06 09:59:23 +0530

    Time: 519 ms (33.65%), Space: 59.2 MB (26.36%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -5,46 +5,71 @@
  * @return {number}
  */
 
-
 var ladderLength = function(beginWord, endWord, wordList) {
-    const [ queue, wordSet, seen ] = [ new Queue([[ beginWord, 1 ]]), new Set(wordList), new Set([ beginWord ]) ];
-
-    return bfs(queue, wordSet, seen, endWord);/* Time O(ROWS * COLS) | Space O(ROWS * COLS) */
+    let n = beginWord.length;
+    let q = [];
+    q.push([beginWord, 1]);
+    let set = new Set([...wordList]);
+    while(q.length){
+        let [word, step] =  q.shift();
+        if(word === endWord) return step;
+        for(let i = 0; i < n ; i++){
+            for (const char of 'abcdefghijklmnopqrstuvwxyz') {
+                let newWord =  word.split('')
+                newWord[i] = char;
+                newWord = newWord.join('')
+                if(set.has(newWord)){
+                   set.delete(newWord);
+                   q.push([newWord, step + 1])
+                }
+            }
+        }
+    }
+    return 0;
 };
 
-const bfs = (queue, wordSet, seen, endWord) => {
-    while (!queue.isEmpty()) {
-        for (let i = (queue.size() - 1); 0 <= i; i--) {
-            const [ word, depth ] = queue.dequeue();
 
-            const isTarget = word === endWord
-            if (isTarget) return depth
 
-            transform(queue, wordSet, seen, word, depth)
-        }
-    }
 
-    return 0
-}
+// var ladderLength = function(beginWord, endWord, wordList) {
+//     const [ queue, wordSet, seen ] = [ new Queue([[ beginWord, 1 ]]), new Set(wordList), new Set([ beginWord ]) ];
 
-const transform = (queue, wordSet, seen, word, depth) => {
-    for (const index in word) {
-        for (const char of 'abcdefghijklmnopqrstuvwxyz') {
-            const neighbor = getNeighbor(word, index, char);
+//     return bfs(queue, wordSet, seen, endWord);/* Time O(ROWS * COLS) | Space O(ROWS * COLS) */
+// };
 
-            const hasSeen = !wordSet.has(neighbor) || seen.has(neighbor);
-            if (hasSeen) continue;
+// const bfs = (queue, wordSet, seen, endWord) => {
+//     while (!queue.isEmpty()) {
+//         for (let i = (queue.size() - 1); 0 <= i; i--) {
+//             const [ word, depth ] = queue.dequeue();
 
-            queue.enqueue([ neighbor, (depth + 1) ]);
-            seen.add(neighbor);
-        }
-    }
-}
+//             const isTarget = word === endWord
+//             if (isTarget) return depth
 
-const getNeighbor = (word, index, char) => {
-    const neighbor = word.split('');
+//             transform(queue, wordSet, seen, word, depth)
+//         }
+//     }
 
-    neighbor[index] = char;
+//     return 0
+// }
 
-    return neighbor.join('');
-}
+// const transform = (queue, wordSet, seen, word, depth) => {
+//     for (const index in word) {
+//         for (const char of 'abcdefghijklmnopqrstuvwxyz') {
+//             const neighbor = getNeighbor(word, index, char);
+
+//             const hasSeen = !wordSet.has(neighbor) || seen.has(neighbor);
+//             if (hasSeen) continue;
+
+//             queue.enqueue([ neighbor, (depth + 1) ]);
+//             seen.add(neighbor);
+//         }
+//     }
+// }
+
+// const getNeighbor = (word, index, char) => {
+//     const neighbor = word.split('');
+
+//     neighbor[index] = char;
+
+//     return neighbor.join('');
+// }
