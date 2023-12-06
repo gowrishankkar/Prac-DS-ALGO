e8b13267ac8ed8a1eaf3f8d7b3ca8cbd5b6a4182
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-06 10:01:42 +0530

    Time: 198 ms (71.94%), Space: 50 MB (80.16%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -5,29 +5,62 @@
  * @return {number}
  */
 
+// var ladderLength = function(beginWord, endWord, wordList) {
+//     let n = beginWord.length;
+//     let q = [];
+//     q.push([beginWord, 1]);
+//     let set = new Set([...wordList]);
+//     while(q.length){
+//         let [word, step] =  q.shift();
+//         if(word === endWord) return step;
+//         for(let i = 0; i < n ; i++){
+//             for (const char of 'abcdefghijklmnopqrstuvwxyz') {
+//                 let newWord =  word.split('')
+//                 newWord[i] = char;
+//                 newWord = newWord.join('')
+//                 if(set.has(newWord)){
+//                    set.delete(newWord);
+//                    q.push([newWord, step + 1])
+//                 }
+//             }
+//         }
+//     }
+//     return 0;
+// };
+
+
 var ladderLength = function(beginWord, endWord, wordList) {
-    let n = beginWord.length;
-    let q = [];
-    q.push([beginWord, 1]);
-    let set = new Set([...wordList]);
-    while(q.length){
-        let [word, step] =  q.shift();
-        if(word === endWord) return step;
-        for(let i = 0; i < n ; i++){
-            for (const char of 'abcdefghijklmnopqrstuvwxyz') {
-                let newWord =  word.split('')
-                newWord[i] = char;
-                newWord = newWord.join('')
-                if(set.has(newWord)){
-                   set.delete(newWord);
-                   q.push([newWord, step + 1])
+    const wordSet = new Set(wordList)
+    let queue = [beginWord];
+    let steps = 1;
+    
+    while(queue.length) {
+        const next = [];
+        
+        // loop over each word in the queue
+        for(let word of queue) {
+            if(word === endWord) return steps;
+            
+            // loop over each char of the word 
+            for(let i = 0; i < word.length; i++) {
+                
+                // and replace the char with letters from [a - z]
+                for(let j = 0; j < 26; j++) {
+                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
+                    
+                    // if the new word exist in the word list add it to the queue
+                    if(wordSet.has(newWord)) {
+                        next.push(newWord);
+                        wordSet.delete(newWord);
+                    }
                 }
             }
         }
+        queue = next
+        steps++;
     }
-    return 0;
+    return 0;    
 };
-
 
 
 
