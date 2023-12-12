16be31f066bba59b61ffcd880358308c6095eb00
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-12 23:52:38 +0530

    Time: 53 ms (79.67%), Space: 44.4 MB (26.37%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+    let [ left, right, jumps ] = [ 0, 0, 0 ];
+
+    while (right < nums.length - 1) {
+        const maxReach = getMaxReach(nums, left, right);
+
+        left = right + 1;
+        right = maxReach;
+        jumps += 1;
+    }
+
+    return jumps;
+};
+
+const getMaxReach = (nums, left, right, maxReach = 0) => {
+    for (let i = left; i < right + 1; i++) {
+        const reach = nums[i] + i;
+        maxReach = Math.max(maxReach, reach);
+    }
+
+    return maxReach;
+}
