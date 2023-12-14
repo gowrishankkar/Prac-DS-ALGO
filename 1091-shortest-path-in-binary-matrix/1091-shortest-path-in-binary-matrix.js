fa8804c96ba06861c24420994a2306276a2362da
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-14 21:36:47 +0530

    Time: 104 ms (59.29%), Space: 48.3 MB (99.26%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -19,7 +19,7 @@
 
 
 
-const movs = [
+const dir = [
   [-1,-1], [0,-1], [1,-1],
   [-1,0], [1,0],
   [-1,1], [0,1], [1,1],
@@ -28,20 +28,16 @@ const movs = [
 var shortestPathBinaryMatrix = function(grid) {
   const n = grid.length - 1;
   if (grid[n][n] === 1 || grid[0][0] === 1) return -1;
-
-  const stack = [{ coord: [0,0], count: 1 }];
+  const stack = [[0,0,1]];
   grid[0][0] = 1;
-
   while (stack.length) {
-    const { coord, count } = stack.shift();
-    if (coord[0] === n && coord[1] === n) return count;
-
-    movs.forEach((mov) => {
-      const i = coord[0] + mov[0];
-      const j = coord[1] + mov[1];
-
+    const [r, c, d] = stack.shift();
+    if (r === n && c === n) return d;
+    dir.forEach((mov) => {
+      const i = r + mov[0];
+      const j = c + mov[1];
       if (grid[i]?.[j] === 0) {
-        stack.push({ coord: [i,j], count: count + 1 })
+        stack.push([i, j, d + 1])
         grid[i][j] = 1;
       }
     })
