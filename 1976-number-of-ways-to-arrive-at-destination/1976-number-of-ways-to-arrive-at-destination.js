47081c60a957292d93a009c45142d06026221ca0
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-15 23:14:41 +0530

    Time: 163 ms (27.59%), Space: 57.1 MB (58.62%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.js
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.js
@@ -12,29 +12,28 @@ var countPaths = function(n, roads) {
     }
     
     const heap = new MinPriorityQueue({
-        priority: x => x[1]
+        priority: x => x[0]
     });
-    let dis = new Array(n).fill(Infinity);
+    let dist = new Array(n).fill(Infinity);
     let ways = new Array(n).fill(0);
-    dis[0] = 0;
+    dist[0] = 0;
     ways[0] = 1;
 
     heap.enqueue([0, 0]);
     while(heap.size()) {
-            const [n, cost] = heap.dequeue().element;
-            for(let [nextNode, w] of adj[n]) {
-                const total = cost + w;
-                if(dis[nextNode] == total) {
-                    // already have found a path which set me to this value;
-                    ways[nextNode] = (ways[nextNode] + ways[n]) % 1000000007;
-                }
-                else if(dis[nextNode] > total) {
-                    dis[nextNode] = total;
-                    ways[nextNode] = ways[n];
-                    heap.enqueue([nextNode, total]);
-                }
-            }
-        }
+        const [cost, node] = heap.dequeue().element;
+         for(let [an, e] of adj[node]){
+             const total = cost + e;
+             if(total < dist[an]){
+                 dist[an] = total;
+                 heap.enqueue([total, an]);
+                 ways[an] = ways[node]
+             } else if( total === dist[an]){
+                 ways[an] = (ways[an] + ways[node] ) % 1000000007;
+             }
+         }
+    }
+    
     return ways[n-1] ;
 };
 
