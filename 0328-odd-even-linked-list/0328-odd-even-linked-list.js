f60efd535c2ec0f00b8ca5a33bd27a3610445ec7
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-31 20:59:30 +0530

    Time: 134 ms (5.33%), Space: 44.3 MB (73.23%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -27,21 +27,16 @@
 
 
 var oddEvenList = function (head) {
-  if (!head) return head;
+    if (!head) return head;
 
-  let odd = head;
-  let even = head.next;
-  let evenHead = head.next;
-
-  while (odd && odd.next && even && even.next) {
-    odd.next = odd.next.next;
-    even.next = even.next.next;
-
-    odd = odd.next;
-    even = even.next;
-  }
-
-  odd.next = evenHead;
-
-  return head;
+    let odd = head;
+    let even = head.next;
+    while (odd.next && odd.next.next) {
+        let tmp = odd.next;
+        odd.next = odd.next.next;
+        odd = odd.next;
+        tmp.next = odd.next;
+    }
+    odd.next = even;
+    return head;
 };
