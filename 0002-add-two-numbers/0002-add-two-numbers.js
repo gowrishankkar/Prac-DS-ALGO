8e2adcabae0a006d7e7b24a5f4a4c563be3f9bd2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2023-12-31 19:58:24 +0530

    Time: 190 ms (5.01%), Space: 46.7 MB (91.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,30 +11,13 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let sentinel = tail = new ListNode();
+      const rec=(n1,n2,carry)=>{
+        if(!n1 && !n2 && !carry) return null
+        const sum=(n1?.val||0)+(n2?.val||0)+carry
+        const nextNode= rec(n1?.next, n2?.next,Math.floor(sum/10))
+        return  new ListNode(sum%10,nextNode)
+    }
+    
 
-    return add(l1, l2, tail, sentinel);       /* Time O(MAX(N, M)) | Space O(MAX(N, M)) */
-}
-
-const add = (l1, l2, tail, sentinel, carry = 0) => {
-    const isBaseCase = !(l1 || l2 || carry);
-    if (isBaseCase) return sentinel.next;
-
-    return dfs(l1, l2, tail, sentinel, carry);/* Time O(MAX(N, M)) | Space O(MAX(N, M)) */
-}
-
-const dfs = (l1, l2, tail, sentinel, carry) => {
-    const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
-    const val = sum % 10;
-    carry = Math.floor(sum / 10);
-
-    tail.next = new ListNode(val);
-    tail = tail.next;
-
-    l1 = l1?.next || null;
-    l2 = l2?.next || null;
-
-    add(l1, l2, tail, sentinel, carry);     /* Time O(MAX(N, M)) | Space O(MAX(N, M)) */
-
-    return sentinel.next;
-}
+    return rec(l1,l2,0)
+};
