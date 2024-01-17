fe571734e46bad4ed89981a3368fea964e8c849b
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-01-17 23:27:54 +0530

    Time: 66 ms (94.13%), Space: 46.9 MB (19.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,13 +1,19 @@
-var moveZeroes = (nums) => {
-    let [ left, right ] = [ 0, 0 ];
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let nonZeroIndex = 0;
 
-    while (right < nums.length) {
-        const canSwap = (nums[right] !== 0)
-        if (canSwap) {
-            [nums[left], nums[right]] = [nums[right], nums[left]];
-            left++;
+    // Move all non-zero elements to the front of the array
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[nonZeroIndex++] = nums[i];
         }
+    }
 
-        right++;
+    // Fill the remaining elements with zeroes
+    for (let i = nonZeroIndex; i < nums.length; i++) {
+        nums[i] = 0;
     }
 };
