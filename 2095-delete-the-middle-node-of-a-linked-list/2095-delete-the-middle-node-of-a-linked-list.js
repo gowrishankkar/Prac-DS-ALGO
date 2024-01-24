7f95fa6b1fa9f46b8dab8a4435f7e754d297aa18
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-01-25 00:00:17 +0530

    Time: 668 ms (13.15%), Space: 91.4 MB (95.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -9,14 +9,31 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteMiddle = function(head) {
-    let dummy = new ListNode(null, head);    
-    let slow = dummy;
-    let fast = dummy;
-    while(fast?.next?.next) {        
-        slow = slow.next;
-        fast = fast.next.next;
-    }       
-    slow.next = slow.next.next;    
-    return dummy.next;    
-}
+// var deleteMiddle = function(head) {
+//     let dummy = new ListNode(null, head);    
+//     let slow = dummy;
+//     let fast = dummy;
+//     while(fast?.next?.next) {        
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }       
+//     slow.next = slow.next.next;    
+//     return dummy.next;    
+// }
+var deleteMiddle = function(head) 
+{
+    let n = 0, p = head;
+    while(p)
+    {
+        n++;
+        p = p.next;
+    }
+    let preceding = Math.floor(n/2);
+    let start = new ListNode(0,head);
+    p = start;
+    for(let i=0; i<preceding; i++)
+        p = p.next;
+    p.next = p.next.next;
+    
+    return start.next;
+};
