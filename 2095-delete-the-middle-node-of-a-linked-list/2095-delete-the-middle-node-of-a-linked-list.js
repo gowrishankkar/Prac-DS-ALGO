5cecdecb4ea10300157e9e05cb0ff89f176cac2d
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-01-25 00:00:57 +0530

    Time: 717 ms (11.57%), Space: 90.6 MB (96.99%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -20,20 +20,40 @@
 //     slow.next = slow.next.next;    
 //     return dummy.next;    
 // }
-var deleteMiddle = function(head) 
-{
-    let n = 0, p = head;
-    while(p)
-    {
-        n++;
-        p = p.next;
-    }
-    let preceding = Math.floor(n/2);
-    let start = new ListNode(0,head);
-    p = start;
-    for(let i=0; i<preceding; i++)
-        p = p.next;
-    p.next = p.next.next;
+
+
+// var deleteMiddle = function(head) 
+// {
+//     let n = 0, p = head;
+//     while(p)
+//     {
+//         n++;
+//         p = p.next;
+//     }
+//     let preceding = Math.floor(n/2);
+//     let start = new ListNode(0,head);
+//     p = start;
+//     for(let i=0; i<preceding; i++)
+//         p = p.next;
+//     p.next = p.next.next;
     
-    return start.next;
+//     return start.next;
+// };
+
+
+var deleteMiddle = function(head) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let slow = head;
+    let fast = head;
+    let pre_slow = dummy;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        pre_slow = pre_slow.next;
+    }
+    pre_slow.next = slow.next;
+    
+    return dummy.next;
+    
 };
