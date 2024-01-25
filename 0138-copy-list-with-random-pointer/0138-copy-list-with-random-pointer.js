cd16af451dbc8a967c3b490251c3164f69f46273
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-01-25 20:16:26 +0530

    Time: 71 ms (14.09%), Space: 52.5 MB (5.08%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
@@ -11,15 +11,69 @@
  * @param {Node} head
  * @return {Node}
  */
-var copyRandomList = function(head, map = new Map()) {
-    if (!head) return null;
-    if (map.has(head)) return map.get(head);
 
-    const clone = new Node(head.val);
 
-    map.set(head, clone);                           /*           | Space O(N) */
-    clone.next = copyRandomList(head.next, map);    /* Time O(N) | Space O(N) */
-    clone.random = copyRandomList(head.random, map);/* Time O(N) | Space O(N) */
+var copyRandomList = function(head) {
+    let linked = new Node(0, null, null);
+    let cur = head;
+    let headPointer = head;
+    let pointer = linked;
+    let dict = [];
+    let dict2 = []
+    let i = 0;
 
-    return clone;
-}
+    while (cur != null){
+        dict.push(new Node(cur.val, null, null));
+        dict2.push(cur)
+        i+=1;
+        cur = cur.next;
+
+    }
+    let j = 0;
+    while (headPointer!=null){
+        let rando = dict2.indexOf(headPointer.random);
+        dict[j].random = rando
+        headPointer = headPointer.next;
+        j+=1
+
+    }
+
+    let head2 = dict.at(0);
+    let p = 0;
+
+    console.log(dict);
+    while (p<dict.length-1){
+        
+        
+        dict.at(p).next = dict.at(p+1);
+        
+
+
+        if (dict.at(p).random != -1){
+            let val = dict.at(p).random;
+            dict.at(p).random = dict.at(val);
+        }
+        else{
+            dict.at(p).random = null;
+        }
+        p+=1
+    }
+    if (dict.length !=0){
+        if (dict.at(-1).random != -1){
+                let val = dict.at(-1).random;
+                dict.at(-1).random = dict.at(val);
+            }
+        else{
+                dict.at(-1).random = null;
+        }
+    }
+
+
+
+
+    
+    return head2;
+    
+    
+    
+};
