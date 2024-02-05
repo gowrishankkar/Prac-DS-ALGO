d834944d3940812e76a870270bc061f7ce8e9c77
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-05 22:15:06 +0530

    Time: 76 ms (67.77%), Space: 58.1 MB (25.68%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -3,17 +3,19 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function (nums, k) {
-    let count = 0,
-        sum = 0,
-        map = new Map();
-    map.set(0, 1);
-    for (const num of nums) {
-        sum += num;
-        if (map.has(sum - k)) {
-            count += map.get(sum - k);
-        }
-        map.set(sum, (map.get(sum) || 0) + 1);
+
+ // the question is contiguous subarray
+var subarraySum = function(nums, k) {
+    let count = 0
+    let sum = 0
+    let map = new Map()
+    map.set(0, 1)
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i]
+        if (map.has(sum-k)) count += map.get(sum-k);
+        map.set(sum, (map.get(sum) || 0) + 1)
     }
-    return count;
+
+    return count
 };
