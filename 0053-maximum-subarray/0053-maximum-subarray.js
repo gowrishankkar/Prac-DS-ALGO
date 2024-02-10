b63eadc4bf94f9c358b4c2b0cb5c36f5255e0785
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-10 23:27:40 +0530

    Time: 68 ms (74.09%), Space: 58.2 MB (10.74%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,47 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var maxSubArray = function(nums) {
-//      nums.sort((a, b) => a[1] - a[0] - (b[1] - b[0]));
-
-//     let totalCost = 0;
-//     let n = nums.length / 2;
-//     for (let i = 0; i < n; i++) {
-//         totalCost += nums[i][1] + nums[i + n][0];
-//     }
-//     return totalCost;
-// };
-
-
-// var maxSubArray = function(nums) {
-
-
-//         let res = -Infinity
-//         for(let  i = 0; i<nums.length; i++){
-//             let currSum = 0
-           
-//             for(let j = i; j<nums.length; j++){
-//                 currSum += nums[j]
-//                 res = Math.max(res, currSum)
-//             }
-         
-           
-//         }
-//     console.log('res', res)
-//     return res
-// };
-
-
 var maxSubArray = function(nums) {
-
-
-    let res = nums[0]
-    let currSum = 0
-    for(const num of nums){
-        if(currSum < 0) currSum = 0
-        currSum += num
-        res = Math.max(currSum, res)
-    }
-  
-    return res
+     var prev = 0;
+  var max = -Infinity;
+   
+  for (var i = 0; i < nums.length; i++) {
+    prev = Math.max(prev + nums[i], nums[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
 };
