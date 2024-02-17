733c5cc5f570d32f89672405f9855405ce73efcd
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-17 14:33:09 +0530

    Time: 54 ms (49.95%), Space: 48.1 MB (46.47%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -2,26 +2,49 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var findMin = function(arr) {
+//       let low = 0, high = arr.length - 1;
+//     let ans = Infinity;
+//     while (low <= high) {
+//         let mid = Math.floor((low + high) / 2);
+
+//         // If left part is sorted:
+//         if (arr[low] <= arr[mid]) {
+//             // Keep the minimum:
+//             ans = Math.min(ans, arr[low]);
+
+//             // Eliminate left half:
+//             low = mid + 1;
+//         } else { // If right part is sorted:
+//             // Keep the minimum:
+//             ans = Math.min(ans, arr[mid]);
+
+//             // Eliminate right half:
+//             high = mid - 1;
+//         }
+//     }
+//     return ans;
+// };
+
 var findMin = function(arr) {
-      let low = 0, high = arr.length - 1;
+    let n = arr.length; 
+    let l = 0, h = arr.length - 1;
     let ans = Infinity;
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2);
-
-        // If left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            // Keep the minimum:
-            ans = Math.min(ans, arr[low]);
-
-            // Eliminate left half:
-            low = mid + 1;
-        } else { // If right part is sorted:
-            // Keep the minimum:
-            ans = Math.min(ans, arr[mid]);
-
-            // Eliminate right half:
-            high = mid - 1;
+    while(l <= h){
+        let m= Math.floor((l + h) / 2);
+        if(arr[l] <= arr[h]){
+            ans = Math.min(ans, arr[l])
+            break;
+        }
+        if(arr[l] <= arr[m]){
+            ans = Math.min(ans, arr[m])
+            l = m + 1;
+        
+        }else if(arr[m] <= arr[h]){
+            ans = Math.min(ans, arr[m])
+                h = m - 1
         }
     }
+    
     return ans;
 };
