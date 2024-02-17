41d5025dcf7ce630a9c297f406a253bf4b7d250f
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-17 18:15:11 +0530

    Time: 44 ms (91.32%), Space: 48.5 MB (42.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,29 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(arr) {
-        let n = arr.length; // Size of the array
-
-    // Edge cases:
-    if (n === 1) return 0;
-    if (arr[0] > arr[1]) return 0;
-    if (arr[n - 1] > arr[n - 2]) return n - 1;
-
-    let low = 1, high = n - 2;
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2);
-
-        // If arr[mid] is the peak:
-        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
-            return mid;
-
-        // If we are in the left:
-        if (arr[mid] > arr[mid - 1]) low = mid + 1;
-
-        // If we are in the right:
-        // Or, arr[mid] is a common point:
-        else high = mid - 1;
+var findPeakElement = function(nums) {
+    //binary search
+    let left=0,right=nums.length-1;
+    while(left<=right){
+        let mid=~~((left+right)/2);
+        if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
+        nums[mid] < nums[mid+1] ? left=mid+1 : right=mid-1;
     }
-    // Dummy return statement
-    return -1;
+    return left;
 };
