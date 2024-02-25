8dcc48fa652b79abe62b63c4e9a85e15898dc9ad
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-25 23:18:22 +0530

    Time: 61 ms (65.35%), Space: 53.3 MB (32.00%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -10,19 +10,15 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root, min = -Infinity, max = Infinity) {
-    const isBaseCase = root === null;
-    if (isBaseCase) return true;
-
-    const isInvalid = (root.val <= min) || (max <= root.val);
-    if (isInvalid) return false;
-
-    return dfs(root, min, max);
+var isValidBST = function(root) {
+    return dfs(root, -Infinity, Infinity);
 };
 
 const dfs = (root, min, max) => {
-    const left = isValidBST(root.left, min, root.val);
-    const right = isValidBST(root.right, root.val, max);
+    if(root == null) return true;
+    if(root.val >= max || root.val <= min) return false;
+    const left = dfs(root.left, min, root.val);
+    const right = dfs(root.right, root.val, max);
 
     return left && right;
 }
