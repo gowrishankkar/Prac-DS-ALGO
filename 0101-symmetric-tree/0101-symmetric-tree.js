15e3f21c61c7c52ab2a99759e8287270119ecfde
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-25 12:15:19 +0530

    Time: 61 ms (56.33%), Space: 51.5 MB (35.38%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -10,20 +10,41 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function(root) {
+// var isSymmetric = function(root) {
     
     
  
-        if (root === null) return false;
-        return test(root.left, root.right);
+//         if (root === null) return false;
+//         return test(root.left, root.right);
  
 
+// };
+
+//     function test(left, right) {
+//         if (left === null || right === null) return left === right 
+
+//         if (left.val !== right.val) return false;
+
+//         return test(left.left, right.right) && test(left.right, right.left);
+//     }
+
+
+
+var isSymmetric = function(root) {
+    if (root == null) {
+        return true;
+    }
+    return recurse(root.left, root.right);
 };
 
-    function test(left, right) {
-        if (left === null || right === null) return left === right 
-
-        if (left.val !== right.val) return false;
-
-        return test(left.left, right.right) && test(left.right, right.left);
+var recurse = function(L, R) {
+    if (L == null && R == null) {
+        return true;
+    } else if (L == null || R == null) {
+        return false;
+    } else if (L.val != R.val) {
+        return false;
+    } else {
+        return (recurse(L.left, R.right) && recurse(L.right, R.left));
     }
+};
