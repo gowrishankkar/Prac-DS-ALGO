f7bd3d97cf8a7a835cd7e70a60e5fb652a542844
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-27 23:19:50 +0530

    Time: 69 ms (21.29%), Space: 53.3 MB (6.45%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+// var bstFromPreorder = function(preorder) {
+//     let i = 0;
+//     function build(A, i, bound){
+//         if(i == A.length || A[i] > bound) return null;
+//         let root = new TreeNode(A[i]);
+//         i++;
+//         root.left =  build(A, i, root.val);
+//         root.right =  build(A, i, bound);
+//         return root;
+//     }
+//     return build(preorder, i, Infinity)
+// };
+
+
+var bstFromPreorder = function (preorder) {
+        if (preorder.length === 0) return null
+        if (preorder.length === 1) return new TreeNode(preorder[0])
+        let root = new TreeNode(preorder[0])
+        let left = bstFromPreorder(preorder.slice(1).filter(each => each < preorder[0]))
+        let right = bstFromPreorder(preorder.slice(1).filter(each => each > preorder[0]))
+        if (root) root.left = left;
+        if (root) root.right = right;
+        return root
+};
