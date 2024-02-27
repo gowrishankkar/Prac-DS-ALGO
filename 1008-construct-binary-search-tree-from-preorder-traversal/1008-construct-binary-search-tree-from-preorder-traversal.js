62df4880800e8b846bcfc25a49ff229e85e607fe
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-27 23:21:04 +0530

    Time: 64 ms (38.06%), Space: 52.2 MB (45.81%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -24,13 +24,32 @@
 // };
 
 
+// var bstFromPreorder = function (preorder) {
+//         if (preorder.length === 0) return null
+//         if (preorder.length === 1) return new TreeNode(preorder[0])
+//         let root = new TreeNode(preorder[0])
+//         let left = bstFromPreorder(preorder.slice(1).filter(each => each < preorder[0]))
+//         let right = bstFromPreorder(preorder.slice(1).filter(each => each > preorder[0]))
+//         if (root) root.left = left;
+//         if (root) root.right = right;
+//         return root
+// };
+
+
 var bstFromPreorder = function (preorder) {
-        if (preorder.length === 0) return null
-        if (preorder.length === 1) return new TreeNode(preorder[0])
-        let root = new TreeNode(preorder[0])
-        let left = bstFromPreorder(preorder.slice(1).filter(each => each < preorder[0]))
-        let right = bstFromPreorder(preorder.slice(1).filter(each => each > preorder[0]))
-        if (root) root.left = left;
-        if (root) root.right = right;
+    let i = 0;
+    return build(preorder);
+
+
+    function build(arr, min =  Number.MIN_SAFE_INTEGER, max =  Number.MAX_SAFE_INTEGER) {
+      if (i === arr.length || arr[i] < min || arr[i] > max) {
+            return null
+        }
+        let root = new TreeNode(arr[i++])
+        root.left = build(arr, min , root.val)
+        root.right = build(arr, root.val , max)
+
         return root
+
+    }
 };
