1988f66d573649db2451a4db2c74a71d192631b3
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-28 22:56:47 +0530

    Time: 66 ms (80.54%), Space: 51.1 MB (27.52%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
@@ -3,19 +3,27 @@
  * @return {number}
  */
 var numberOfSubstrings = function(s) {
-    let obj = {'a': 0, 'b': 0, 'c': 0};
-    let total = 0, count = 0, start = 0;
-    for (let end = 0; end < s.length; end++) {
-        obj[s[end]]++;
-        if (obj[s[end]] == 1) count++;
-        while (count === 3) {
-            total += s.length - end;
-            obj[s[start]]--;
-            if (obj[s[start]] == 0) count--;
-            start++;
+    let a=0,b=0,c=0,count=0;
+    for(let i=0,j=0;i<s.length;i++){
+        if(s[i]=='a'){
+            a++;
+        }else if(s[i]=='b'){
+            b++;
+        }else{
+            c++;
         }
+        while(a&&b&&c){
+            count+=s.length-i;
+            if(s[j]=="a"){
+                a--;
+            }else if(s[j]=="b"){
+                b--;
+            }else{
+                c--;
+            }
+            j++;
+        }
+        
     }
-    return total;
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
+    return count;
 };
