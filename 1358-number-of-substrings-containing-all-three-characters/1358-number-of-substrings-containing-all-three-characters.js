cb3a99873d3a673f842affc32e93dfac26b810cc
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-28 22:55:25 +0530

    Time: 82 ms (45.64%), Space: 51 MB (39.60%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function(s) {
+    let obj = {'a': 0, 'b': 0, 'c': 0};
+    let total = 0, count = 0, start = 0;
+    for (let end = 0; end < s.length; end++) {
+        obj[s[end]]++;
+        if (obj[s[end]] == 1) count++;
+        while (count === 3) {
+            total += s.length - end;
+            obj[s[start]]--;
+            if (obj[s[start]] == 0) count--;
+            start++;
+        }
+    }
+    return total;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+};
