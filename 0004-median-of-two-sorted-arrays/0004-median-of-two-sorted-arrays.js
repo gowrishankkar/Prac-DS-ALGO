f9c7e42c1aa7bfeda64cce2fd8ada6e1736a1aad
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-02-29 22:57:42 +0530

    Time: 112 ms (23.79%), Space: 56.1 MB (27.45%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -3,23 +3,6 @@
  * @param {number[]} nums2
  * @return {number}
  */
-// var findMedianSortedArrays = function(nums1, nums2) {
-//   let mergedArr = [];
-//     mergedArr=[...nums1, ...nums2];
-//     mergedArr = mergedArr.sort((a,b)=>a-b);
-//     let median=Math.floor(mergedArr.length/2);
-//     if(mergedArr.length%2==0){
-        
-//         return (mergedArr[median] + mergedArr[median-1])/2;
-        
-//     }
-//     else{
-//         return mergedArr[median];
-//     }
-    
-// };
-
-
 var findMedianSortedArrays = function (nums1, nums2) {
     const canSwap = nums2.length < nums1.length;
     if (canSwap) [nums1, nums2] = [nums2, nums1];
