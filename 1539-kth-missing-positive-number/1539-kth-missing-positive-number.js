34654a8596b8b88815a3af6764e801a629242716
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-03 22:29:26 +0530

    Time: 64 ms (18.43%), Space: 51.1 MB (7.43%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -4,17 +4,28 @@
  * @return {number}
  */
 
-var findKthPositive = function(arr, k) {
-    if (!arr || arr.length < 1) {
-        return k;
-    }
+// var findKthPositive = function(arr, k) {
+//     if (!arr || arr.length < 1) {
+//         return k;
+//     }
         
-    let l = 0, h = arr.length - 1;
-    while(l <= h){
-        let mid = l + Math.floor((h - l)/2);
-        let missing = arr[mid] - (mid + 1);
-        if(missing < k) l = mid + 1;
-        else h = mid - 1;
+//     let l = 0, h = arr.length - 1;
+//     while(l <= h){
+//         let mid = l + Math.floor((h - l)/2);
+//         let missing = arr[mid] - (mid + 1);
+//         if(missing < k) l = mid + 1;
+//         else h = mid - 1;
+//     }
+//     return l + k;
+// };
+
+var findKthPositive = function(arr, k) {
+     let count = 0;
+  for (item of arr) {
+    if (item <= k + count) {
+      count++;
     }
-    return l + k;
+  }
+
+  return k + count;
 };
