bb433b8c747f9d68ac915c2830b1ccbf755c4a17
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-04 00:00:06 +0530

    Time: 50 ms (64.38%), Space: 48.7 MB (22.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,12 +2,24 @@
  * @param {number} n
  * @return {number}
  */
-memo = {0:1, 1:1}
+// memo = {0:1, 1:1}
+// var climbStairs = function(n) {
+//     if (n in memo) {
+//         return memo[n];
+//     }
+//     numWays = climbStairs(n - 1) + climbStairs(n - 2);
+//     memo[n] = numWays;
+//     return numWays;
+// };
+
+
 var climbStairs = function(n) {
-    if (n in memo) {
-        return memo[n];
+    let dp =  new Array(n + 1).fill(null);
+    var test = (i) =>{
+        if(i == 0) return 1;
+        if( i < 0) return 0;
+        if(dp[i]) return dp[i]
+        return dp[i] = test(i - 1) + test(i - 2)
     }
-    numWays = climbStairs(n - 1) + climbStairs(n - 2);
-    memo[n] = numWays;
-    return numWays;
+    return test(n)
 };
