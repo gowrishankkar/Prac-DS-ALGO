1ac40eec28ee17caf69b59ec85d081905c126eef
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-04 21:33:46 +0530

    Time: 286 ms (81.44%), Space: 52.4 MB (48.25%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -3,17 +3,34 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-     let n = matrix.length;
-    let m = matrix[0].length;
-    let row = 0;
-    let col = m - 1;
+// var searchMatrix = function(matrix, target) {
+//      let n = matrix.length;
+//     let m = matrix[0].length;
+//     let row = 0;
+//     let col = m - 1;
 
-    // Traverse the matrix from (0, m-1):
-    while (row < n && col >= 0) {
-        if (matrix[row][col] === target) return true;
-        else if (matrix[row][col] < target) row++;
-        else col--;
+//     // Traverse the matrix from (0, m-1):
+//     while (row < n && col >= 0) {
+//         if (matrix[row][col] === target) return true;
+//         else if (matrix[row][col] < target) row++;
+//         else col--;
+//     }
+//     return false;
+// };
+
+
+var searchMatrix = function(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) {
+            return true;
+        }
+        if (matrix[row][col] < target) {
+            row ++;
+        } else {
+            col --;
+        }
     }
     return false;
 };
