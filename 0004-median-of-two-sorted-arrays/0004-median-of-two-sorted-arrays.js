80969709ca7869e371be2b550a82b8bce4d61c8a
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-04 19:29:19 +0530

    Time: 100 ms (45.88%), Space: 53.4 MB (72.62%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,47 +4,28 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-    const canSwap = nums2.length < nums1.length;
-    if (canSwap) [nums1, nums2] = [nums2, nums1];
-
-    let [left, right] = [0, nums1.length - 1];
-    const totalLength = nums1.length + nums2.length;
-    const mid = totalLength >> 1;
-    const isEven = totalLength % 2 === 0;
-
-    while (true) {
-        const mid1 = left + right;
-        const mid2 = mid - mid1 - 2;
-        const { aLeft, aRight, bLeft, bRight } = getPointers(
-            nums1,
-            mid1,
-            nums2,
-            mid2
-        );
-
-        const isTarget = aLeft <= bRight && bLeft <= aRight;
-        if (isTarget)
-            return isEven
-                ? (Math.max(aLeft, bLeft) + Math.min(aRight, bRight)) / 2
-                : Math.min(aRight, bRight);
-
-        const isTargetGreater = aLeft <= bRight;
-        if (isTargetGreater) left = mid1 + 1;
-
-        const isTargetLess = bRight < aLeft;
-        if (isTargetLess) right = mid1 - 1;
+    let n1 = nums1.length, n2 = nums2.length;
+    if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+    let left = Math.floor((n1 + n2 + 1) / 2); 
+    let n = n1 + n2;
+    let l = 0, h = n1;
+    while(l <= h){
+        let m1 = Math.floor((l + h) / 2)
+        let m2 = left - m1;
+        let l1 = Number.MIN_SAFE_INTEGER, l2 = Number.MIN_SAFE_INTEGER;
+        let r1 = Number.MAX_SAFE_INTEGER, r2 = Number.MAX_SAFE_INTEGER;
+        if(m1 < n1) r1 = nums1[m1];
+        if(m2 < n2) r2 = nums2[m2];
+        if(m1 - 1 >= 0) l1 = nums1[m1 - 1];
+        if(m2 - 1 >= 0) l2 = nums2[m2 - 1];
+        if(l1 <= r2 && l2 <= r1){
+            if(n%2 == 1) return Math.max(l1, l2)
+            else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+        }
+        
+        if(l1 > r2) h = m1 - 1;
+        else l = m1 + 1
     }
+    return 0;
 };
 
-const getPointers = (nums1, mid1, nums2, mid2) => {
-    const getLeft = (nums, index) => (0 <= index ? nums[index] : -Infinity);
-
-    const [aLeft, bLeft] = [getLeft(nums1, mid1), getLeft(nums2, mid2)];
-
-    const getRight = (nums, index) =>
-        index + 1 < nums.length ? nums[index + 1] : Infinity;
-
-    const [aRight, bRight] = [getRight(nums1, mid1), getRight(nums2, mid2)];
-
-    return { aLeft, aRight, bLeft, bRight };
-};
