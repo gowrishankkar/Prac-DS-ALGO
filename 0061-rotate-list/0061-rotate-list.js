53fadb9947c8ceaa0927a87a545671d563ead979
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-06 21:42:26 +0530

    Time: 72 ms (14.01%), Space: 52.1 MB (14.16%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.js
+++ 0061-rotate-list/0061-rotate-list.js
@@ -10,34 +10,68 @@
  * @param {number} k
  * @return {ListNode}
  */
+// var rotateRight = function(head, k) {
+//     if (head == null || head.next == null) return head;
+//     let tail = head;
+//     let n = 1;
+//     while(tail.next){
+//         tail = tail.next;
+//         n++;
+//     }
+    
+//     if(k % n == 0) return head;
+    
+//     k = k % n;
+    
+//     tail.next =  head;
+//     let lastNode =  findNthNode(head, n - k)
+//     head = lastNode.next;
+//     lastNode.next = null;
+//     return head;
+    
+// };
+
+
+// function findNthNode(head, n){
+//     let count = 1;
+//     while(head){
+//         if(count == n ) return head;
+//         count++;
+//         head = head.next;
+//     }
+//     return head;
+// }
+
+
+
 var rotateRight = function(head, k) {
-    if (head == null || head.next == null) return head;
-    let tail = head;
-    let n = 1;
-    while(tail.next){
-        tail = tail.next;
-        n++;
+    if (!head) return null;
+    if (!head?.next  || k === 0) return head;
+    
+    let length = 0;
+    let curr = head;
+
+    while (curr) {
+        length++;
+        curr = curr.next
     }
-    
-    if(k % n == 0) return head;
-    
-    k = k % n;
-    
-    tail.next =  head;
-    let lastNode =  findNthNode(head, n - k)
-    head = lastNode.next;
-    lastNode.next = null;
+
+    k = k < length ? k : k % length;
+    if (k === 0) return head;
+
+    curr = head
+    while (curr) {
+        if (!curr?.next?.next) {
+            const value = curr?.next?.val;
+            const node = new ListNode(value);
+            curr.next = null;
+            node.next = head
+            head = node;
+            curr = head;
+            k--;
+            if (k === 0) break;
+        }
+        curr = curr.next
+    }
     return head;
-    
 };
-
-
-function findNthNode(head, n){
-    let count = 1;
-    while(head){
-        if(count == n ) return head;
-        count++;
-        head = head.next;
-    }
-    return head;
-}
