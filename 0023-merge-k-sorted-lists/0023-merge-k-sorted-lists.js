deaf1f7ad87374b81271d4177579f8974905ccbf
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-06 23:41:45 +0530

    Time: 93 ms (55.86%), Space: 55.6 MB (43.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,38 +9,49 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
- var mergeKLists = function(lists) {
-    const minHeap = getMinHeap(lists);
+var mergeKLists = function(lists) {
+    
+    if(lists.length === 0) {
+        return null;
+    };
 
-    return mergeLists(minHeap)
+    while(lists.length > 1) {
+        let list1 = lists.shift();
+        let list2 = lists.shift();
+
+        const merged = mergeLists(list1, list2);
+
+        lists.push(merged);
+    };
+
+    return lists[0];
+
 };
 
-const getMinHeap = (lists) => {
-    const heap = new MinPriorityQueue({ priority: ({ val }) => val });
+const mergeLists = (list1, list2) => {
+    let dummy = new ListNode();
+    let head = dummy;
 
-    for (const node of lists) {
-        if (!node) continue;
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            dummy.next = list1;
+            list1 = list1.next;
 
-        heap.enqueue(node);
-    }
+        } else {
+            dummy.next = list2;
+            list2 = list2.next;
+        };
 
-    return heap;
-}
+        dummy = dummy.next
+    };
 
+    if(list1) {
+        dummy.next = list1;
+    };
 
-const mergeLists = (minHeap) => {
-    let sentinel = tail = new ListNode();
+    if(list2) {
+        dummy.next = list2;
+    };
 
-    while (!minHeap.isEmpty()) {
-        const node = minHeap.dequeue().element;
-
-        tail.next = node;
-        tail = tail.next;
-
-        if (!node.next) continue;
-
-        minHeap.enqueue(node.next);
-    }
-
-    return sentinel.next;
-}
+    return head.next;
+};
