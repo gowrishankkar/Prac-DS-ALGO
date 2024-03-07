e6cf8a42f1989b6272aea1f79533a7dc5845dbcb
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-07 18:44:35 +0530

    Time: 66 ms (17.35%), Space: 49.6 MB (66.40%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+
+    for (let c of s) {
+        if (
+            c == "(" || c == "{" || c == "["
+        ) {
+            stack.push(c);
+
+        }
+        else {
+
+            if (!stack.length ||
+                (c === ")" && stack[stack.length - 1] !== "(") ||
+                (c === "}" && stack[stack.length - 1] !== "{") ||
+                (c === "]" && stack[stack.length - 1] !== "[")) 
+                {
+                return false
+               }
+              {
+                stack.pop()
+              }
+        }
+    }
+    return !stack.length
+};
