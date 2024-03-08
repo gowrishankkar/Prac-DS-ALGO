83c43f1bd1bb058cda6f2bafc9edd2e1be5072a7
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-08 10:34:25 +0530

    Time: 80 ms (23.10%), Space: 55.2 MB (26.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,73 +4,52 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    const freqMap = new Map();
-    for(const num of nums) {
-        const freq = freqMap.has(num) ? freqMap.get(num) + 1: 1;
-        freqMap.set(num, freq);
+    let map = new Map();
+    let minHeap = new MinPriorityQueue();
+    let ans = new Array();
+
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i]))
+            map.set(nums[i], map.get(nums[i])+1);
+        else
+            map.set(nums[i], 1);
     }
-    const heap = new MaxHeap([...freqMap.entries()]);
-    let ans = []
-    while(ans.length < k){
-        ans.push(heap.pop().value);
+    for(let items of map){
+        minHeap.enqueue(items[0], items[1]);
+
+        if(minHeap.size() > k){
+            minHeap.dequeue();
+        }
+    }
+    while(!minHeap.isEmpty()){
+        ans.push(minHeap.dequeue().element);
     }
     return ans;
 
 };
+// var topKFrequent = function(nums, k) {
+//     let map = new Map();
+//     let minHeap = new MinPriorityQueue();
+//     let ans = new Array();
 
+//     for(let i = 0; i < nums.length; i++){
+//         if(map.has(nums[i]))
+//             map.set(nums[i], map.get(nums[i])+1);
+//         else
+//             map.set(nums[i], 1);
+//     }
 
-class MaxHeap {
-    constructor(arr) {
-        this.heap = [];
-        this.size = 0;
-        for(const item of arr) {
-            this.push({
-                freq: item[1],
-                value: item[0]
-            })
-        }
-    }
-    push(value) {
-        this.heap.push(value);
-        if(this.size === 0) {
-            ++this.size;
-            return;
-        }
-        this.siftDown(this.size++)
-    }
-    siftDown(curr) {
-        const parent = Math.floor((curr-1)/2);
-        if(parent < 0) return;
-        if(this.heap[parent].freq < this.heap[curr].freq) {
-            [this.heap[parent], this.heap[curr]] = [this.heap[curr], this.heap[parent]];
-            this.siftDown(parent);
-        }
-    }
-    pop() {
-        [this.heap[0], this.heap[this.size-1]] = [this.heap[this.size-1], this.heap[0]];
-        const value = this.heap.pop();
-        --this.size;
-        this.siftUp(0);
-        return value;
-    }
-    siftUp(parent) {
-        if(parent >= this.size) return;
-        const child1 = parent * 2 +1;
-        const child2 = child1 + 1;
-        let minIndex = parent;
-        let minVal = this.heap[minIndex];
-        if(child1 < this.size && this.heap[child1].freq > minVal.freq) {
-            minIndex = child1;
-            minVal = this.heap[child1];
-        }
-        // console.log(this.heap, child2)
-        if(child1 < this.size && child2 < this.size && this.heap[child2].freq > minVal.freq) {
-            minIndex = child2;
-            minVal = this.heap[child2];
-        }
-        if(minIndex !== parent) {
-             [this.heap[minIndex], this.heap[parent]] = [this.heap[parent], this.heap[minIndex]];
-            this.siftUp(minIndex)
-        }
-    }
-}
+//     for(let items of map){
+//         minHeap.enqueue(items[0], items[1]);
+
+//         if(minHeap.size() > k){
+//             minHeap.dequeue();
+//         }
+//     }
+
+//     while(!minHeap.isEmpty()){
+//         ans.push(minHeap.dequeue().element);
+//     }
+
+//     return ans;
+// };
