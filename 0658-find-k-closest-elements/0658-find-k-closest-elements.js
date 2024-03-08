3295d30163d40a1a320958ca3161d386830b22e0
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-08 09:39:55 +0530

    Time: 152 ms (7.17%), Space: 59.9 MB (7.17%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.js
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -5,10 +5,15 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    arr = arr.map((val) => [Math.abs(x - val), val]);
-    arr.sort((a, b) => {
-        if(a[0] == b[0]) return a[1] - b[1];
-        return a[0] - b[0];
-    });
-    return arr.slice(0, k).map((a) => a[1]).sort((a, b) => a - b);
+    let maxHeap = new MaxPriorityQueue();
+    let ans = new Array();
+    for(let i = 0; i < arr.length; i++){
+        maxHeap.enqueue(arr[i], Math.abs(arr[i]-x));
+        if(maxHeap.size() > k)
+            maxHeap.dequeue();
+    }
+    while(!maxHeap.isEmpty()){
+        ans.push(maxHeap.dequeue().element);
+    }
+    return ans.sort((a,b)=> a-b);
 };
