e3be10e0ad3861f2ae7666b5d1ac1713985d83bd
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-08 09:40:41 +0530

    Time: 79 ms (61.78%), Space: 55.1 MB (72.29%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.js
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -4,16 +4,30 @@
  * @param {number} x
  * @return {number[]}
  */
+// var findClosestElements = function(arr, k, x) {
+//     let maxHeap = new MaxPriorityQueue();
+//     let ans = new Array();
+//     for(let i = 0; i < arr.length; i++){
+//         maxHeap.enqueue(arr[i], Math.abs(arr[i]-x));
+//         if(maxHeap.size() > k)
+//             maxHeap.dequeue();
+//     }
+//     while(!maxHeap.isEmpty()){
+//         ans.push(maxHeap.dequeue().element);
+//     }
+//     return ans.sort((a,b)=> a-b);
+// };
+
+
 var findClosestElements = function(arr, k, x) {
-    let maxHeap = new MaxPriorityQueue();
-    let ans = new Array();
-    for(let i = 0; i < arr.length; i++){
-        maxHeap.enqueue(arr[i], Math.abs(arr[i]-x));
-        if(maxHeap.size() > k)
-            maxHeap.dequeue();
+    let left = 0;
+    let right = arr.length - 1
+    while (right - left >= k) {
+        if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+            right --
+        } else {
+            left ++
+        }
     }
-    while(!maxHeap.isEmpty()){
-        ans.push(maxHeap.dequeue().element);
-    }
-    return ans.sort((a,b)=> a-b);
+    return arr.slice(left, right + 1);
 };
