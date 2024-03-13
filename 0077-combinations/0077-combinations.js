2e7a990b02cc2dcf291f451566d914a9d0cc534c
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-14 00:53:18 +0530

    Time: 453 ms (79.08%), Space: 121.9 MB (43.58%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -3,25 +3,42 @@
  * @param {number} k
  * @return {number[][]}
  */
-var combine = function(n, k) {
-    const res = [];
-    const track = [];
+// var combine = function(n, k) {
+//     const res = [];
+//     const track = [];
     
-    const backtrack = (n, k, start)=>{
-        if(track.length === k){
-            res.push(track.slice());
+//     const backtrack = (n, k, start)=>{
+//         if(track.length === k){
+//             res.push(track.slice());
+//             return
+//         }
+        
+//         if(track.length > k) return;
+//         for(let i = start; i <= n; i++){
+//             track.push(i);
+//             backtrack(n,k,i+1);
+//             track.pop();
+//         }
+//     }
+    
+//     backtrack(n, k, 1);
+    
+//     return res;
+// };
+
+var combine = function (n, k) {
+    let ans = [];
+    const helper = (index, pairs, n, k) => {
+        if (pairs.length === k) {
+            ans.push([...pairs]);
             return
         }
-        
-        if(track.length > k) return;
-        for(let i = start; i <= n; i++){
-            track.push(i);
-            backtrack(n,k,i+1);
-            track.pop();
+        for (let i = index; i <= n; i++) {
+            pairs.push(i);
+            helper(i + 1, pairs, n, k);
+            pairs.pop();
         }
     }
-    
-    backtrack(n, k, 1);
-    
-    return res;
+    helper(1, [], n, k);
+    return ans;
 };
