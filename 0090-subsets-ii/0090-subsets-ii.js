da016f5966ce7a954083ce64bfd850ae58a63d68
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-14 11:02:36 +0530

    Time: 81 ms (9.39%), Space: 54.9 MB (9.58%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -2,60 +2,27 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// var subsetsWithDup = function(nums) {
-//      let A = nums
-//        let result = []
-//         A.sort((a, b) => a - b)
-//         let subset = [];
-//         const dfs = (subset, map, index) => {
-//             result.push(subset.slice());
+ var subsetsWithDup = function(nums) {
+    nums.sort((a, b) => a - b);
 
-//             for (let i = 0; i < Object.keys(map).length; i++) {
-//                 if()
-//                 subset.push(A[i]);
-//                 dfs(i + 1);
-//                 subset.pop()
-//             }
-
-//         }
-//         let map = {}
-//         A.forEach(ele =>{
-//             map[ele] = (map[ele] || 0) + 1
-//         } )
-//     console.log('map', map)
-//         dfs(subset, , map, 0);
-//         return result
-// };
-
-
-var subsetsWithDup = function(nums) {
-   let res = []
-   let A = nums
-        A.sort((a, b) => a - b);
-     
-        function dfs(sub, idx) {
-            if (idx == A.length) {
-                res.push(sub.slice());
-                return
-            }
-
-            sub.push(A[idx])
-            dfs(sub, idx + 1);
-            sub.pop();
-
-            while(idx + 1 < A.length && A[idx] === A[idx + 1]) idx++;
-            dfs(sub, idx +1)
-
-            // for (let i = idx; i < A.length; i++) {
-            //     sub.push(A[i])
-            //     dfs(i + 1);
-            //     sub.pop();
-
-            // }
-        }
-       let sub = []
-        console.log('res', res)
-        dfs( sub, 0);
-        return res
+    return dfs(nums);
 };
 
+const dfs = (nums, index = 0, set = [], subset = []) => {
+    subset.push(set.slice())
+
+    for (let i = index; i < nums.length; i++) {
+        const isDuplicate = (index < i) && (nums[i - 1] === nums[i])
+        if (isDuplicate) continue;
+
+        backTrack(nums, i, set, subset);
+    }
+
+    return subset
+}
+
+const backTrack = (nums, i, set, subset) => {
+    set.push(nums[i]);
+        dfs(nums, (i + 1), set, subset);
+    set.pop();
+}
