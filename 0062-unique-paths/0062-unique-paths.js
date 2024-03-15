86361c1202d295b33020d838e5a4edfca6ea543f
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-15 14:43:20 +0530

    Time: 62 ms (18.00%), Space: 49.2 MB (50.08%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -21,14 +21,54 @@
 //     return data
 // }; 
 
+// var uniquePaths = function(m, n) {
+    
+//  let dp = new Array(m).fill(-1).map(()=>Array(n).fill(-1))
+//   for (let i = 0; i < m; i++) {
+//     for (let j = 0; j < n; j++) {
+//       // Base condition: If we are at the top-left cell (0, 0), there is only one way to reach it.
+//       if (i === 0 && j === 0) {
+//         dp[i][j] = 1;
+//         continue;
+//       }
+
+//       // Initialize variables to store the number of ways to reach the current cell by moving up and left.
+//       let up = 0;
+//       let left = 0;
+
+//       // Check if we can move up (i > 0).
+//       if (i > 0) {
+//         up = dp[i - 1][j];
+//       }
+
+//       // Check if we can move left (j > 0).
+//       if (j > 0) {
+//         left = dp[i][j - 1];
+//       }
+
+//       // The total number of ways to reach the current cell is the sum of ways from above (up) and from the left (left).
+//       dp[i][j] = up + left;
+//     }
+//   }
+//     console.table(dp)
+//   // The result is stored in the bottom-right cell of the grid (m-1, n-1).
+//   return dp[m - 1][n - 1];
+//     // const data =  test(m - 1, n - 1, dp);
+//     // console.log(dp)
+//     // return data
+// }; 
+
+
+
 var uniquePaths = function(m, n) {
     
- let dp = new Array(m).fill(-1).map(()=>Array(n).fill(-1))
+ let prev = new Array(m).fill(0)
   for (let i = 0; i < m; i++) {
+    let curr = new Array(n).fill(0)
     for (let j = 0; j < n; j++) {
       // Base condition: If we are at the top-left cell (0, 0), there is only one way to reach it.
       if (i === 0 && j === 0) {
-        dp[i][j] = 1;
+        curr[j] = 1;
         continue;
       }
 
@@ -38,21 +78,22 @@ var uniquePaths = function(m, n) {
 
       // Check if we can move up (i > 0).
       if (i > 0) {
-        up = dp[i - 1][j];
+        up = prev[j];
       }
 
       // Check if we can move left (j > 0).
       if (j > 0) {
-        left = dp[i][j - 1];
+        left = curr[j - 1];
       }
 
       // The total number of ways to reach the current cell is the sum of ways from above (up) and from the left (left).
-      dp[i][j] = up + left;
+      curr[j] = up + left;
     }
+    prev = [...curr]
   }
-    console.table(dp)
+    // console.table(dp)
   // The result is stored in the bottom-right cell of the grid (m-1, n-1).
-  return dp[m - 1][n - 1];
+  return prev[n - 1];
     // const data =  test(m - 1, n - 1, dp);
     // console.log(dp)
     // return data
