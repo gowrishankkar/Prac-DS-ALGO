468f4553c5b55b58e99d602061b91ce1348715a9
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-15 11:10:36 +0530

    Time: 39 ms (97.83%), Space: 48.6 MB (65.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -2,13 +2,38 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var rob = function(nums) {
+//     if(nums.length == 1) return nums[0];
+//     let dp = [];
+//     dp[0] = nums[0];
+//     dp[1] = Math.max(nums[0], nums[1]);
+//     for(let i=2; i < nums.length; i++) {
+//         dp[i] =  Math.max(nums[i] + dp[i-2], dp[i-1]);
+//     }
+//     return dp[dp.length-1]
+// };
+
 var rob = function(nums) {
-    if(nums.length == 1) return nums[0];
-    let dp = [];
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-    for(let i=2; i < nums.length; i++) {
-        dp[i] =  Math.max(nums[i] + dp[i-2], dp[i-1]);
-    }
-    return dp[dp.length-1]
+    let n = nums.length;
+    let dp = new Array(n).fill(-1) ; 
+    // Call the solveUtil function with the last index
+    return solveUtil(n - 1, nums, dp);
 };
+
+
+function solveUtil(ind, arr, dp) {
+
+    // Base cases
+    if (ind === 0) return arr[ind];
+    if (ind < 0) return 0;
+        // Check if the result for this index is already calculated
+    if (dp[ind] !== -1) return dp[ind];
+
+
+    // Calculate the maximum value by either picking or not picking the current element
+    const pick = arr[ind] + solveUtil(ind - 2, arr, dp);
+    const nonPick = 0 + solveUtil(ind - 1, arr, dp);
+
+    // Store the result in the DP array and return it
+    return (dp[ind] = Math.max(pick, nonPick));
+}
