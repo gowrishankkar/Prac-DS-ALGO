4d2eb433dfbccd8e3b42ee92ede910662f713078
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-15 21:30:33 +0530

    Time: 71 ms (27.48%), Space: 52 MB (35.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -2,31 +2,63 @@
  * @param {number[][]} matrix
  * @return {number}
  */
+// var minFallingPathSum = function(matrix) {
+//     const n = matrix.length;
+//     const m = matrix[0].length;
+//     const dp = new Array(n).fill().map(() => new Array(m).fill(0));
+//       for (let j = 0; j < m; j++) {
+//         dp[0][j] = matrix[0][j];
+//       }
+ 
+    
+//     for(let i = 1; i < n ; i++){
+//         for(let j = 0; j < m ; j++){
+//             const up = matrix[i][j] + dp[i-1][j];
+//             let left = matrix[i][j] 
+//             let right = matrix[i][j]
+//             if(j - 1 >= 0) left = matrix[i][j] + dp[i-1][j-1]
+//             else left = Infinity;
+//             if(j + 1 < m) right = matrix[i][j] + dp[i-1][j+1]
+//             else right = Infinity;
+//             dp[i][j] = Math.min(up, left , right)
+//         }
+//     }
+    
+//     let max = Number.MAX_SAFE_INTEGER;
+//     for(let i = 0; i < m; i++){
+//         max = Math.min(max, dp[n-1][i])
+//     }
+    
+//     return max;
+
+// };
+
+
 var minFallingPathSum = function(matrix) {
     const n = matrix.length;
     const m = matrix[0].length;
-    const dp = new Array(n).fill().map(() => new Array(m).fill(0));
-      for (let j = 0; j < m; j++) {
-        dp[0][j] = matrix[0][j];
-      }
- 
-    
+    let prev = new Array(m).fill(0);
+    let cur = new Array(m).fill(0);
+    for (let j = 0; j < m; j++) {
+        prev[j] = matrix[0][j];
+    }
     for(let i = 1; i < n ; i++){
         for(let j = 0; j < m ; j++){
-            const up = matrix[i][j] + dp[i-1][j];
+            const up = matrix[i][j] + prev[j];
             let left = matrix[i][j] 
             let right = matrix[i][j]
-            if(j - 1 >= 0) left = matrix[i][j] + dp[i-1][j-1]
+            if(j - 1 >= 0) left = matrix[i][j] + prev[j-1]
             else left = Infinity;
-            if(j + 1 < m) right = matrix[i][j] + dp[i-1][j+1]
+            if(j + 1 < m) right = matrix[i][j] + prev[j+1]
             else right = Infinity;
-            dp[i][j] = Math.min(up, left , right)
+            cur[j] = Math.min(up, left , right)
         }
+            prev = [...cur];
     }
     
     let max = Number.MAX_SAFE_INTEGER;
     for(let i = 0; i < m; i++){
-        max = Math.min(max, dp[n-1][i])
+        max = Math.min(max,prev[i])
     }
     
     return max;
