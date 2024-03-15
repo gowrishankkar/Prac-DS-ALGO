53988c0f219cec17d365c2a3c6d2b2e59e7dcef7
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-16 01:41:10 +0530

    Time: 88 ms (93.45%), Space: 56.4 MB (93.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -1,26 +1,68 @@
-var longestCommonSubsequence = (text1, text2, p1 = 0, p2 = 0, memo = initMemo(text1, text2)) => {
-    const isBaseCase = ((p1 === text1.length) || (p2 === text2.length));
-    if (isBaseCase) return 0;
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+// var longestCommonSubsequence = function(text1, text2) {
+//     const n = text1.length;
+//     const m = text2.length;
+//     let dp = Array.from({length : n}, () => Array(m).fill(-1));
+//     function lcs(ind1, ind2) {
+//         if(ind1 < 0 || ind2 < 0) return 0;
+//         if(dp[ind1][ind2] !== - 1) return dp[ind1][ind2];
+//         if(text1[ind1] === text2[ind2]){
+//             return dp[ind1][ind2] = 1 + lcs(ind1 - 1 , ind2 - 1);
+//         } else {
+//             return dp[ind1][ind2] = Math.max(lcs(ind1 - 1, ind2), lcs(ind1, ind2 - 1))
+//         }
+//     }
+//     return lcs(n - 1, m - 1);
+// };
 
-    const hasSeen = (memo[p1][p2] !== null);
-    if (hasSeen) return memo[p1][p2];
 
-    return dfs(text1, text2, p1, p2, memo);/* Time O(N * M) | Space O((N * M) + HEIGHT) */
-}
 
-var initMemo = (text1, text2) => new Array((text1.length + 1)).fill()/* Time O(N) | Space O(N) */
-    .map(() => new Array((text2.length + 1)).fill(null));                 /* Time O(M) | Space O(M) */
+// var longestCommonSubsequence = function(text1, text2) {
+//     const n = text1.length;
+//     const m = text2.length;
+//     const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(-1));
+//     for (let i = 0; i <= n; i++) {
+//         dp[i][0] = 0;
+//     }
+//     for (let i = 0; i <= m; i++) {
+//         dp[0][i] = 0;
+//     }
+//     for (let ind1 = 1; ind1 <= n; ind1++) {
+//         for (let ind2 = 1; ind2 <= m; ind2++) {
+//             if (text1[ind1 - 1] === text2[ind2 - 1]) {
+//                 dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+//             } else {
+//                 dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+//             }
+//         }
+//     }
 
-var dfs = (text1, text2, p1, p2, memo) => {
-    const left = (longestCommonSubsequence(text1, text2, (p1 + 1), (p2 + 1), memo) + 1);/* Time O(N * M) | Space O(HEIGHT) */
-    const right =                                                                       /* Time O(N * M) | Space O(HEIGHT) */
-        Math.max(longestCommonSubsequence(text1, text2, p1, (p2 + 1), memo), longestCommonSubsequence(text1, text2, (p1 + 1), p2, memo));
+//         return dp[n][m];
+// };
 
-    const isEqual = (text1[p1] == text2[p2]);
-    const count = isEqual
-        ? left
-        : right
 
-    memo[p1][p2] = count;                                                               /*               | Space O(N * M) */
-    return memo[p1][p2];
-}
+
+var longestCommonSubsequence = function(text1, text2) {
+    const n = text1.length;
+    const m = text2.length;
+    let prev = new Array(m + 1).fill(0);
+    let cur = new Array(m + 1).fill(0);
+    for (let ind1 = 1; ind1 <= n; ind1++) {
+        for (let ind2 = 1; ind2 <= m; ind2++) {
+            if (text1[ind1 - 1] === text2[ind2 - 1]) {
+                cur[ind2] = 1 + prev[ind2 - 1];
+            } else {
+                cur[ind2] = Math.max(prev[ind2], cur[ind2 - 1]);
+            }
+        }
+        // Update 'prev' with the values of 'cur' for the next iteration
+        prev = [...cur];
+    }
+
+        return prev[m];
+};
+
