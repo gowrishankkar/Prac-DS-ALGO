865dc851b7cfbadf61dfdaf60fa78570c731d3cf
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-16 23:22:02 +0530

    Time: 159 ms (36.28%), Space: 64.6 MB (58.11%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.js
+++ 0044-wildcard-matching/0044-wildcard-matching.js
@@ -47,23 +47,44 @@
 // };
 
 
-const dp = (s, p, si, pi, memo) => {
-    if (si > s.length) { return false; }
-    if (pi >= p.length) { return si >= s.length; }
-    if (memo[si][pi] !== undefined) { return memo[si][pi]; }
+// const dp = (s, p, si, pi, memo) => {
+//     if (si > s.length) { return false; }
+//     if (pi >= p.length) { return si >= s.length; }
+//     if (memo[si][pi] !== undefined) { return memo[si][pi]; }
     
-    if (s[si] === p[pi] || p[pi] === '?') {
-        memo[si][pi] = dp(s, p, si + 1, pi + 1, memo);
-    } else if (p[pi] === '*') {
-        memo[si][pi] = dp(s, p, si, pi + 1, memo) || dp(s, p, si + 1, pi, memo);
-    }  else {
-        memo[si][pi] = false;
-    }
+//     if (s[si] === p[pi] || p[pi] === '?') {
+//         memo[si][pi] = dp(s, p, si + 1, pi + 1, memo);
+//     } else if (p[pi] === '*') {
+//         memo[si][pi] = dp(s, p, si, pi + 1, memo) || dp(s, p, si + 1, pi, memo);
+//     }  else {
+//         memo[si][pi] = false;
+//     }
     
-    return memo[si][pi];
-}
+//     return memo[si][pi];
+// }
+
+// const isMatch = (s, p) => {
+//     const memo = Array.from(Array(s.length + 1), () => Array(p.length + 1));
+//     return dp(s, p, 0, 0, memo);
+// };
+
 
 const isMatch = (s, p) => {
-    const memo = Array.from(Array(s.length + 1), () => Array(p.length + 1));
-    return dp(s, p, 0, 0, memo);
+    let n = s.length, m = p.length;
+    let dp = Array.from(Array(n + 1), () => Array(m + 1).fill(false));
+    
+	dp[n][m] = true;
+    
+    for (let i = n; i >= 0; i--) {
+        for (let j = m - 1; j >= 0; j--) {
+            if (i < n && (s[i] === p[j] || p[j] === '?')) {
+                dp[i][j] = dp[i + 1][j + 1];
+            }
+            else if (p[j] === '*') {
+				dp[i][j] = i === n ? dp[i][j + 1] : (dp[i][j + 1] || dp[i + 1][j]);
+            }
+        }
+    }
+    
+    return dp[0][0];
 };
