6c27a262e81e78277cd46492d5721e5a3a93ac79
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-03-16 10:24:22 +0530

    Time: 87 ms (77.27%), Space: 71.2 MB (56.64%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.js
+++ 0115-distinct-subsequences/0115-distinct-subsequences.js
@@ -1,36 +1,27 @@
-var numDistinct = (s, t) => {
-    const tabu = initTabu(s, t);/* Time O(N * M) | Space O(N * M) */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var numDistinct = function(s, t) {
+    let n = s.length;
+    let m = t.length;
+    
+    let dp = new Array(n).fill(null).map(() => new Array(m).fill(-1));
+    return getCount(s, t, n - 1, m - 1, dp);
+};
 
-    search(s, t, tabu);         /* Time O(N * M) | Space O(N * M) */
-
-    return tabu[0][0];
-}
-
-var initTabu = (s, t) => {
-    const tabu = new Array(s.length + 1).fill()/* Time O(N) | Space O(N) */
-        .map(() => new Array(t.length + 1));       /* Time O(M) | Space O(M) */
-
-    tabu[s.length].fill(0);                    /*           | Space O(N * M) */
-
-    for (let r = 0; r <= s.length; ++r) {      /* Time O(N) */
-        tabu[r][t.length] = 1;                     /*       | Space O(N * M) */
+function getCount(s, t, i, j, dp){
+    if(j < 0) return 1;
+    if(i < 0) return 0;
+    if(dp[i][j] !== -1) return dp[i][j];
+    
+    if(s[i] === t[j]){
+        const take = getCount(s, t, i - 1, j - 1, dp);
+        const notTake = getCount(s, t, i - 1, j, dp);
+        return dp[i][j] = take + notTake;
+    } else {
+         return dp[i][j] = getCount(s, t, i - 1, j, dp);
     }
 
-    return tabu;
-}
-
-var search = (s, t, tabu) => {
-    for (let r = (s.length - 1); (0 <= r); r--) {/* Time O(N) */
-        for (let c = (t.length - 1); (0 <= c); c--) {/* Time O(M) */
-            const left = tabu[r + 1][c];
-
-            const isEqual = (s[r] === t[c]);
-
-            const right = isEqual
-                ? tabu[r + 1][c + 1] 
-                : 0
-            
-            tabu[r][c] = left + right;                     /* Space O(N * M) */
-        }
-    }
 }
