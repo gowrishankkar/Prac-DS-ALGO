2d7258d41585ddc32f36ea78930aee75892a266e
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-04-01 23:32:18 +0530

    Time: 64 ms (17.04%), Space: 50.9 MB (45.90%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -1,4 +1,8 @@
+// var reverseWords = function(s) {
+//     let arr = s.split(" ").filter(elem => elem !== "");
+//     return arr.reverse().join(" ");
+// };
 var reverseWords = function(s) {
-    let arr = s.split(" ").filter(elem => elem !== "");
-    return arr.reverse().join(" ");
+    const ss = s.split(' ');
+    return ss.filter(e => e !== '').map(e => e.trim()).reverse().join(' ');
 };
