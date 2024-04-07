563a54d606c8a8727ff394e13252e781475e8bb7
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-04-07 23:36:03 +0530

    Time: 55 ms (42.46%), Space: 49.2 MB (25.97%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for(let i = 0; i < haystack.length; i++) {
+        if(haystack[i] == needle[0]) {
+            let temp = i;
+           for(let j = 0; j < needle.length; j++) {
+               if(haystack[i + j] != needle[j]) {
+                 break;
+               } else if(j === needle.length - 1) {
+                 return temp;
+               }
+           }
+        }
+    }
+    return -1;
+};
