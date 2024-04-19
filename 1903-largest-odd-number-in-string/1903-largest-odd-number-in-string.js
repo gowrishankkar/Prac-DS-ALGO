0930cb120c343942d8f3391bd0be387a30137b35
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-04-19 23:18:36 +0530

    Time: 60 ms (68.14%), Space: 55.4 MB (22.55%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -2,10 +2,20 @@
  * @param {string} num
  * @return {string}
  */
+// var largestOddNumber = function(num) {
+//     let i = num.length - 1
+//     while (i >= 0 && !(num.charCodeAt(i) % 2)) {
+//         i--
+//     }
+//     return num.slice(0, ++i)
+// };
+
 var largestOddNumber = function(num) {
-    let i = num.length - 1
-    while (i >= 0 && !(num.charCodeAt(i) % 2)) {
-        i--
+     for(let i =num.length-1;i>=0;i--){
+        if(num[i]%2!=0){
+            console.log(num[i])
+            return num.substring(0,i+1);
+        }
     }
-    return num.slice(0, ++i)
+    return ""
 };
