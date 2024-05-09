3d4aa48170ba9eadc0fe537a95561e101bc77d52
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-05-10 00:15:13 +0530

    Time: 485 ms (38.23%), Space: 53.4 MB (38.91%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,40 +4,63 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    for(let row = 0; row < board.length; row++) {
-        for(let col = 0; col < board[0].length; col++){
-            if (dfs(board, row, col, word, 0)) return true;
+    const dir = [[0, -1], [-1, 0], [0, 1], [1, 0]];
+    let n = board.length;
+    let m = board[0].length;
+    function dfs(r, c, index = 0){
+       if(index === word.length ) return true;
+       if (r < 0 || c < 0 || r >= n || c >= m || word[index] !== board[r][c]) return false;
+        board[r][c] = "*";
+        let found = false;
+        for(let [dr, dc] of dir){
+            let nr = r + dr;
+            let nc = c + dc;
+            if(dfs( nr, nc,  index + 1)){
+                found=  true;
+            }
+        }
+        board[r][c] = word[index];
+        return found;
+    }
+    
+
+    for(let i = 0; i < n ; i++){
+        for(let j = 0; j < m ; j++){
+           if(board[i][j] === word[0] && dfs( i, j, 0)) return true;
         }
     }
+    return false
+};
 
-    return false;
-}
+// var exist = function (board, word) {
+//     for (let i = 0; i < board.length; i++) {
+//         for (let j = 0; j < board[0].length; j++) {
+//             if (board[i][j] == word[0] && searchWord(board, word, i, j, 0)) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false
+// };
 
-const dfs = (board, row, col, word, index) => {
-    if (index === word.length) return true;
-    if (isOutOfBound(board, row, col)) return false;
-    if (board[row][col] !== word[index]) return false
-        
-    board[row][col] = '*';
-    
-    const hasWord = Object
-        .values(directions(row, col))
-        .filter(([r, c]) => dfs(board, r, c, word, index + 1))
-        .length
-    
-    board[row][col] = word[index];
-    return hasWord;
-}
+// var searchWord = function (board, word, row, col, position) {
+//     if (position == word.length) {
+//         return true;
+//     }
+//     if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || position > word.length || board[row][col] == "*" || board[row][col] != word.charAt(position)) {
+//         return false;
+//     }
 
-const isOutOfBound = (board, row, col) => {
-    const isRowOutOfBound = row < 0 || board.length - 1 < row
-    const isColOutOfBound = col < 0 || board[0].length - 1 < col 
-    return isRowOutOfBound || isColOutOfBound
-}
-
-const directions = (row, col) => ({
-    up: [row - 1, col],
-    down: [row + 1, col],
-    left: [row, col - 1,],
-    right: [row, col + 1]
-})
+//     let temp = board[row][col];
+//     board[row][col] = "*"
+//     let down = searchWord(board, word, row + 1, col, position + 1);
+//     let up = searchWord(board, word, row - 1, col, position + 1);
+//     let left = searchWord(board, word, row, col - 1, position + 1);
+//     let right = searchWord(board, word, row, col + 1, position + 1);
+//     if (down || up || left || right) {
+//         return true;
+//     } else {
+//         board[row][col] = temp;
+//     }
+//     return false
+// }
