7b7258cf2eb65a2a0ae8416c3850cf1944f344c5
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-05-12 22:12:36 +0530

    Time: 65 ms (84.91%), Space: 53.7 MB (39.62%) - LeetHub

---
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var largestLocal = function (grid) {
+    let len = grid.length;
+    let res = [];
+
+    for (let i = 1; i < len - 1; i++) {
+        let row = [];
+        for (let j = 1; j < len - 1; j++) {
+            let max = 0;
+            for (let k = i - 1; k <= i + 1; k++) {
+                for (let l = j - 1; l <= j + 1; l++) {
+                    max = Math.max(max, grid[k][l]);
+                }
+            }
+            row.push(max);
+        }
+        res.push(row);
+    }
+    return res;
+};
