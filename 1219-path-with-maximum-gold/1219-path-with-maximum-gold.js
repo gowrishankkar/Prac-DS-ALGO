55ef0bc71d2fd036f50f541b256476388fd78b5a
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-05-14 23:44:30 +0530

    Time: 195 ms (72.73%), Space: 49.1 MB (67.53%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
@@ -2,53 +2,32 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var getMaximumGold = function(grid) {
-    const lenH = grid.length;
-    const lenW = grid[0].length;
-    const traveled = [];
-    for (let i = 0; i < lenH; ++i) {
-        traveled.push([]);
-        for (let j = 0; j < lenW; ++j) {
-            traveled[i].push(0);
+var getMaximumGold = function (grid) {
+    let m = grid[0].length;
+    let n = grid.length;
+    let maxPath = 0;
+   for(let i = 0; i<n; i++){
+        for(let j = 0; j<m; j++){
+            if(grid[i][j]!==0) maxPath = Math.max(maxPath, maxGold(grid, i, j));
         }
     }
-    
-    const dfs = (i, j) => {
-        if (grid[i][j] <=0 ) {
-            return 0;
-        }
-        let result = 0;
-        if (i > 0 && !traveled[i-1][j]) {
-            traveled[i-1][j] = 1;
-            result = Math.max(result, dfs(i-1, j));
-            traveled[i-1][j] = 0;
-        }
-        if (j > 0 && !traveled[i][j-1]) {
-            traveled[i][j-1] = 1;
-            result = Math.max(result, dfs(i, j-1));
-            traveled[i][j-1] = 0;
-        }
-        if (i < lenH-1 && !traveled[i+1][j]) {
-            traveled[i+1][j] = 1;
-            result = Math.max(result, dfs(i+1, j));
-            traveled[i+1][j] = 0;
-        }
-        if (j < lenW-1 && !traveled[i][j+1]) {
-            traveled[i][j+1] = 1;
-            result = Math.max(result, dfs(i, j+1));
-            traveled[i][j+1] = 0;
-        }
-        result += grid[i][j];
+    return maxPath;
+};
+
+const maxGold= (grid, l=0, r=0, result=0)=>{
+    if(l>=grid.length || r>=grid[0].length || l<0 || r<0){
         return result;
     }
-    
-    let maxGold = 0;
-    for (let i = 0; i < lenH; ++i) {
-        for (let j = 0; j < lenW; ++j) {
-            traveled[i][j] = 1;
-            maxGold = Math.max(dfs(i, j), maxGold);
-            traveled[i][j] = 0;
-        }
+     else if (grid[l][r] !== 0) {
+        result=result+grid[l][r];
+        let temp = grid[l][r];
+        grid[l][r] = 0;
+        let bottom = maxGold(grid,l,r+1, result);
+        let top = maxGold(grid,l+1,r, result);
+        let right = maxGold(grid,l,r-1, result);
+        let left = maxGold(grid,l-1,r, result);
+        grid[l][r] = temp;
+        return Math.max(left, right, top, bottom);
     }
-    return maxGold;
-};
+    else return result;
+}
