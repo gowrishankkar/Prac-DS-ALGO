80829917308cc2b03bb99f9d4b580415fc26b87e
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-05-20 22:32:47 +0530

    Time: 60 ms (36.75%), Space: 54.9 MB (17.95%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
@@ -2,4 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-let subsetXORSum = nums => nums.reduce((xors, num) => [...xors, ...xors.map((xor) => xor ^ num)], [0]).reduce((sum, xor) => sum + xor, 0)
+var subsetXORSum = (nums) => arraySum(arraySubsets(nums).map((subset) => arrayXor(subset)));
+
+const arrayXor = (arr) => arr.reduce((p, c) => p ^ c, 0);
+
+const arraySum = (arr) => arr.reduce((p, c) => p + c, 0);
+
+const arraySubsets = (arr) => {
+  const subsets = [];
+  const find = (result, arr, temp, index) => {
+    if (index === arr.length) {
+      result.push(temp);
+      return;
+    }
+    find(result, arr, [...temp], index + 1);
+    temp.push(arr[index]);
+    find(result, arr, [...temp], index + 1);
+  };
+  find(subsets, arr, [], 0);
+  return subsets;
+};
