4141da9760e5c485c94248d030b170391a538af2
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-06-04 23:25:18 +0530

    Time: 67 ms (17.06%), Space: 51.1 MB (39.77%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -2,41 +2,58 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindrome = function(s) {
-    let hash = new Map();
+// var longestPalindrome = function(s) {
+//     let hash = new Map();
 
-    let result = 0;
-    let seenOne = false;
-    let odd = 0;
+//     let result = 0;
+//     let seenOne = false;
+//     let odd = 0;
 
-    for(let i = 0; i < s.length; i++){
-        if(hash.has(s[i])){
-            hash.set(s[i], hash.get(s[i]) + 1)
-        } else {
-            hash.set(s[i], 1)
-        }
-    }
+//     for(let i = 0; i < s.length; i++){
+//         if(hash.has(s[i])){
+//             hash.set(s[i], hash.get(s[i]) + 1)
+//         } else {
+//             hash.set(s[i], 1)
+//         }
+//     }
 
-    for(let value of hash.values()){
-        if(value == 1 && !seenOne){
-            seenOne = true
-            if(odd != 0) result += odd-1
-            continue
-        }
+//     for(let value of hash.values()){
+//         if(value == 1 && !seenOne){
+//             seenOne = true
+//             if(odd != 0) result += odd-1
+//             continue
+//         }
 
-        if(value % 2 == 0){
-            result += value
-            continue
-        }
+//         if(value % 2 == 0){
+//             result += value
+//             continue
+//         }
 
-        if(value % 2 != 0){
-            if(odd == 0 && !seenOne){
-                odd = value
-            } else {
-                result += value-1
-            }
-        }
-    }
+//         if(value % 2 != 0){
+//             if(odd == 0 && !seenOne){
+//                 odd = value
+//             } else {
+//                 result += value-1
+//             }
+//         }
+//     }
     
-    return seenOne ? result+1 : result+odd;
+//     return seenOne ? result+1 : result+odd;
+// };
+
+var longestPalindrome = function(s) {
+    
+    let stack= new Set();
+    let result=0;
+    
+    for (const c of s ){
+        if (stack.has(c)){
+            result+=2
+            stack.delete(c)
+        }
+        else {
+            stack.add(c)
+        }
+    }
+    return result + (stack.size>0?1:0)
 };
