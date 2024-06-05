7986763099c2ffbd86cf74cc823cc1a1fce53665
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-06-05 22:17:03 +0530

    Time: 58 ms (78.02%), Space: 53.6 MB (47.80%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -2,16 +2,29 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var commonChars = function (words) {
-  const result = [];
-  const firstWordCharacters = words[0].split("");
+// var commonChars = function (words) {
+//   const result = [];
+//   const firstWordCharacters = words[0].split("");
 
-  for (let i = 0; i < firstWordCharacters.length; i++) {
-    if (words.every((x) => x.includes(firstWordCharacters[i]))) {
-      result.push(firstWordCharacters[i]);
-      words = words.map((x) => x.replace(firstWordCharacters[i], ""));
+//   for (let i = 0; i < firstWordCharacters.length; i++) {
+//     if (words.every((x) => x.includes(firstWordCharacters[i]))) {
+//       result.push(firstWordCharacters[i]);
+//       words = words.map((x) => x.replace(firstWordCharacters[i], ""));
+//     }
+//   }
+
+//   return result;
+// };
+
+
+var commonChars = function(words) {
+    let result = [...words[0]];
+    for(let i = 1; i < words.length; i++) {
+      result = result.filter(c => {
+        const l = words[i].length;
+        words[i] = words[i].replace(c, '');
+        return l > words[i].length;
+      });
     }
-  }
-
-  return result;
+    return result;
 };
