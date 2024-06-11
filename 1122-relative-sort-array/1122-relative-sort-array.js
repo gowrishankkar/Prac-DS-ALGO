d5c530118958fb04d3dfcb371f2a45a492970a24
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-06-11 23:30:04 +0530

    Time: 54 ms (64.09%), Space: 49.3 MB (66.82%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -3,24 +3,42 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-var relativeSortArray = function(arr1, arr2) {
-    let count = Array(1001)
-    for (let a of arr2)
-        count[a] = 0
-    let not_arr2 = []
-    for (let a of arr1) {
-        if (count[a] != undefined)
-            count[a]++
-        else
-            not_arr2.push(a)
+// var relativeSortArray = function(arr1, arr2) {
+//     let count = Array(1001)
+//     for (let a of arr2)
+//         count[a] = 0
+//     let not_arr2 = []
+//     for (let a of arr1) {
+//         if (count[a] != undefined)
+//             count[a]++
+//         else
+//             not_arr2.push(a)
+//     }
+//     let in_arr2 = []
+//     for (let a of arr2) {
+//         if (count[a] != undefined) {
+//             for (let i = 0; i < count[a]; i++) {
+//                 in_arr2.push(a)
+//             }
+//         }
+//     }
+//     return in_arr2.concat(not_arr2.sort((a,b)=>a-b))
+// };
+
+const relativeSortArray = (arr1, arr2) => {
+  let sortedList = [];
+  for (const n of arr2) {
+    let index = -1;
+    index = arr1.indexOf(n, index + 1);
+    while (index !== -1) {
+      arr1[index] = undefined;
+      sortedList.push(n);
+      index = arr1.indexOf(n, index + 1);
     }
-    let in_arr2 = []
-    for (let a of arr2) {
-        if (count[a] != undefined) {
-            for (let i = 0; i < count[a]; i++) {
-                in_arr2.push(a)
-            }
-        }
-    }
-    return in_arr2.concat(not_arr2.sort((a,b)=>a-b))
+  }
+
+  return [
+    ...sortedList,
+    ...arr1.filter(item => item !== undefined).sort((a, b) => a - b)
+  ];
 };
