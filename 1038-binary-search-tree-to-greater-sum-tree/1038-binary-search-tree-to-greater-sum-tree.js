8d9178bd32a1c45f58ceb9d42d71c0f7c713c4e4
Gowri Shankar Prashanth <shankarg.kvsulur@gmail.com>
2024-06-25 11:58:42 +0530

    Time: 58 ms (31.74%), Space: 49.2 MB (53.92%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    const gt = (root) => {
+        if (root.right)
+            gt(root.right)
+        this.s += root.val
+        root.val = this.s 
+        if (root.left)
+            gt(root.left) 
+    }     
+
+    this.s = 0
+    gt(root)
+    return root
+};
